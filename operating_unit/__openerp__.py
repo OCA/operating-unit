213d4044efb4c7c9b7e6d7999c5c134729dc777f
jbeficent <jordi.ballester@eficent.com>
2015-12-10 11:26:09 +0100

    [FIX] remove description from __openerp__.py

--- operating_unit/__openerp__.py
+++ operating_unit/__openerp__.py
@@ -5,27 +5,13 @@
 
 {
     "name": "Operating Unit",
+    "summary": "An operating unit (OU) is an organizational entity part of a "
+               "company",
     "version": "9.0.1.0.0",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
     "category": "Generic",
     "depends": ["base"],
-    "description": """
-Operating Unit
-==============
-An operating unit (OU) is an organizational entity part of a company, with
-separate management ownership. Management by OU is aimed to introduce the
-following features:
-
-- Partition data from other OU's.
-- Define its own sequencing schemes.
-- Administer user access to the data for processing and reporting.
-- Is not product or customer specific.
-- Provides OU specific P&L and Balance sheet
-
-The current module defines the operating unit entity and the user's security
-rules. Other modules extend the standard Odoo apps with the OU.
-    """,
     "license": "LGPL-3",
     "data": [
         "security/operating_unit_security.xml",
