7387dbbedff0650fd52bf64973520f1f899ad308
Sudhir Arya <sudhir.arya@serpentcs.com>
2015-12-23 19:24:56 +0530

    [ADD] Added Account Operating Unit module for v9.

---
+++ account_operating_unit/models/company.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    inter_ou_clearing_account_id = fields.Many2one('account.account',
+                                                   'Inter-operating unit\
+                                                   clearing account')
---
+++ account_operating_unit/wizard/account_report_common.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import fields, models
+
+
+class AccountCommonReport(models.TransientModel):
+    _inherit = "account.common.report"
+
+    operating_unit_ids = fields.Many2many('operating.unit',
+                                          string='Operating Units',
+                                          required=False)
+
+    def _build_contexts(self, data):
+        result = super(AccountCommonReport, self)._build_contexts(data)
+        data2 = {}
+        data2['form'] = self.read(['operating_unit_ids'])[0]
+        result['operating_unit_ids'] = \
+            'operating_unit_ids' in data2['form'] \
+            and data2['form']['operating_unit_ids'] or False
+        return result
