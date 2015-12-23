3097d03fc6a5452eacefcd4a677dd50b747adc9c
Sudhir Arya <sudhir.arya@serpentcs.com>
2015-12-23 19:24:56 +0530

    [ADD] Added Account Operating Unit module for v9.

---
+++ account_operating_unit/models/invoice.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import api, fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
+                                        default=lambda self:
+                                        self.env['res.users'].
+                                        operating_unit_default_get(self._uid))
+
+    @api.multi
+    def finalize_invoice_move_lines(self, move_lines):
+        move_lines = super(AccountInvoice,
+                           self).finalize_invoice_move_lines(move_lines)
+        new_move_lines = []
+        for line_tuple in move_lines:
+            if self.operating_unit_id:
+                line_tuple[2]['operating_unit_id'] = \
+                    self.operating_unit_id.id
+            new_move_lines.append(line_tuple)
+        return new_move_lines
+
+    @api.multi
+    def _check_company_operating_unit(self):
+        for pr in self.browse():
+            if (
+                pr.company_id and
+                pr.operating_unit_id and
+                pr.company_id != pr.operating_unit_id.company_id
+            ):
+                return False
+        return True
+
+    _constraints = [
+        (_check_company_operating_unit,
+         'The Company in the Invoice and in the Operating '
+         'Unit must be the same.', ['operating_unit_id',
+                                    'company_id'])]
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    operating_unit_id = fields.Many2one('operating.unit',
+                                        related='invoice_id.operating_unit_id',
+                                        string='Operating Unit', store=True,
+                                        readonly=True)
