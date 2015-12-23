7eb1ef6b85ad96aacf6d22776462ec475028f15a
Sudhir Arya <sudhir.arya@serpentcs.com>
2015-12-23 19:24:56 +0530

    [ADD] Added Account Operating Unit module for v9.

---
+++ account_operating_unit/models/account_account.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import fields, models
+
+
+class AccountAccount(models.Model):
+    _inherit = "account.account"
+
+    operating_unit_id = fields.Many2one('operating.unit',
+                                        'Default Operating Unit',
+                                        default=lambda self:
+                                        self.env['res.users'].
+                                        operating_unit_default_get(self._uid))
