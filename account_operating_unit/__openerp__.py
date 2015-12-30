8d9abcee054d8a30905e895829e816a168f787ee
jbeficent <jordi.ballester@eficent.com>
2015-12-30 12:48:37 +0100

    [IMP] changed Eficent copyright

--- account_operating_unit/__openerp__.py
+++ account_operating_unit/__openerp__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
@@ -8,7 +9,8 @@
     "summary": "An operating unit (OU) is an organizational entity part of a\
         company",
     "version": "9.0.1.0.0",
-    "author": "Eficent, Serpent Consulting Services Pvt. Ltd.,"
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
     "category": "Generic/Sales & Purchases",
