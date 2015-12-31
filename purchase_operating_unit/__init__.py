00adf11ff9e9dd56ac636900473464026e64fbcb
Sudhir Arya <sudhir.arya@serpentcs.com>
2015-12-31 19:24:48 +0530

    [ADD] Added Purchase Operating Unit module.

--- purchase_operating_unit/__init__.py
+++ purchase_operating_unit/__init__.py
@@ -2,4 +2,4 @@
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from . import stock
+from . import models
