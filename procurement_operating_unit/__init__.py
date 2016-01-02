1b613f86de0e2ac2ea6e4d4a4aa9573bb85cc301
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-01-02 10:31:06 +0530

    Added Procurement Operating Unit module.

---
+++ procurement_operating_unit/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
