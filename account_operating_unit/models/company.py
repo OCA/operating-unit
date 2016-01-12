3ecd65cab6a566e134cd818a8208cbf976948e45
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2016-01-12 19:25:47 +0530

    [IMP] : Migrated the code to version 9

--- account_operating_unit/models/company.py
+++ account_operating_unit/models/company.py
@@ -3,7 +3,7 @@
 # - Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ResCompany(models.Model):
@@ -12,3 +12,15 @@ class ResCompany(models.Model):
     inter_ou_clearing_account_id = fields.Many2one('account.account',
                                                    'Inter-operating unit\
                                                    clearing account')
+
+    ou_is_self_balanced = fields.Boolean('Operating Units are self-balanced',
+                help="Activate if your company is required to generate a "
+                     "balanced balance sheet for each operating unit.",
+                     default = True)
+
+    @api.one
+    @api.constrains('ou_is_self_balanced')
+    def _inter_ou_clearing_acc_required(self):
+        if self.ou_is_self_balanced and not \
+                self.inter_ou_clearing_account_id:
+            return False
