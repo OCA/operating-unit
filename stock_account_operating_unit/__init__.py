b2837b0e55e5c0a1ac5f451fc571fca097cc0e3e
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2016-01-18 14:37:44 +0530

    Added stock_account OU module
    Added Test Cases.
    
    Improved code for test cases
    
    Migrated Valuation method of quants.
    
    Migrated Valuation method _account_entry_move of quants.
    
    Completed test cases and modified valuation method

---
+++ stock_account_operating_unit/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from . import model
