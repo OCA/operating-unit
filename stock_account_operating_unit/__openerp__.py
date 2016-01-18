28c7b6ae2479663082d61cd6d2c13a3fb8ceb47d
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2016-01-18 14:37:44 +0530

    [ADD] : Added stock_account OU module

---
+++ stock_account_operating_unit/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Operating Unit in Stock Management with Real-Time Valuation",
+    "summary": "An operating unit (OU) is an organizational entity part of a\
+        company",
+    "version": "9.0.1.0.0",
+    "category": "Generic Modules/Sales & Purchases",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "website": "http://www.eficent.com",
+    "license": "LGPL-3",
+    "website": "http://www.eficent.com",
+    "depends": ['stock_operating_unit', 'account_operating_unit'],
+    "data": [],
+    "installable": True,
+}
