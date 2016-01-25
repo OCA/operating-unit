ca1375dad1a09d6024876ba8504d85360c9f9e09
jbeficent <jordi.ballester@eficent.com>
2016-01-25 09:42:27 +0100

    [FIX] Applied review comments by @elicoidal

--- account_operating_unit/__openerp__.py
+++ account_operating_unit/__openerp__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# © 2016 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2016 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": 'Accounting with Operating Units',
-    "summary": "An operating unit (OU) is an organizational entity part of a\
-        company",
+    "summary": "Introduces Operating Unit fields in invoices and "
+               "Accounting Entries with clearing account",
     "version": "9.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
-    "category": "Generic/Sales & Purchases",
+    "category": "Accounting & Finance",
     "depends": ['account', 'operating_unit'],
     "license": "LGPL-3",
     "data": [
