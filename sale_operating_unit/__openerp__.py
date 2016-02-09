2dfa48a0bbff89d226237ec23dae7e075ce01321
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-02-09 15:06:46 +0530

    Migrated sale_operating_unit module and its test cases.

---
+++ sale_operating_unit/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Operating Unit in Sales",
+    "version": "8.0.1.0.0",
+    "summary": "An operating unit (OU) is an organizational entity part of a\
+        company",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "http://www.eficent.com",
+    "category": "Purchase Management",
+    "depends": ["sale", "operating_unit"],
+    "data": [
+        "views/sale_view.xml",
+        "security/sale_security.xml",
+    ],
+    'installable': True,
+    'active': False,
+}
