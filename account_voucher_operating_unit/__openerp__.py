cca7297de198d6264b1544f4d131368c3f8b08d2
jbeficent <jordi.ballester@eficent.com>
2016-02-09 14:39:24 +0100

    [ADD] account_voucher_operating_unit

---
+++ account_voucher_operating_unit/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Ecosoft Co. Ltd. - Kitti Upariphutthiphong
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Vouchers with Operating Units',
+    'version': '1.0',
+    'category': 'Generic Modules/Sales & Purchases',
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Ecosoft Co. Ltd.,"
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.eficent.com',
+    'depends': ['account_voucher_move_line_create_hooks',
+                'account_operating_unit', 'operating_unit'],
+    'data': [
+        'views/account_voucher_payment_receipt_view.xml',
+        'views/account_voucher_sales_purchase_view.xml',
+        'security/account_voucher_security.xml'
+    ],
+    'installable': True,
+}
