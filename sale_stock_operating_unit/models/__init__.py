f1b8b8b3eaaddab29866080aef663d7af89c8e26
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-02-10 17:09:27 +0530

    Migrated Sale Stock OU module from v7 to v8.

--- sale_stock_operating_unit/models/__init__.py
+++ sale_stock_operating_unit/models/__init__.py
@@ -3,4 +3,4 @@
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import sale
+from . import sale_stock
