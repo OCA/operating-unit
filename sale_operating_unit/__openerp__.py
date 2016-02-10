0310349ee9fdb24d4df0bfed0ea96f9699e09400
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-02-10 17:10:46 +0530

    [IMP] Added OU and WH constraints in SO.

--- sale_operating_unit/__openerp__.py
+++ sale_operating_unit/__openerp__.py
@@ -6,14 +6,14 @@
 {
     "name": "Operating Unit in Sales",
     "version": "8.0.1.0.0",
-    "summary": "An operating unit (OU) is an organizational entity part of a\
-        company",
+    "summary": "An operating unit (OU) is an organizational entity part of a "
+        "company",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "http://www.eficent.com",
-    "category": "Purchase Management",
+    "category": "Sales Management",
     "depends": ["sale", "operating_unit"],
     "data": [
         "views/sale_view.xml",
