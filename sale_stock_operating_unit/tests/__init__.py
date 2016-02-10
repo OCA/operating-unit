e83496bd6868fe1ee1c9ee159447492cc34e7dd8
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-02-10 18:27:35 +0530

    Added Test cases and Readme file.

--- sale_stock_operating_unit/tests/__init__.py
+++ sale_stock_operating_unit/tests/__init__.py
@@ -4,7 +4,3 @@
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import test_sale_stock_operating_unit
-
-checks = [
-    test_sale_stock_operating_unit,
-]
