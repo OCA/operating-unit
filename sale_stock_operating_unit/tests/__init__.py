1a6932a6112d1380782d794be9866512d7c61bef
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-02-10 15:12:43 +0530

    Added Sale Stock OU module for v8.

---
+++ sale_stock_operating_unit/tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import test_sale_stock_operating_unit
+
+checks = [
+    test_sale_stock_operating_unit,
+]
