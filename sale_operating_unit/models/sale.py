49c22bf2f61cabac897f0d4fd8b2ce66599893bc
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-02-10 17:10:46 +0530

    Added OU and WH constraints in SO.

--- sale_operating_unit/models/sale.py
+++ sale_operating_unit/models/sale.py
@@ -4,12 +4,11 @@
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from openerp import api, fields, models
-from openerp.tools.translate import _
 from openerp.exceptions import Warning
+from openerp.tools.translate import _
 
 
 class SaleOrder(models.Model):
-
     _inherit = 'sale.order'
 
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
@@ -25,12 +24,20 @@ class SaleOrder(models.Model):
             raise Warning(_('Configuration error!\nThe Company in the\
             Sales Order and in the Operating Unit must be the same.'))
 
+    @api.one
+    @api.constrains('operating_unit_id', 'warehouse_id')
+    def _check_wh_operating_unit(self):
+        if self.operating_unit_id and\
+                self.operating_unit_id != self.warehouse_id.operating_unit_id:
+            raise Warning(_('Configuration error!\nThe Operating Unit \
+            in the Sales Order and in the Warehouse must be the same.'))
+
     @api.model
     def _make_invoice(self, order, lines):
-        res = super(SaleOrder, self)._make_invoice(order, lines)
-        invoice = self.env['account.invoice'].browse(res)
+        inv_id = super(SaleOrder, self)._make_invoice(order, lines)
+        invoice = self.env['account.invoice'].browse(inv_id)
         invoice.write({'operating_unit_id': order.operating_unit_id.id})
-        return res
+        return inv_id
 
 
 class SaleOrderLine(models.Model):
