dd062555f1f54410c14b9fedde9d35684acc9d67
jbeficent <jordi.ballester@eficent.com>
2016-02-17 15:49:35 +0100

    [ADD] purchase request operating unit
    [FIX] remove reference to warehouse
    
    [ENH] purchase_request_operating_unit,
    purchase_rquest_procurement_operating_unit
    
    9.0 purchase request operating unit - fixed travis (#81)
    
    * [FIX] travis

---
+++ purchase_request_procurement_operating_unit/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016 Serpent Consulting Services Pvt. Ltd.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Purchase Request Procurement with Operating Units",
+    "version": "9.0.1.0.0",
+    "author": "Eficent"
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "website": "http://www.eficent.com",
+    "category": "Purchase Management",
+    "depends": ["purchase_request_procurement",
+                "purchase_request_operating_unit",
+                'procurement_operating_unit'],
+    'installable': True,
+}
