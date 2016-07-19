8c4b325ba92ce282ff832ab6a0d266373427dcf7
aheficent <ahenriquez@eficent.com>
2016-07-19 13:24:23 +0200

    [FIX] Adding constraint. The Operating unit must belong the company in the stock warehouse. Changing api.one by api.multi

--- stock_operating_unit/model/stock.py
+++ stock_operating_unit/model/stock.py
@@ -18,63 +18,77 @@ class StockWarehouse(models.Model):
                                         operating_unit_default_get(self._uid))
 
 
+    @api.multi
+    @api.constrains('operating_unit_id', 'company_id')
+    def _check_company_operating_unit(self):
+        for rec in self:
+            if rec.company_id and rec.operating_unit_id and \
+                            rec.company_id != rec.operating_unit_id.company_id:
+                raise Warning(_('Configuration error!\nThe Company in the\
+                Stock Warehouse and in the Operating Unit must be the same.'))
+
+
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
     operating_unit_id = fields.Many2one('operating.unit',
                                         'Operating Unit')
 
-    @api.one
+    @api.multi
     @api.constrains('operating_unit_id')
     def _check_warehouse_operating_unit(self):
-        warehouse_obj = self.env['stock.warehouse']
-        warehouse = warehouse_obj.search([('wh_input_stock_loc_id', 'in',
-                                           self.ids)])
-        for w in warehouse:
-            if self.operating_unit_id != w.operating_unit_id:
-                raise Warning(_('Configuration error!\nThis location is\
-                                assigned to a warehouse that belongs to a\
-                                different operating unit.'))
-        warehouse = warehouse_obj.search([('lot_stock_id', 'in', self.ids)])
-        for w in warehouse:
-            if self.operating_unit_id != w.operating_unit_id:
-                raise Warning(_('Configuration error!\nThis location is\
-                                assigned to a warehouse that belongs to a\
-                                different operating unit.'))
-        warehouse = warehouse_obj.search([('wh_output_stock_loc_id', 'in',
-                                           self.ids)])
-        for w in warehouse:
-            if self.operating_unit_id != w.operating_unit_id:
-                raise Warning(_('Configuration error!\nThis location is\
-                assigned to a warehouse that belongs to a different\
-                operating unit.'))
+        for rec in self:
+            warehouse_obj = self.env['stock.warehouse']
+            warehouse = warehouse_obj.search([('wh_input_stock_loc_id', 'in',
+                                               rec.ids)])
+            for w in warehouse:
+                if rec.operating_unit_id != w.operating_unit_id:
+                    raise Warning(_('Configuration error!\nThis location is\
+                                    assigned to a warehouse that belongs to a\
+                                    different operating unit.'))
+            warehouse = warehouse_obj.search([('lot_stock_id', 'in', rec.ids)])
+            for w in warehouse:
+                if self.operating_unit_id != w.operating_unit_id:
+                    raise Warning(_('Configuration error!\nThis location is\
+                                    assigned to a warehouse that belongs to a\
+                                    different operating unit.'))
+            warehouse = warehouse_obj.search([('wh_output_stock_loc_id', 'in',
+                                               rec.ids)])
+            for w in warehouse:
+                if rec.operating_unit_id != w.operating_unit_id:
+                    raise Warning(_('Configuration error!\nThis location is\
+                    assigned to a warehouse that belongs to a different\
+                    operating unit.'))
 
-    @api.one
+    @api.multi
     @api.constrains('operating_unit_id')
     def _check_required_operating_unit(self):
-        if self.usage == 'internal' and not self.operating_unit_id:
-            raise Warning(_('Configuration error!\nThe operating unit\
-            should be assigned to internal locations and to non other.'))
-        if self.usage != 'internal' and self.operating_unit_id:
-            raise Warning(_('Configuration error!\nThe operating unit\
-            should be assigned to internal locations and to non other.'))
+        for rec in self:
+            if rec.usage == 'internal' and not rec.operating_unit_id:
+                raise Warning(_('Configuration error!\nThe operating unit\
+                should be assigned to internal locations and to non other.'))
+            if rec.usage != 'internal' and rec.operating_unit_id:
+                raise Warning(_('Configuration error!\nThe operating unit\
+                should be assigned to internal locations and to non other.'))
 
-    @api.one
+    @api.multi
     @api.constrains('operating_unit_id', 'company_id')
     def _check_company_operating_unit(self):
-        if self.company_id and self.operating_unit_id and\
-                self.company_id != self.operating_unit_id.company_id:
-            raise Warning(_('Configuration error!\nThe Company in the\
-            Stock Location and in the Operating Unit must be the same.'))
+        for rec in self:
+            if rec.company_id and rec.operating_unit_id and\
+                    rec.company_id != rec.operating_unit_id.company_id:
+                raise Warning(_('Configuration error!\nThe Company in the\
+                Stock Location and in the Operating Unit must be the same.'))
 
-    @api.one
+    @api.multi
     @api.constrains('operating_unit_id', 'location_id')
     def _check_parent_operating_unit(self):
-        if (self.location_id and self.location_id.usage == 'internal'
-            and self.operating_unit_id and self.operating_unit_id !=
-                self.location_id.operating_unit_id):
-                raise Warning(_('Configuration error!\nThe Parent\
-                Stock Location must belong to the same Operating Unit.'))
+        for rec in self:
+            if (rec.location_id and rec.location_id.usage == 'internal'
+                and rec.operating_unit_id and rec.operating_unit_id !=
+                    rec.location_id.operating_unit_id):
+                    raise Warning(_('Configuration error!\nThe Parent\
+                    Stock Location must belong to the same Operating Unit.'))
 
 
 class StockPicking(models.Model):
@@ -86,34 +100,36 @@ class StockPicking(models.Model):
                                         self.env['res.users'].
                                         operating_unit_default_get(self._uid))
 
-    @api.one
+    @api.multi
     @api.constrains('operating_unit_id', 'company_id')
     def _check_company_operating_unit(self):
-        if self.company_id and self.operating_unit_id and \
-                self.company_id != self.operating_unit_id.company_id:
-            raise Warning(_('Configuration error!\nThe Company in the\
-            Stock Picking and in the Operating Unit must be the same.'))
+        for rec in self:
+            if rec.company_id and rec.operating_unit_id and \
+                    rec.company_id != rec.operating_unit_id.company_id:
+                raise Warning(_('Configuration error!\nThe Company in the\
+                Stock Picking and in the Operating Unit must be the same.'))
 
-    @api.one
+    @api.multi
     @api.constrains('operating_unit_id', 'move_lines')
     def _check_stock_move_operating_unit(self):
-        if not self.operating_unit_id:
-            return True
-        operating_unit = self.operating_unit_id
-        for sm in self.move_lines:
-            if (
-                sm.location_id and
-                sm.location_id.operating_unit_id and
-                operating_unit != sm.location_id.operating_unit_id
-            ) and (
-                sm.location_dest_id and
-                sm.location_dest_id.operating_unit_id and
-                operating_unit !=
-                    sm.location_dest_id.operating_unit_id
-            ):
-                raise Warning(_('Configuration error!\nThe Stock moves\
-                must be related to a location (source or destination)\
-                that belongs to the requesting Operating Unit.'))
+        for rec in self:
+            if not rec.operating_unit_id:
+                return True
+            operating_unit = rec.operating_unit_id
+            for sm in rec.move_lines:
+                if (
+                    sm.location_id and
+                    sm.location_id.operating_unit_id and
+                    operating_unit != sm.location_id.operating_unit_id
+                ) and (
+                    sm.location_dest_id and
+                    sm.location_dest_id.operating_unit_id and
+                    operating_unit !=
+                        sm.location_dest_id.operating_unit_id
+                ):
+                    raise Warning(_('Configuration error!\nThe Stock moves\
+                    must be related to a location (source or destination)\
+                    that belongs to the requesting Operating Unit.'))
 
 
 class StockMove(models.Model):
