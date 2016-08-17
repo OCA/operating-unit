6560412565873f4ae1561bee326ec7ea4de5fbb8
aheficent <ahenriquez@eficent.com>
2016-08-17 18:10:07 +0200

    [FIX] Do not assert_balance until all journal items exist. Fix search view in report. Other minor fixes.

--- account_operating_unit/__init__.py
+++ account_operating_unit/__init__.py
@@ -3,5 +3,7 @@
 # - Jordi Ballester Alomar
 # © 2016 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 from . import models
 from . import wizard
+from . import report
