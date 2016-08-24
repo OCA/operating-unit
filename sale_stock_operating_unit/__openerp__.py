ed7597be666110aee9ed159689e597c6942b8239
aheficent <ahenriquez@eficent.com>
2016-08-24 11:14:24 +0200

    Migrate to v9

--- sale_stock_operating_unit/__openerp__.py
+++ sale_stock_operating_unit/__openerp__.py
@@ -2,15 +2,16 @@
 # © 2015 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in Sales Stock",
     "summary": "An operating unit (OU) is an organizational entity part of a "
                "company",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Eficent, Serpent Consulting Services Pvt. Ltd., "
               "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
     "website": "http://www.eficent.com",
     "category": "Sales Management",
     "depends": [
@@ -18,13 +19,5 @@
         "sale_operating_unit",
         "stock_operating_unit",
     ],
-    "description": """
-Operating Unit in Sales Stock
-=============================
-This module prevents a user from selecting a Warehouse in the Sale Shop
-that does not belong to the same operating unit.
-    """,
-    "data": [],
     'installable': True,
-    'active': False,
 }
