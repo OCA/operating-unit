71a0630eaa3c3f94c573c7048114dd81761b6bc7
SerpentCS <support@serpentcs.com>
2016-08-24 11:49:44 +0530

    migrated hr-expense-operating-unit to 9.0

---
+++ hr_expense_operating_unit/models/hr_expense.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from openerp import api, fields, models
+
+
+class HrExpenseExpense(models.Model):
+
+    _inherit = 'hr.expense'
+
+    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
+                                        default=lambda self:
+                                        self.env['res.users'].
+                                        operating_unit_default_get(self._uid))
