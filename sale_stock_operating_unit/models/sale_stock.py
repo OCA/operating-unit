f471ec7f234b08963fae8a3b207df713e38d0eb2
aheficent <ahenriquez@eficent.com>
2016-08-24 11:14:24 +0200

    Migrate to v9

--- sale_stock_operating_unit/models/sale_stock.py
+++ sale_stock_operating_unit/models/sale_stock.py
@@ -2,8 +2,9 @@
 # © 2015 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from openerp import api, models
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import api, models, _
+from openerp.exceptions import ValidationError
 
 
 class StockMove(models.Model):
@@ -22,3 +23,47 @@ class StockMove(models.Model):
                 sale_line.order_id.operating_unit_id.id
             })
         return values
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange('team_id')
+    def onchange_team_id(self):
+        super(SaleOrder, self).onchange_team_id()
+        if self.team_id and self.team_id.operating_unit_id:
+            warehouses = self.env['stock.warehouse'].search(
+                [('operating_unit_id', '=',
+                  self.team_id.operating_unit_id.id)])
+            if warehouses:
+                self.warehouse_id = warehouses[0]
+
+    @api.onchange('operating_unit_id')
+    def onchange_operating_unit_id(self):
+        if self.operating_unit_id:
+            warehouses = self.env['stock.warehouse'].search(
+                [('operating_unit_id', '=',
+                  self.operating_unit_id.id)])
+            if warehouses:
+                self.warehouse_id = warehouses[0]
+            if self.team_id and self.team_id.operating_unit_id != \
+                    self.operating_unit_id:
+                self.team_id = False
+
+    @api.onchange('warehouse_id')
+    def onchange_warehouse_id(self):
+        if self.warehouse_id:
+            self.operating_unit_id = self.warehouse_id.operating_unit_id
+            if self.team_id and self.team_id.operating_unit_id != \
+                    self.operating_unit_id:
+                self.team_id = False
+
+    @api.multi
+    @api.constrains('operating_unit_id', 'warehouse_id')
+    def _check_wh_operating_unit(self):
+        for rec in self:
+            if rec.operating_unit_id and rec.operating_unit_id != \
+                    rec.warehouse_id.operating_unit_id:
+                raise ValidationError(_('Configuration error!\nThe Operating'
+                                        'Unit in the Sales Order and in the'
+                                        ' Warehouse must be the same.'))
--- sale_stock_operating_unit/tests/__init__.py
+++ sale_stock_operating_unit/tests/__init__.py
@@ -2,5 +2,5 @@
 # © 2015 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import test_sale_stock_operating_unit
