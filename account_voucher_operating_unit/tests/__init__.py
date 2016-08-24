f7cc48d1f8f9eee14473cf761e7f930abbf1b1aa
aheficent <ahenriquez@eficent.com>
2016-08-24 17:49:57 +0200

    license from agpl to lgpl

--- account_voucher_operating_unit/tests/__init__.py
+++ account_voucher_operating_unit/tests/__init__.py
@@ -2,7 +2,7 @@
 # © 2015 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # © 2015 Ecosoft Co. Ltd. - Kitti Upariphutthiphong
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import test_account_voucher_operating_unit
 from . import test_payment_operating_unit
