dea5da3deff3c52f7b9f2e57bb39f90803482c36
jbeficent <jordi.ballester@eficent.com>
2016-09-09 19:00:34 +0200

    [IMP] Add the operating units in the financial reports qweb

--- account_operating_unit/wizard/account_financial_report.py
+++ account_operating_unit/wizard/account_financial_report.py
@@ -30,3 +30,8 @@ class AccountingReport(models.TransientModel):
                                        and data['form']['operating_unit_ids'] \
                                        or False
         return result
+
+    def _print_report(self, data):
+        operating_units = ', '.join([ou.name for ou in self.operating_unit_ids])
+        data['form'].update({'operating_units': operating_units})
+        return super(AccountingReport, self)._print_report(data)
