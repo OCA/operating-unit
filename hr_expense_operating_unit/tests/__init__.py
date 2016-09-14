c320e5aa33c8107cd271ba5b73146194fd51e8de
SerpentCS <support@serpentcs.com>
2016-09-14 11:40:34 +0530

    Added Test Cases.

--- hr_expense_operating_unit/tests/__init__.py
+++ hr_expense_operating_unit/tests/__init__.py
@@ -3,5 +3,4 @@
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from . import models
-from . import tests
+from . import test_hr_expense_operating_unit
