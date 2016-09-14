40678b0a8962d310e3a52e9ee842156c8e36beb7
jbeficent <jordi.ballester@eficent.com>
2016-09-14 17:54:22 +0200

    [IMP] simplify headers

--- account_operating_unit/report/__init__.py
+++ account_operating_unit/report/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 # © 2016 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
-# © 2016 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
+# © 2016 Serpent Consulting Services Pvt. Ltd.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import account_invoice_report
