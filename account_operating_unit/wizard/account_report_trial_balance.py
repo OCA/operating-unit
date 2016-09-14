2dc3a9665bd4c0e3257c78e4f0f2b153bba1e109
jbeficent <jordi.ballester@eficent.com>
2016-09-14 17:54:22 +0200

    [IMP] simplify headers

--- account_operating_unit/wizard/account_report_trial_balance.py
+++ account_operating_unit/wizard/account_report_trial_balance.py
@@ -2,7 +2,6 @@
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
 from openerp import fields, models
 
 
