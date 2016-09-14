478da11ee5bfad345a727e1bc64943853b69e7a3
jbeficent <jordi.ballester@eficent.com>
2016-09-14 17:54:22 +0200

    [IMP] simplify headers

--- account_operating_unit/tests/__init__.py
+++ account_operating_unit/tests/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
-# © 2016 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import test_account_operating_unit
 from . import test_invoice_operating_unit
