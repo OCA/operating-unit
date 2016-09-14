9eba3758311616bd57119f55185c5a5df3cad37b
jbeficent <jordi.ballester@eficent.com>
2016-09-14 17:41:20 +0200

    [IMP] various additions, including: * Add management of payments using operating units * Trial Balance by operating units * Improve tests

---
+++ account_operating_unit/models/account_journal.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016 Serpent Consulting Services Pvt. Ltd.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    operating_unit_id = fields.Many2one(comodel_name='operating.unit',
+                                        string='Operating Unit',
+                                        help="Operating Unit that will be "
+                                             "used in payments, when this "
+                                             "journal is used.")
+
+    @api.multi
+    @api.constrains('type')
+    def _check_ou(self):
+        for journal in self:
+            if journal.type in ('bank', 'cash') \
+                    and journal.company_id.ou_is_self_balanced \
+                    and not journal.operating_unit_id:
+                raise UserError(_('Configuration error!\nThe operating unit '
+                                  'must be indicated in bank journals, '
+                                  'if it has been defined as self-balanced '
+                                  'at company level.'))
