9571de9aeaa778b960a1057fdcab8c6c0f02154e
ahenriquez <ahenriquez@eficent.com>
2016-09-19 19:02:47 +0200

    migrate code to v9

--- account_voucher_operating_unit/models/account_voucher.py
+++ account_voucher_operating_unit/models/account_voucher.py
@@ -5,7 +5,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from openerp import api, fields, models, _
-from openerp.exceptions import Warning
+from openerp.exceptions import ValidationError
 
 
 class AccountVoucher(models.Model):
@@ -13,87 +13,71 @@ class AccountVoucher(models.Model):
 
     @api.multi
     def _get_default_operating_unit(self):
-        journal = self._get_journal()
+        journal = self._default_journal()
         if isinstance(journal, int):
             journal = self.env['account.journal'].browse(journal)
-        ttype = self.env.context.get('type', False)
-        if ttype in ('payment', 'receipt'):
+        ttype = self.env.context.get('voucher_type', False)
+        if ttype in ('sale', 'purchase'):
             return journal.default_debit_account_id.operating_unit_id.id
-
-    @api.multi
-    def onchange_journal(self, journal_id, line_ids, tax_id,
-                         partner_id, date, amount, ttype, company_id):
-        res = super(AccountVoucher, self).onchange_journal(
-            journal_id, line_ids, tax_id, partner_id, date, amount, ttype,
-            company_id)
-        if journal_id and ttype in ('payment', 'receipt'):
-            journal = self.env['account.journal'].browse(journal_id)
-            res['value']['operating_unit_id'] = \
-                journal.default_debit_account_id.operating_unit_id.id
-            res['value']['writeoff_operating_unit_id'] = \
-                journal.default_debit_account_id.operating_unit_id.id
-        return res
 
     operating_unit_id = fields.Many2one(
         'operating.unit',
         string='Operating Unit',
         default=_get_default_operating_unit,
     )
-    writeoff_operating_unit_id = fields.Many2one(
-        'operating.unit',
-        string='Write-off Operating Unit',
-        default=_get_default_operating_unit,
-        required=False,
-    )
 
-    @api.one
+    @api.multi
     @api.constrains('operating_unit_id', 'company_id')
     def _check_company_operating_unit(self):
-        if self.company_id and self.operating_unit_id and \
-                self.company_id != self.operating_unit_id.company_id:
-            raise Warning(_('The Company in the Move Line and in the '
-                            'Operating Unit must be the same.'))
+        for rec in self:
+            if rec.company_id and rec.operating_unit_id and \
+                    rec.company_id != rec.operating_unit_id.company_id:
+                raise ValidationError(_('The Company in the Move Line and in '
+                                        'the Operating Unit must be the same.'
+                                        ))
 
-    @api.one
-    @api.constrains('operating_unit_id', 'journal_id', 'type')
+    @api.multi
+    @api.constrains('operating_unit_id', 'journal_id', 'voucher_type')
     def _check_journal_account_operating_unit(self):
-        if self.type not in ('payment', 'receipt'):
-            return True
-        if (
-            self.journal_id and self.operating_unit_id and
-            self.journal_id.default_debit_account_id and
-            self.journal_id.default_debit_account_id.operating_unit_id and
-            self.journal_id.default_debit_account_id.operating_unit_id.id !=
-                self.operating_unit_id.id
-        ) or (
-            self.journal_id and self.operating_unit_id and
-            self.journal_id.default_credit_account_id and
-            self.journal_id.default_credit_account_id.operating_unit_id and
-            self.journal_id.default_credit_account_id.operating_unit_id.id !=
-                self.operating_unit_id.id
-        ):
-            raise Warning(_('The Default Debit and Credit Accounts '
-                            'defined in the Journal must have the same '
-                            'Operating Unit as the one indicated in the '
-                            'payment or receipt.'))
+        for rec in self:
+            if rec.voucher_type not in ('purchase', 'sale'):
+                return True
+            if (
+                rec.journal_id and rec.operating_unit_id and
+                rec.journal_id.default_debit_account_id and
+                rec.journal_id.default_debit_account_id.operating_unit_id and
+                rec.journal_id.default_debit_account_id.operating_unit_id.id !=
+                    rec.operating_unit_id.id
+            ) or (
+                rec.journal_id and rec.operating_unit_id and
+                rec.journal_id.default_credit_account_id and
+                rec.journal_id.default_credit_account_id.operating_unit_id and
+                rec.journal_id.default_credit_account_id.operating_unit_id.id !=
+                    rec.operating_unit_id.id
+            ):
+                raise ValidationError(_('The Default Debit and Credit Accounts'
+                                        ' defined in the Journal must have the'
+                                        ' same Operating Unit as the one '
+                                        'indicated in the payment or receipt.'
+                                        ))
         return True
 
-    @api.model
-    def first_move_line_get(self, voucher_id, move_id,
-                            company_currency, current_currency):
+    @api.multi
+    def first_move_line_get(self, move_id, company_currency, current_currency):
+        self.ensure_one()
         res = super(AccountVoucher, self).first_move_line_get(
-            voucher_id, move_id, company_currency, current_currency)
-        voucher = self.env['account.voucher'].browse(voucher_id)
+            move_id, company_currency, current_currency)
+        voucher = self
         if not voucher.operating_unit_id:
             return res
-        if voucher.type in ('payment', 'receipt'):
+        if voucher.voucher_type == 'purchase':
             if voucher.account_id.operating_unit_id:
                 res['operating_unit_id'] = \
                     voucher.account_id.operating_unit_id.id
             else:
-                raise Warning(_('Account %s - %s does not have a '
+                raise ValidationError(_('Account %s - %s does not have a '
                                 'default operating unit. \n '
-                                'Payment Method %s default Debit and '
+                                'Journal %s default Debit and '
                                 'Credit accounts should have a '
                                 'default Operating Unit.') %
                               (voucher.account_id.code,
@@ -103,71 +87,11 @@ class AccountVoucher(models.Model):
             if voucher.operating_unit_id:
                 res['operating_unit_id'] = voucher.operating_unit_id.id
             else:
-                raise Warning(_('The Voucher must have an Operating '
+                raise ValidationError(_('The Voucher must have an Operating '
                                 'Unit.'))
         return res
 
-    @api.model
-    def _voucher_move_line_prepare(self, voucher_id, line_total,
-                                   move_id, company_currency, current_currency,
-                                   voucher_line_id, ):
-        res = super(AccountVoucher, self)._voucher_move_line_prepare(
-            voucher_id, line_total, move_id, company_currency,
-            current_currency, voucher_line_id)
-        line = self.env['account.voucher.line'].browse(voucher_line_id)
 
-        if line.voucher_id.type in ('sale', 'purchase') \
-                and line.voucher_id.operating_unit_id:
-            res['operating_unit_id'] = line.voucher_id.operating_unit_id.id
-        elif line.move_line_id and line.move_line_id.operating_unit_id:
-            res['operating_unit_id'] = line.move_line_id.operating_unit_id.id
-        return res
-
-    @api.model
-    def _voucher_move_line_foreign_currency_prepare(
-            self, voucher_id, line_total, move_id,
-            company_currency, current_currency, voucher_line_id,
-            foreign_currency_diff):
-
-        res = super(AccountVoucher, self)._voucher_move_line_prepare(
-            voucher_id, line_total, move_id, company_currency,
-            current_currency, voucher_line_id, foreign_currency_diff)
-        line = self.env['account.voucher.line'].browse(voucher_line_id)
-        if line.move_line_id and line.move_line_id.operating_unit_id:
-            res['operating_unit_id'] = line.move_line_id.operating_unit_id.id
-        return res
-
-    @api.model
-    def writeoff_move_line_get(self, voucher_id,
-                               line_total, move_id, name,
-                               company_currency, current_currency):
-        res = super(AccountVoucher, self).writeoff_move_line_get(
-            voucher_id, line_total, move_id, name, company_currency,
-            current_currency)
-        if res:
-            voucher = self.env['account.voucher'].browse(voucher_id)
-            if (voucher.payment_option == 'with_writeoff' or
-                    voucher.partner_id):
-                if not voucher.writeoff_operating_unit_id:
-                    raise Warning(_('Please indicate a write-off Operating '
-                                    'Unit.'))
-                else:
-                    res['operating_unit_id'] = \
-                        voucher.writeoff_operating_unit_id.id
-            else:
-                if not voucher.writeoff_operating_unit_id:
-                    if not voucher.account_id.operating_unit_id:
-                        raise Warning(_('Please indicate a write-off '
-                                        'Operating Unit or a default '
-                                        'Operating Unit for account %s') %
-                                      voucher.account_id.code)
-                    else:
-                        res['operating_unit_id'] = \
-                            voucher.account_id.operating_unit_id.id
-                else:
-                        res['operating_unit_id'] = \
-                            voucher.writeoff_operating_unit_id.id
-        return res
 
 
 class AccountVoucherLine(models.Model):
@@ -179,3 +103,11 @@ class AccountVoucherLine(models.Model):
         string='Operating Unit', readonly=True,
         store=True,
     )
+
+    @api.model
+    def create(self, vals):
+        if not 'operating_unit_id' in vals:
+            voucher= self.env['account.voucher'].browse(vals['voucher_id'])
+            if voucher.operating_unit_id:
+                vals['operating_unit_id'] = voucher.operating_unit_id.id
+        return super(AccountVoucherLine, self).create(vals)
