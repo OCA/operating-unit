bc93d31905138b51c0306cb3b88aee69aeb57e5e
SerpentCS <support@serpentcs.com>
2016-09-23 18:59:24 +0530

    Added test cases for mrp.

--- mrp_operating_unit/tests/__init__.py
+++ mrp_operating_unit/tests/__init__.py
@@ -4,3 +4,4 @@
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import test_procurement
+from . import test_mrp_operating_unit
