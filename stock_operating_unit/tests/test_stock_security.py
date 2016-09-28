dc96023109dfb04a3c673a093b90af8430485c0a
jbeficent <jordi.ballester@eficent.com>
2016-09-28 14:17:11 +0200

    flake 8

--- stock_operating_unit/tests/test_stock_security.py
+++ stock_operating_unit/tests/test_stock_security.py
@@ -57,8 +57,8 @@ class TestStockPicking(test_stock_ou.TestStockOperatingUnit):
         move_ids =\
             self.MoveObj.sudo(self.user2_id).\
             search([('picking_id', '=', self.picking_in1.id)]).ids
-        self.assertEqual(move_ids, [], 'User 2 should not be able to list'
-                        'the stock moves assigned to picking 1.')
+        self.assertEqual(move_ids, [], 'User 2 should not be able to list the '
+                                       'stock moves assigned to picking 1.')
         # User 2 cannot list the pickings 1
         picking_ids =\
             self.PickingObj.sudo(self.user2_id).\
