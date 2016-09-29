ca99988145c4c1ae08c7024b6c99ef88f6ff1dd6
SerpentCS <support@serpentcs.com>
2016-09-29 13:37:45 +0530

    added sale_crm_operating_unit module

---
+++ sale_crm_operating_unit/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale CRM Operating Unit",
+    "version": "9.0.1.0.0",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org/",
+    "category": "Hidden",
+    "depends": ["sale_crm",
+                "crm_operating_unit",
+                "sale_operating_unit"],
+    "data": ['views/sale_crm_view.xml'],
+    "license": "AGPL-3",
+    'installable': True,
+}
