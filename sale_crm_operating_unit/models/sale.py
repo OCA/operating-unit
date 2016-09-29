0099ee4793c99c5f81df0859bc1a3b255b3b37ea
SerpentCS <support@serpentcs.com>
2016-09-29 13:37:45 +0530

    added sale_crm_operating_unit module

---
+++ sale_crm_operating_unit/models/sale.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from openerp import api, models
+from openerp.exceptions import Warning
+from openerp.tools.translate import _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.constrains('opportunity_id', 'operating_unit_id')
+    def _check_sale_operating_unit(self):
+        if self.opportunity_id and self.opportunity_id.operating_unit_id and\
+                self.opportunity_id.operating_unit_id != self.operating_unit_id:
+            raise Warning(_('Configuration error!\nThe Operating Unit\
+            of Sales Order should be same that of Opportunity.'))
