320a4ea379552ec832ec0cdc9181f56e72efd63a
jbeficent <jordi.ballester@eficent.com>
2016-10-06 08:39:32 +0200

    [FIX] add sales team as dependency of crm_operating_unit

--- crm_operating_unit/__openerp__.py
+++ crm_operating_unit/__openerp__.py
@@ -13,7 +13,7 @@
     "license": "LGPL-3",
     "website": "http://www.eficent.com",
     "category": "Sales",
-    "depends": ["crm", "operating_unit"],
+    "depends": ["crm", "sales_team_operating_unit"],
     "data": [
         "views/crm_lead_view.xml",
         "security/crm_security.xml",
