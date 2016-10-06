d967c987da1bc87a43aed5451d4280d750dc3fc0
jbeficent <jordi.ballester@eficent.com>
2016-10-06 10:31:48 +0200

    create PO from procurement

--- purchase_operating_unit/__openerp__.py
+++ purchase_operating_unit/__openerp__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in Purchase Orders",
-    "summary": "An operating unit (OU) is an organizational entity part of a\
-        company",
+    "summary": "An operating unit (OU) is an organizational entity part of a "
+               "company",
     "version": "9.0.1.0.0",
-    "author": "Eficent Business and IT Consulting Services S.L., "
+    "author": "Eficent, "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
