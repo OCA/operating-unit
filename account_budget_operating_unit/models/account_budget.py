cb22f481e5b626c99c4704bab381beac5a10b4e8
SerpentCS <support@serpentcs.com>
2016-10-06 19:21:44 +0530

    [ADD] Migrated to v9 and added test cases

---
+++ account_budget_operating_unit/models/account_budget.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Ecosoft Co. Ltd. - Kitti Upariphutthiphong
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models, _
+from openerp.exceptions import Warning
+
+
+class CrossoveredBudget(models.Model):
+    _inherit = "crossovered.budget"
+
+    operating_unit_id = fields.Many2one(
+        'operating.unit',
+        string='Operating Unit',
+        default=lambda self: self.env['res.users'].
+        operating_unit_default_get(self._uid),
+    )
+
+    @api.one
+    @api.constrains('operating_unit_id', 'company_id')
+    def _check_company_operating_unit(self):
+        if self.company_id and self.operating_unit_id and \
+                self.company_id != self.operating_unit_id.company_id:
+            raise Warning(_('The Company in the Move Line and in the '
+                            'Operating Unit must be the same.'))
+
+
+class CrossoveredBudgetLines(models.Model):
+    _inherit = "crossovered.budget.lines"
+
+    operating_unit_id = fields.Many2one(
+        'operating.unit',
+        related='crossovered_budget_id.operating_unit_id',
+        string='Operating Unit', readonly=True, store=True,
+    )
---
+++ account_budget_operating_unit/tests/test_budget_security.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2015 Ecosoft Co. Ltd. - Kitti Upariphutthiphong
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp.addons.account_budget_operating_unit.tests import\
+    test_budget_operating_unit as test_budget_ou
+
+
+class TestBudgetSecurity(test_budget_ou.TestBudgetOperatingUnit):
+
+    def test_budget_ou_security(self):
+        """Test Security of Budget Operating Unit"""
+        # User 1 can list the budget assigned to
+        # Main and B2C OU
+        budget_ids =\
+            self.BudgetObj.sudo(self.user1_id).\
+            search([('operating_unit_id', 'in',
+                     [self.ou1.id, self.b2c.id])]).ids
+        self.assertNotEqual(budget_ids, [], 'User1 should have access to'
+                            'Budget which belong to Main and B2C'
+                            'Operating Unit.')
+        # User 2 cannot list the budget assigned to Main OU
+        budget_ids =\
+            self.BudgetObj.sudo(self.user2_id).\
+            search([('operating_unit_id', '=', self.ou1.id)]).ids
+        self.assertEqual(budget_ids, [], 'User 2 should not be able to list'
+                         'the budget assigned to Main Operating Unit.')
