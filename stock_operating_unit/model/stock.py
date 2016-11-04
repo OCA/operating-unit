edfae40d964178a17002afd901ec6befed119726
ahenriquez <ahenriquez@eficent.com>
2016-11-04 10:56:58 +0100

    travis issues and test stock picking

--- stock_operating_unit/model/stock.py
+++ stock_operating_unit/model/stock.py
@@ -152,16 +152,16 @@ class StockMove(models.Model):
             operating_unit_dest = stock_move.operating_unit_dest_id
             if (
                 stock_move.location_id and
-                    stock_move.location_id.operating_unit_id and
-                        stock_move.picking_id and
-                            operating_unit != stock_move.picking_id.
-                                operating_unit_id
+                stock_move.location_id.operating_unit_id and
+                stock_move.picking_id and
+                operating_unit !=
+                stock_move.picking_id.operating_unit_id
             ) and (
                     stock_move.location_dest_id and
-                        stock_move.location_dest_id.operating_unit_id and
-                            stock_move.picking_id and
-                                operating_unit_dest !=
-                                    stock_move.picking_id.operating_unit_id
+                    stock_move.location_dest_id.operating_unit_id and
+                    stock_move.picking_id and
+                    operating_unit_dest !=
+                    stock_move.picking_id.operating_unit_id
             ):
                 raise UserError(_('Configuration error!\nThe Stock moves\
                 must be related to a location (source or destination)\
