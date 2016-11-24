7b29518d5537f994068919cd22454242bfdc7f6f
ahenriquez <ahenriquez@eficent.com>
2016-11-24 17:48:56 +0100

    [IMP] minor changes

--- purchase_requisition_operating_unit/tests/test_purchase_requisition_operating_unit.py
+++ purchase_requisition_operating_unit/tests/test_purchase_requisition_operating_unit.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from openerp.tests import common
 from openerp.exceptions import ValidationError
