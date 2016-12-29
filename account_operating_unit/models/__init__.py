c8c33230ed8c3600fe5b5b1c6ea9887d6b7f3675
ougc27 <oscar_garzacordova@hotmail.com>
2016-12-29 13:48:20 -0600

    [ADD][account_bank_statement] - Operating Units in bank statements.

--- account_operating_unit/models/__init__.py
+++ account_operating_unit/models/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # © 2016-17 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
+# © 2016 Jarsa Sistemas S.A. de C.V..
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import company
+from . import account_bank_statement
 from . import account_journal
 from . import account_move
 from . import invoice
