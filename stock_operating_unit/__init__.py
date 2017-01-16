902e962b94cfc68700132f880968f331ac7462e7
ahenriquez <ahenriquez@eficent.com>
2017-01-16 17:47:41 +0100

    Update operating units of previous locations

--- stock_operating_unit/__init__.py
+++ stock_operating_unit/__init__.py
@@ -3,3 +3,4 @@
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import model
+from .hooks import update_operating_unit_location
