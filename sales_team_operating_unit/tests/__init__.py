aefd26d3038a7c607576c113c0db043782511450
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2017-01-23 10:02:23 -0700

    sales_team_operating_unit (#35)
    * [ADD] sales_team_operating_unit

---
+++ sales_team_operating_unit/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import test_crm_team_operating_unit
