56c5d71e3489509f2f2e3962d03c00bf8ea8c62e
lreficent <lois.rilo@eficent.com>
2017-01-26 14:25:54 +0100

    [MIG] crm_operating_unit to v10.0
    various fixes

--- crm_operating_unit/models/crm_lead.py
+++ crm_operating_unit/models/crm_lead.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
+# © 2015-17 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp import fields, models
+from odoo import api, fields, models
 
 
 class CRMLead(models.Model):
 
     _inherit = 'crm.lead'
 
+    @api.model
+    def _get_default_operating_unit(self):
+        team = self.env['crm.team']._get_default_team_id()
+        if team.operating_unit_id:
+            return team.operating_unit_id
+        else:
+            return self.env['res.users'].operating_unit_default_get(self._uid)
+
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
-                                        default=lambda self:
-                                        self.env['res.users'].
-                                        operating_unit_default_get(self._uid))
+                                        related='team_id.operating_unit_id',
+                                        default=_get_default_operating_unit)
