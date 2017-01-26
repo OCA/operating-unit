f2b15dd88b1c55974bf73e3b6467463572cf4f17
mreficent <miquel.raich@eficent.com>
2017-01-26 18:41:43 +0100

    [MIG] operating_unit to v10.0 (#45)
    * [MIG] operating_unit to v10.0

--- operating_unit/models/operating_unit.py
+++ operating_unit/models/operating_unit.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# © 2015-2017 Eficent
 # - Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2015-2017 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class OperatingUnit(models.Model):
