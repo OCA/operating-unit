c096152e0ebb4e5b78243acfba8e8be24f541df0
lreficent <lois.rilo@eficent.com>
2017-01-26 14:21:05 +0100

    [MIG] sales_team_operating_unit to v10.0

--- sales_team_operating_unit/tests/test_crm_team_operating_unit.py
+++ sales_team_operating_unit/tests/test_crm_team_operating_unit.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016-17 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from openerp.tests import common
