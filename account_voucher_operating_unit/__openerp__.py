c98fddd362f7a94cecfa6dd95bc65f43355adb93
lreficent <lois.rilo@eficent.com>
2017-01-26 14:13:20 +0100

    [MIG] account_voucher_operating_unit to v10.0

--- account_voucher_operating_unit/__openerp__.py
+++ account_voucher_operating_unit/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # © 2015 Ecosoft Co. Ltd. - Kitti Upariphutthiphong
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Vouchers with Operating Units',
     'summary': 'Introduces the operating unit to vouchers',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Generic Modules/Sales & Purchases',
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Ecosoft Co. Ltd.,"
