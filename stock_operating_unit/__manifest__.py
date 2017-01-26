1a31c24b44fe13c4eb5b55b8d5f22b9bdb506861
ahenriquez <ahenriquez@eficent.com>
2017-01-26 14:51:00 +0100

    [MIG] stock_operating_unit to v10.0

--- stock_operating_unit/__manifest__.py
+++ stock_operating_unit/__manifest__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L.
-# © 2016 Serpent Consulting Services Pvt. Ltd.
+# © 2016-2017 Eficent Business and IT Consulting Services S.L.
+# © 2016-2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Stock with Operating Units",
     "summary": "An operating unit (OU) is an organizational entity part of a "
                "company",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Generic Modules/Sales & Purchases",
-    "author": "Eficent, Serpent Consulting Services Pvt. Ltd., "
+    "author": "Eficent, "
+              "Serpent Consulting Services Pvt. Ltd., "
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "http://www.eficent.com",
--- stock_operating_unit/tests/test_stock_operating_unit.py
+++ stock_operating_unit/tests/test_stock_operating_unit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L.
-# © 2016 Serpent Consulting Services Pvt. Ltd.
+# © 2016-2017 Eficent Business and IT Consulting Services S.L.
+# © 2016-2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from openerp.addons.stock.tests import common
 
