d955763ed88b5e521da022664ab71e8ce0fd3968
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-01-27 17:09:36 +0530

    [IMP] test cases and other changes

--- sales_team_operating_unit/tests/__init__.py
+++ sales_team_operating_unit/tests/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2016-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import test_crm_team_operating_unit
