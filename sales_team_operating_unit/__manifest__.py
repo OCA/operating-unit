ff29c37a491782e05854928bd4e9a52b3109876f
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-01-27 17:09:36 +0530

    test cases and other changes

--- sales_team_operating_unit/__manifest__.py
+++ sales_team_operating_unit/__manifest__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-# © 2016-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
 {
     "name": "Sales Team Operating Unit",
     "version": "10.0.1.0.0",
     "author": "Eficent, "
-              "Serpent Consulting Services Pvt. Ltd.,"
+              "SerpentCS,"
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    "website": "http://www.eficent.com",
+    "website": "https://github.com/OCA/operating-unit",
     "category": "Sales",
     "depends": ["sales_team", "operating_unit"],
     "data": [
