fed3a9605857d508c2d418b1bdbeb6d0cfd42776
mreficent <miquel.raich@eficent.com>
2017-01-27 14:38:42 +0100

    [FIX] ... when cash basis is installed

--- account_operating_unit/tests/test_payment_operating_unit.py
+++ account_operating_unit/tests/test_payment_operating_unit.py
@@ -2,7 +2,7 @@
 # © 2016-17 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp.addons.account_operating_unit.tests import\
+from odoo.addons.account_operating_unit.tests import\
     test_account_operating_unit as test_ou
 import time
 
