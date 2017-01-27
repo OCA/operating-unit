8bb86f174c6fa7b50c2cf6b3f87b28078053310b
mreficent <miquel.raich@eficent.com>
2017-01-27 12:20:11 +0100

    [MIG] account_voucher_operating_unit to v10.0

--- account_voucher_operating_unit/README.rst
+++ account_voucher_operating_unit/README.rst
@@ -21,7 +21,7 @@ Usage
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/213/9.0
+   :target: https://runbot.odoo-community.org/runbot/213/10.0
 
 Bug Tracker
 ===========
@@ -30,7 +30,6 @@ Bugs are tracked on `GitHub Issues
 <https://github.com/OCA/operating-unit/issues>`_. In case of trouble, please
 check there if your issue has already been reported. If you spotted it first,
 help us smashing it by providing a detailed and welcomed feedback.
-
 
 Credits
 =======
--- account_voucher_operating_unit/__manifest__.py
+++ account_voucher_operating_unit/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# © 2015-17 Eficent
 # - Jordi Ballester Alomar
 # © 2015 Ecosoft Co. Ltd. - Kitti Upariphutthiphong
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
@@ -9,7 +9,7 @@
     'summary': 'Introduces the operating unit to vouchers',
     'version': '10.0.1.0.0',
     'category': 'Generic Modules/Sales & Purchases',
-    "author": "Eficent Business and IT Consulting Services S.L., "
+    "author": "Eficent, "
               "Ecosoft Co. Ltd.,"
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
