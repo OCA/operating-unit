ecf37d3a2e220ab65f3c7791ced7e88b06f01645
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2017-01-30 11:44:07 +0100

    [10.0][account_operating_unit]Add support for cash basis. Migrate to v10 nomenclature (#55)
    * Add support for cash basis. Migrate to v10 nomenclature
    
    * Remove apply_taxes from the create method, as in v10 this parameter is not accepted in the created method

--- account_operating_unit/wizard/account_report_trial_balance.py
+++ account_operating_unit/wizard/account_report_trial_balance.py
@@ -2,7 +2,7 @@
 # © 2016-17 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountBalanceReport(models.TransientModel):
