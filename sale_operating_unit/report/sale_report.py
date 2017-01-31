cc40331655ba283cd27dfd2f690f134b616b8fa0
ahenriquez <ahenriquez@eficent.com>
2017-01-31 12:27:40 +0100

    [MIG] sale_operating_unit: Migrated to 10.0

--- sale_operating_unit/report/sale_report.py
+++ sale_operating_unit/report/sale_report.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class SaleReport(models.Model):
