45235a23b606537166545b82d07b670a113508ad
ahenriquez <ahenriquez@eficent.com>
2017-01-31 10:02:04 +0100

    [MIG] purchase_operating_unit: Migrated to 10.0

--- purchase_operating_unit/tests/test_po_security.py
+++ purchase_operating_unit/tests/test_po_security.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp.addons.purchase_operating_unit.tests import\
+from odoo.addons.purchase_operating_unit.tests import\
     test_purchase_operating_unit as test_po_ou
 
 
