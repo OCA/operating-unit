6cb35a8af7851b88638e00b86313f571d42e1ce5
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-02-07 16:30:12 +0530

    [MIG] hr_contract_operating_unit to v10

--- hr_contract_operating_unit/models/hr_contract.py
+++ hr_contract_operating_unit/models/hr_contract.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class HrContract(models.Model):
