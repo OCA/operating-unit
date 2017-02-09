4f28f474caea06099fc057fb98c9731ff5d460a0
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-02-09 18:16:08 +0530

    [ADD] manifest

---
+++ hr_payroll_account_operating_unit/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "HR Payroll Account Operating Unit",
+    "version": "10.0.1.0.0",
+    "license": 'LGPL-3',
+    "author": "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
+              "Odoo Community Association (OCA)",
+    "category": "Generic Modules/Human Resources",
+    "website": "http://www.eficent.com",
+    "depends": ["hr_payroll_account", "hr_contract_operating_unit",
+                "account_operating_unit"],
+    'installable': True,
+}
