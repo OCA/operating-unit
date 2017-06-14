81b7b795c288ef2e4798d8224c3f66629f209d41
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-14 15:55:00 +0530

    minor changes

--- account_invoice_merge_operating_unit/models/account_invoice.py
+++ account_invoice_merge_operating_unit/models/account_invoice.py
@@ -10,8 +10,8 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.model
-    def _get_invoice_line_key_cols(self):
-        res = super(AccountInvoice, self)._get_invoice_line_key_cols()
+    def _get_invoice_key_cols(self):
+        res = super(AccountInvoice, self)._get_invoice_key_cols()
         res.append('operating_unit_id')
         return res
 
