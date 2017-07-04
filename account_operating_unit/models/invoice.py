6acfc53b0a8e83f7cda57b9f9764c550525ddd38
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-07-04 14:48:56 +0530

    [FIX] readonly on OU

--- account_operating_unit/models/invoice.py
+++ account_operating_unit/models/invoice.py
@@ -13,7 +13,10 @@ class AccountInvoice(models.Model):
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
                                         default=lambda self:
                                         self.env['res.users'].
-                                        operating_unit_default_get(self._uid))
+                                        operating_unit_default_get(self._uid),
+                                        readonly=True,
+                                        states={'draft': [('readonly',
+                                                           False)]})
 
     @api.multi
     def finalize_invoice_move_lines(self, move_lines):
