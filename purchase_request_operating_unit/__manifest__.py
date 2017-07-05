7b658ca1e4d35d093da82d9fb8cca5435d80da1e
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-07-05 15:56:08 +0530

    [ADD] purchase_request_operating_unit v10

--- purchase_request_operating_unit/__manifest__.py
+++ purchase_request_operating_unit/__manifest__.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent
-# © 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in Purchase Requests",
-    "version": "9.0.1.0.0",
-    "author": "Eficent"
-              "Serpent Consulting Services Pvt. Ltd.,"
+    "version": "10.0.1.0.0",
+    "author": "Eficent, "
+              "SerpentCS, "
               "Odoo Community Association (OCA)",
-    "license": "LGPL-3",
     "website": "http://www.eficent.com",
+    "license": "LGPL-3",
     "category": "Purchase Management",
     "depends": ["purchase_request",
                 "purchase_operating_unit"],
     "data": [
-        "view/purchase_request_view.xml",
         "security/purchase_security.xml",
+        "view/purchase_request_view.xml",
     ],
     'installable': True,
 }
