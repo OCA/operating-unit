dd0a58b387836d2207bbba7981ea977c22e149ae
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-21 15:53:34 +0530

    [MIG] hr_expense_operating_unit v10

--- hr_expense_operating_unit/tests/__init__.py
+++ hr_expense_operating_unit/tests/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 from . import test_hr_expense_operating_unit
--- hr_expense_operating_unit/tests/test_hr_expense_operating_unit.py
+++ hr_expense_operating_unit/tests/test_hr_expense_operating_unit.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp.tests import common
-from openerp.exceptions import AccessError
+
+from odoo.tests import common
+from odoo.exceptions import ValidationError, UserError
 
 
 class TestHrExpenseOperatingUnit(common.TransactionCase):
@@ -13,6 +13,7 @@ class TestHrExpenseOperatingUnit(common.TransactionCase):
         super(TestHrExpenseOperatingUnit, self).setUp()
         self.res_users_model = self.env['res.users']
         self.hr_expense_model = self.env['hr.expense']
+        self.hr_expense_sheet_model = self.env['hr.expense.sheet']
         self.hr_employee_model = self.env['hr.employee']
 
         self.company = self.env.ref('base.main_company')
@@ -22,7 +23,7 @@ class TestHrExpenseOperatingUnit(common.TransactionCase):
         # Expense Product
         self.product1 = self.env.ref('hr_expense.air_ticket')
 
-        self.grp_hr_user = self.env.ref('base.group_hr_user')
+        self.grp_hr_user = self.env.ref('hr.group_hr_user')
         self.grp_accou_mng = self.env.ref('account.group_account_manager')
         self.grp_account_invoice =\
             self.env.ref('account.group_account_invoice')
@@ -80,21 +81,30 @@ class TestHrExpenseOperatingUnit(common.TransactionCase):
 
     def _create_hr_expense(self, operating_unit, emp):
         """Creates Expense for employee."""
-        expense = self.hr_expense_model.create({
-            'name': " Food Expense ",
+        expense_sheet = self.hr_expense_sheet_model.create({
+            'name': "Traveling Expense",
+            'employee_id': emp.id,
+            'operating_unit_id': operating_unit.id
+            })
+        self.hr_expense_model.create({
+            'name': "Traveling Expense",
             'product_id': self.product1.id,
             'operating_unit_id': operating_unit.id,
             'unit_amount': '10.0',
             'quantity': '5',
-            'employee_id': emp.id
+            'employee_id': emp.id,
+            'sheet_id': expense_sheet.id
             })
-        return expense
+        return expense_sheet
 
-    def _post_journal_entries(self, expense):
+    def _post_journal_entries(self, expense_sheet):
         """Approves the Expense and creates accounting entries."""
-        expense.submit_expenses()
-        expense.approve_expenses()
-        expense.action_move_create()
+        expense_sheet.approve_expense_sheets()
+        expense_sheet.action_sheet_move_create()
+        self.assertEqual(
+            expense_sheet.account_move_id.line_ids[0].operating_unit_id,
+            expense_sheet.operating_unit_id
+        )
 
     def test_security(self):
         # User 2 is only assigned to Operating Unit B2C, and cannot
@@ -104,15 +114,43 @@ class TestHrExpenseOperatingUnit(common.TransactionCase):
              ('operating_unit_id', '=', self.ou1.id)])
         self.assertEqual(record.ids, [], 'User 2 should not have access to %s'
                          % self.ou1.name)
-        with self.assertRaises(AccessError):
-            self.hr_expense1.sudo(self.user2.id).unlink()
-        with self.assertRaises(AccessError):
-            self.hr_expense1.sudo(self.user2.id).write({'name': 'new name'})
 
         # Expense OU should have same OU of its accounting entries
-        self.assertEqual(self.hr_expense1.operating_unit_id.id,
-                         self.hr_expense1.account_move_id.operating_unit_id.id,
+        self.assertEqual(self.hr_expense1.expense_line_ids.operating_unit_id,
+                         self.hr_expense1.account_move_id.line_ids.
+                         mapped('operating_unit_id'),
                          "Expense OU should match with accounting entries OU")
-        self.assertEqual(self.hr_expense2.operating_unit_id.id,
-                         self.hr_expense2.account_move_id.operating_unit_id.id,
+        self.assertEqual(self.hr_expense2.expense_line_ids.operating_unit_id,
+                         self.hr_expense2.account_move_id.line_ids.
+                         mapped('operating_unit_id'),
                          "Expense OU should match with accounting entries OU")
+
+    def test_constrains_error(self):
+        with self.assertRaises(ValidationError):
+            self.hr_expense1.write({'operating_unit_id': self.ou1.id})
+            self.hr_expense1.expense_line_ids.write({'operating_unit_id':
+                                                     self.b2c.id})
+
+        self.hr_expense1.expense_line_ids.write({'state': 'draft'})
+        self.hr_expense1.expense_line_ids.submit_expenses()
+
+        company_id = self.env['res.company'].create({
+            'name': 'My Company',
+            'partner_id': self.partner1.id,
+            'rml_header1': 'My Company',
+            'currency_id': self.env.ref('base.EUR').id
+        })
+
+        with self.assertRaises(ValidationError):
+            self.hr_expense1.expense_line_ids.write({'company_id':
+                                                     company_id.id})
+
+        with self.assertRaises(UserError):
+            self.hr_expense1.expense_line_ids.write({
+                'state': 'draft',
+                'operating_unit_id': False
+            })
+            self.hr_expense1.expense_line_ids.submit_expenses()
+
+        with self.assertRaises(ValidationError):
+            self.hr_expense1.write({'company_id': company_id.id})
