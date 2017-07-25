7c92dd5be7c67a9cfa77dcc2fe6a83b1445a5fbe
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-25 16:09:05 +0530

    [MIG] mrp_operating_unit v10

--- mrp_operating_unit/models/procurement.py
+++ mrp_operating_unit/models/procurement.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp import api, fields, models
-from openerp.tools.translate import _
-from openerp.exceptions import Warning
+from odoo import api, models
+from odoo.tools.translate import _
+from odoo.exceptions import ValidationError
 
 
 class ProcurementOrder(models.Model):
@@ -21,14 +20,14 @@ class ProcurementOrder(models.Model):
                 pr.production_id.operating_unit_id !=
                     pr.location_id.operating_unit_id
             ):
-                raise Warning(_('The Production Order and the Procurement\
-                 Order must belong to the same Operating Unit.'))
+                raise ValidationError(_(
+                    'The Production Order and the Procurement Order must '
+                    'belong to the same Operating Unit.'))
         return True
 
     @api.model
-    def _prepare_mo_vals(self, procurement):
-        res = super(ProcurementOrder, self)._prepare_mo_vals(procurement)
-        if procurement.location_id.operating_unit_id:
-            res['operating_unit_id'] = \
-                procurement.location_id.operating_unit_id.id
+    def _prepare_mo_vals(self, bom):
+        res = super(ProcurementOrder, self)._prepare_mo_vals(bom)
+        if self.location_id.operating_unit_id:
+            res['operating_unit_id'] = self.location_id.operating_unit_id.id
         return res
