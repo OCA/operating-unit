51aaebba15250fca57a0a8a61b6351c56314db70
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-25 16:09:05 +0530

    [MIG] mrp_operating_unit v10

--- mrp_operating_unit/models/mrp.py
+++ mrp_operating_unit/models/mrp.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
-from openerp import api, fields, models
-from openerp.tools.translate import _
-from openerp.exceptions import Warning
+from odoo import api, fields, models
+from odoo.tools.translate import _
+from odoo.exceptions import ValidationError
 
 
 class MrpProduction(models.Model):
 
     _inherit = 'mrp.production'
 
-    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
-                                        default=lambda self:
-                                        self.env['res.users'].
-                                        operating_unit_default_get(self._uid))
+    operating_unit_id = fields.Many2one(
+        'operating.unit', 'Operating Unit',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        default=lambda self: self.env['res.users'
+                                      ].operating_unit_default_get(self._uid)
+    )
 
     @api.constrains('operating_unit_id', 'location_src_id', 'location_dest_id')
     def _check_location_operating_unit(self):
@@ -26,21 +27,24 @@ class MrpProduction(models.Model):
                 (mo.location_src_id.operating_unit_id or
                  mo.location_dest_id.operating_unit_id)
             ):
-                raise Warning(_('The Operating Unit of the Manufacturing Order\
-                                 must match with that of the Raw Materials and\
-                                  Finished Product Locations.'))
+                raise ValidationError(_(
+                    'The Operating Unit of the Manufacturing Order must match '
+                    'with that of the Raw Materials and Finished Product '
+                    'Locations.'))
             if (
                 mo.operating_unit_id and
                 mo.operating_unit_id != mo.location_src_id.operating_unit_id
             ):
-                raise Warning(_('The Operating Unit of the Manufacturing Order\
-                                 must match with that of the Raw Materials and\
-                                  Finished Product Locations.'))
+                raise ValidationError(_(
+                    'The Operating Unit of the Manufacturing Order must match '
+                    'with that of the Raw Materials and Finished Product '
+                    'Locations.'))
             if (
                 mo.operating_unit_id and
                 mo.operating_unit_id != mo.location_dest_id.operating_unit_id
             ):
-                raise Warning(_('The Operating Unit of the Manufacturing Order\
-                                 must match with that of the Raw Materials and\
-                                  Finished Product Locations.'))
+                raise ValidationError(_(
+                    'The Operating Unit of the Manufacturing Order must match '
+                    'with that of the Raw Materials and Finished Product '
+                    'Locations.'))
         return True
