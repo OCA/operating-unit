08e112b63953700ad465f5b92662b8812c9a123f
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-25 16:09:05 +0530

    [MIG] mrp_operating_unit v10

--- mrp_operating_unit/__manifest__.py
+++ mrp_operating_unit/__manifest__.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in MRP",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
     "license": "LGPL-3",
     "category": "Manufacturing",
-    "depends": ["mrp",
-                "procurement_operating_unit"],
+    "depends": [
+        "mrp",
+        "procurement_operating_unit"
+    ],
     "data": [
         "security/mrp_security.xml",
         "views/mrp_view.xml"
--- mrp_operating_unit/tests/test_procurement.py
+++ mrp_operating_unit/tests/test_procurement.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp.tests import common
+from odoo.tests import common
+from odoo.exceptions import ValidationError
 
 
 class TestProcurement(common.TransactionCase):
 
     def setUp(self):
         super(TestProcurement, self).setUp()
-        self.res_users_model = self.env['res.users']
         self.procurement_order_model = self.env['procurement.order']
         self.procurement_rule_model = self.env['procurement.rule']
         self.bom_model = self.env['mrp.bom']
         self.warehouse = self.env.ref('stock.warehouse0')
+        self.picking_type = self.env.ref('stock.picking_type_internal')
 
         # Main Operating Unit
         self.ou1 = self.env.ref('operating_unit.main_operating_unit')
@@ -24,53 +24,53 @@ class TestProcurement(common.TransactionCase):
         # Products
         self.product1 = self.env.ref('product.product_product_9')
 
+        # Stock Location
+        self.stock_location_1 = self.env.ref('stock.stock_location_shop0')
+        self.stock_location_2 = self.env.ref('stock.stock_location_14')
+
         self.rule = self._create_procurement_rule()
         self.mrp_bom_method = self._create_bom()
         self.procurement_order = self._create_procurement_order()
-
+        self.procurement_order.run()
 
     def _create_procurement_rule(self):
         rule = self.procurement_rule_model.create({
-                'name': 'Procurement rule',
-                'action': 'manufacture'
-                })
+            'name': 'Procurement rule',
+            'action': 'manufacture',
+            'picking_type_id': self.picking_type.id,
+        })
         return rule
 
     def _create_bom(self):
         bom = self.bom_model.create({
-                'product_tmpl_id': self.product1.product_tmpl_id.id,
-                'product_id': self.product1.id,
-                'product_qty': '1',
-                'type': 'normal',
-                'product_efficiency': '1.00',
+            'product_tmpl_id': self.product1.product_tmpl_id.id,
+            'product_id': self.product1.id,
+            'product_qty': '1',
+            'type': 'normal',
         })
         return bom
 
     def _create_procurement_order(self):
-        # On change for warehouse_id
-        new_line = self.procurement_order_model.new()
-        res = new_line.change_warehouse_id(self.warehouse.id)
-        if res.get('value') and res.get('value').get('location_id'):
-            location_id = res.get('value').get('location_id')
-        # On change for product_id
-        new_line = self.procurement_order_model.new()
-        res = new_line.onchange_product_id(self.product1.id)
-        if res.get('value') and res.get('value').get('product_uom'):
-            product_uom = res.get('value').get('product_uom')
         order = self.procurement_order_model.create({
-                'product_id': self.product1.id,
-                'product_uom': product_uom,
-                'product_qty': '10',
-                'name': 'Procurement Order',
-                'warehouse_id': self.warehouse.id,
-                'bom_id': self.mrp_bom_method.id,
-                'rule_id': self.rule.id,
-                'location_id': location_id,
+            'product_id': self.product1.id,
+            'product_uom': self.product1.uom_id.id,
+            'product_qty': '10',
+            'name': 'Procurement Order',
+            'warehouse_id': self.warehouse.id,
+            'bom_id': self.mrp_bom_method.id,
+            'rule_id': self.rule.id,
+            'location_id': self.stock_location_1.id,
         })
         return order
 
     def test_security(self):
-        self.assertEqual(self.procurement_order.location_id.operating_unit_id,
-                         self.procurement_order.production_id.operating_unit_id,
-                         'The Operating Unit in Procurement Order Location'
-                         'does not match to Manufacturing Order OU.')
+        self.assertEqual(
+            self.procurement_order.location_id.operating_unit_id,
+            self.procurement_order.production_id.operating_unit_id,
+            'The Operating Unit in Procurement Order Location'
+            'does not match to Manufacturing Order OU.'
+        )
+
+        with self.assertRaises(ValidationError):
+            self.procurement_order.write({'location_id':
+                                          self.stock_location_2.id})
