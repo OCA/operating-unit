f35dd0b60f5c26c447f60b7cf41a712f8335d398
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-25 16:09:05 +0530

    [MIG] mrp_operating_unit v10

--- mrp_operating_unit/tests/test_mrp_operating_unit.py
+++ mrp_operating_unit/tests/test_mrp_operating_unit.py
@@ -1,4 +1,9 @@
-from openerp.tests import common
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from odoo.tests import common
+from odoo.exceptions import ValidationError
 
 
 class TestMrpOperatingUnit(common.TransactionCase):
@@ -8,6 +13,7 @@ class TestMrpOperatingUnit(common.TransactionCase):
         self.res_users_model = self.env['res.users']
         self.mrp_production_model = self.env['mrp.production']
         self.company = self.env.ref('base.main_company')
+        self.bom_id = self.env.ref('mrp.mrp_bom_manufacture')
 
         # Products
         self.product1 = self.env.ref('product.product_product_4c')
@@ -17,26 +23,24 @@ class TestMrpOperatingUnit(common.TransactionCase):
         # Main Operating Unit
         self.ou1 = self.env.ref('operating_unit.main_operating_unit')
         # Chicago Operating Unit
-        self.chicago = self.env.ref('stock_operating_unit.operating_unit_shop0')
-
+        self.chicago = \
+            self.env.ref('stock_operating_unit.operating_unit_shop0')
         # Groups
-        self.grp_mrp_saleman = self.env.ref('base.group_sale_salesman')
+        self.grp_mrp_saleman = self.env.ref('sales_team.group_sale_salesman')
+        self.grp_mrp_manager = self.env.ref('mrp.group_mrp_manager')
 
         # Users
-        self.user1 = self._create_user('user_1',
-                                       [self.grp_mrp_saleman],
-                                       self.company,
-                                       [self.ou1, self.chicago])
-        self.user2 = self._create_user('user_2',
-                                       [self.grp_mrp_saleman],
-                                       self.company,
-                                       [self.chicago])
+        self.user1 = self._create_user(
+            'user_1', [self.grp_mrp_saleman],
+            self.company, [self.ou1, self.chicago])
+        self.user2 = self._create_user(
+            'user_2', [self.grp_mrp_saleman, self.grp_mrp_manager],
+            self.company, [self.chicago])
 
         # Manufacturing Orders
         self.mrp_record1 = self._create_mrp('Manufacturing Order 1', self.ou1)
         self.mrp_record2 = self._create_mrp('Manufacturing Order 2',
                                             self.chicago, self.stock_location)
-
 
     def _create_user(self, login, groups, company, operating_units,
                      context=None):
@@ -55,38 +59,36 @@ class TestMrpOperatingUnit(common.TransactionCase):
         return user
 
     def _create_mrp(self, name, operating_unit, stock_location=False):
-        new_line = self.mrp_production_model.new()
-        res = new_line.product_id_change(product_id=self.product1.id)
-        if res.get('value') and res.get('value').get('bom_id'):
-            bom_id = res.get('value').get('bom_id')
-        if res.get('value') and res.get('value').get('product_uom'):
-            product_uom = res.get('value').get('product_uom')
-        if operating_unit is self.ou1:
+        if operating_unit == self.ou1:
             mrp = self.mrp_production_model.create({
                 'name': name,
                 'product_id': self.product1.id,
-                'bom_id': bom_id,
+                'bom_id': self.bom_id.id,
                 'product_qty': '10.0',
-                'product_uom': product_uom,
+                'product_uom_id': self.product1.uom_id.id,
                 'operating_unit_id': operating_unit.id,
-                })
+            })
         else:
             mrp = self.mrp_production_model.create({
                 'name': name,
                 'product_id': self.product1.id,
-                'bom_id': bom_id,
+                'bom_id': self.bom_id.id,
                 'product_qty': '10.0',
-                'product_uom': product_uom,
+                'product_uom_id': self.product1.uom_id.id,
                 'operating_unit_id': operating_unit.id,
                 'location_src_id': stock_location.id,
                 'location_dest_id': stock_location.id
-                })
+            })
         return mrp
 
     def test_mrp_ou(self):
-        record = self.mrp_production_model.sudo(self.user2.id).search(
-                                          [('id', '=', self.mrp_record1.id),
-                                           ('operating_unit_id', '=',
-                                           self.ou1.id)])
+        record = self.mrp_production_model.sudo(self.user2.id).\
+            search([('id', '=', self.mrp_record1.id),
+                    ('operating_unit_id', '=', self.ou1.id)])
         self.assertEqual(record.ids, [], 'User 2 should not have access to '
-                                       'OU : %s' % self.ou1.name)
+                         'OU : %s' % self.ou1.name)
+
+        with self.assertRaises(ValidationError):
+            self.mrp_record1.operating_unit_id = False
+        with self.assertRaises(ValidationError):
+            self.mrp_record1.write({'operating_unit_id': self.chicago.id})
