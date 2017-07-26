eb43d9f78ffc38ba2ade8e6f951f450651831e0d
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-26 15:03:15 +0530

    [MIG] purchase_request_qweb_operating_unit & report_qweb_operating_unit v10

--- purchase_request_qweb_operating_unit/__manifest__.py
+++ purchase_request_qweb_operating_unit/__manifest__.py
@@ -4,17 +4,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Qweb Report With Operating Unit',
-    'version': '9.0.1.0.0',
+    'name': 'Purchase Request Qweb Report With Operating Unit',
+    'version': '10.0.1.0.0',
     'category': 'Reports/Qweb',
     'license': 'AGPL-3',
     'author': 'Eficent Business and IT Consulting Services S.L., '
               'Serpent Consulting Services Pvt. Ltd.,'
               'Odoo Community Association (OCA)',
     'website': 'https://odoo-community.org/',
-    'depends': ['report', 'operating_unit'],
+    'depends': [
+        'purchase_request_operating_unit',
+        'report_qweb_operating_unit'
+    ],
     'data': [
-        'views/report_qweb_operating_unit.xml',
+        'views/report_purchase_request_qweb.xml',
     ],
     'installable': True,
 }
