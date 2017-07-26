a47a7c225b1edf92109b45933f8a8dc068527609
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-26 12:42:31 +0530

    [MIG] account_invoice_merge_operating_unit v10

--- account_invoice_merge_operating_unit/__manifest__.py
+++ account_invoice_merge_operating_unit/__manifest__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': "Account Invoice Merge Operating Unit",
-    'author': "Eficent Business and IT Consulting Services, S.L.,"
+    'author': "Eficent Business and IT Consulting Services S.L., "
+              "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
     'website': "http://www.eficent.com",
     'category': 'Finance',
-    'version': '9.0.1.0.0',
-    'license': 'AGPL-3',
+    'version': '10.0.1.0.0',
+    'license': 'LGPL-3',
     'depends': [
         'account_invoice_merge',
         'account_operating_unit',
--- account_invoice_merge_operating_unit/wizard/invoice_merge.py
+++ account_invoice_merge_operating_unit/wizard/invoice_merge.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from openerp import api, models, _
-from openerp.exceptions import Warning as UserError
+from odoo import api, models, _
+from odoo.exceptions import UserError
 
 
 class InvoiceMerge(models.TransientModel):
@@ -15,10 +15,9 @@ class InvoiceMerge(models.TransientModel):
         res = super(InvoiceMerge, self)._dirty_check()
         if self.env.context.get('active_model', '') == 'account.invoice':
             ids = self.env.context['active_ids']
-
             invs = self.env['account.invoice'].browse(ids)
             for d in invs:
                 if d['operating_unit_id'] != invs[0]['operating_unit_id']:
-                    raise UserError(
-                        _('Not all invoices are at the same Operating Unit!'))
+                    raise UserError(_('Not all invoices are at the same '
+                                      'Operating Unit!'))
         return res
