bd1d21154b96f7c470b9dbf3d2e7f8d00fcdccc0
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-26 11:37:13 +0530

    [MIG] sale_crm_operating_unit v10

--- sale_crm_operating_unit/models/sale.py
+++ sale_crm_operating_unit/models/sale.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
-# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp import api, models
-from openerp.exceptions import Warning as UserError
-from openerp.tools.translate import _
+from odoo import api, models
+from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
 
 class SaleOrder(models.Model):
@@ -16,5 +16,5 @@ class SaleOrder(models.Model):
         if self.opportunity_id and self.opportunity_id.operating_unit_id and\
                 self.opportunity_id.\
                 operating_unit_id != self.operating_unit_id:
-            raise UserError(_('Configuration error!\nThe Operating Unit\
-            of Sales Order should be same that of Opportunity.'))
+            raise ValidationError(_('''Configuration error! The Operating Unit
+                of Sales Order should be same that of Opportunity.'''))
