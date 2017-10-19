6eee1ed574088bafcbcc2270565cfff4148f475f
root <matiar.rahman@gmail.com>
2017-10-19 18:04:42 +0600

    [FIX] Travis CI Failed Issue 3.1

--- hr_employee_operating_unit/models/hr_employee.py
+++ hr_employee_operating_unit/models/hr_employee.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # © 2017 Genweb2 Limited - Matiar Rahman
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
 from odoo import api, fields, models
+
 
 class HREmployee(models.Model):
 
