fa13d3521defa3b49f6de10fec06e047936c9701
Edi Santoso <repodevs@gmail.com>
2017-10-19 22:02:34 +0700

    res_partner_operating_unit: remove default assigned operating unit in partner

--- res_partner_operating_unit/models/res_partner.py
+++ res_partner_operating_unit/models/res_partner.py
@@ -9,8 +9,5 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     operating_unit_id = fields.Many2one(
-        comodel_name='operating.unit',
-        string='Operating Unit',
-        default=lambda self: (self.env['res.users'].
-                              operating_unit_default_get(self.env.uid))
+        comodel_name='operating.unit', string='Operating Unit'
     )
