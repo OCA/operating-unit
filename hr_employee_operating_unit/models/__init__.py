984d57c2f64259f887d885930f625cf75d7893f6
root <matiar.rahman@gmail.com>
2017-10-19 17:14:44 +0600

    [FIX] Travis CI Failed Issue

--- hr_employee_operating_unit/models/__init__.py
+++ hr_employee_operating_unit/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2017 Odoo Community Association - Matiar Rahman
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-import hr_employee
+from . import hr_employee
