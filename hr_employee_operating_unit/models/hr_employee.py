f6a0a14170500febcc0c32a3b5b94eea79cfa807
root <shamimislam39@gmail.com>
2017-10-25 00:06:26 +0600

    [FIX] Remove nested method to get default operating unit and remove on change user method

--- hr_employee_operating_unit/models/hr_employee.py
+++ hr_employee_operating_unit/models/hr_employee.py
@@ -9,15 +9,9 @@ class HREmployee(models.Model):
     _inherit = 'hr.employee'
 
     @api.model
-    def operating_unit_default_get(self, uid2):
-        if not uid2:
-            uid2 = self._uid
-        user = self.env['res.users'].browse(uid2)
-        return user.default_operating_unit_id
-
-    @api.model
     def _get_operating_unit(self):
-        return self.operating_unit_default_get(self._uid)
+        user = self.env['res.users'].browse(self._uid)
+        return user.default_operating_unit_id
 
     @api.model
     def _get_operating_units(self):
@@ -30,18 +24,3 @@ class HREmployee(models.Model):
                                           'operating_unit_emp_rel',
                                           'emp_id', 'ou_id', 'Operating Units',
                                           default=_get_operating_units)
-
-    @api.onchange('user_id')
-    def _onchange_user(self):
-        super(HREmployee, self)._onchange_user()
-        if self.user_id:
-
-            if self.operating_unit_ids:
-                ou_ids = self.operating_unit_ids.ids
-            else:
-                ou_ids = []
-
-            if self.user_id.default_operating_unit_id.id not in ou_ids:
-                ou_ids.append(self.user_id.default_operating_unit_id.id)
-
-            self.operating_unit_ids = [(6, 0, ou_ids)]
