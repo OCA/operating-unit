68dce57ad2fcc3c45f1703b2809022baa788e878
Graeme Gellatly <gdgellatly@gmail.com>
2018-02-19 12:04:57 +1300

    Port to v11 - retry

--- operating_unit/__init__.py
+++ operating_unit/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2015-2017 Eficent
+# Copyright 2015-2017 Eficent
 # - Jordi Ballester Alomar
-# © 2015-2017 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2015-2017 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import models
