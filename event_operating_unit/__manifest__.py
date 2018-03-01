410811052515c68872e27cf7ec34322d3cc9c003
Timon Tschanz <timon.tschanz@camptocamp.com>
2018-03-01 09:48:24 +0100

    fix license

--- event_operating_unit/__manifest__.py
+++ event_operating_unit/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2018 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Event Operating Unit',
     'version': '11.0.1.0.0',
     'category': 'others',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'author': 'camptocamp, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/operating_unit',
     'depends': [
