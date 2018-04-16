88daa7365d72e8ac9f472fd2c484b872ddfc40af
Michael Aldrin Villamar <michael.villamar@cloudemployee.co.uk>
2018-04-16 08:16:42 +0800

    Migrated analytic_operating_unit and account_operating_unit to 11.0

---
+++ account_operating_unit/wizard/__init__.py
@@ -0,0 +1,6 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import account_report_common
+from . import account_financial_report
+from . import account_report_trial_balance
+
