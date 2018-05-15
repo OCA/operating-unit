b2f5c5295704b73ad85d859c76a9925f3009e3e1
Willem Hulshof <w.hulshof@magnus.nl>
2018-05-15 12:07:18 +0200

    New API version of PR42

--- account_operating_unit/models/account_bank_statement.py
+++ account_operating_unit/models/account_bank_statement.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # © 2016 Jarsa Sistemas S.A. de C.V..
+# © 2018 Willem Hulshof, Magnus.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from openerp import api, models
+from odoo import api, fields, models, _
 
 
 class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
 
-    @api.v7
-    def process_reconciliations(self, cr, uid, ids, data, context=None):
-        aml_obj = self.pool.get('account.move.line')
-        for data_line in data:
-            for counterpart in data_line['counterpart_aml_dicts']:
-                counterpart_id = counterpart['counterpart_aml_id']
-                move_line = aml_obj.browse(cr, uid, counterpart_id, context)
-                counterpart['operating_unit_id'] = (
-                    move_line.operating_unit_id.id)
-        return super(AccountBankStatementLine, self).process_reconciliations(
-            cr, uid, ids, data, context=None)
+    @api.multi
+    def process_reconciliations(self, data):
+        AccountMoveLine = self.env['account.move.line']
+        for st_line, datum in zip(self, data):
+            for aml_dict in datum.get('counterpart_aml_dicts', []):
+                aml_dict['move_line'] = AccountMoveLine.browse(aml_dict['counterpart_aml_id'])
+                aml_dict['operating_unit_id'] = (
+                    aml_dict['move_line'].operating_unit_id.id)
+        return super(AccountBankStatementLine, self).process_reconciliations(data)
