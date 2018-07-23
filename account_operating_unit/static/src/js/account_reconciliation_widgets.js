dd5be16a6f0ae95f4450b35e522d1139ae3874e9
Willem Hulshof <w.hulshof@magnus.nl>
2018-07-23 04:07:47 +0200

    Revert "Task: FIN-02: Operating Unit bankstatement"
    This reverts commit d7687c46ecdb532bd5b9448bd19a62c3b69b8b97.

--- account_operating_unit/static/src/js/account_reconciliation_widgets.js
+++ account_operating_unit/static/src/js/account_reconciliation_widgets.js
@@ -26,48 +26,6 @@ odoo.define('account_operating_unit.reconciliation', function (require) {
                     domain: [],
                 },
             }
-        },
-
-        fetchPresets: function() {
-            var self = this;
-            var deferred_last_update = self.model_presets.query(['write_date']).order_by('-write_date').first().then(function (data) {
-                self.presets_last_write_date = (data ? data.write_date : undefined);
-            });
-            var deferred_presets = self.model_presets.query().order_by('-sequence', '-id').all().then(function (data) {
-                self.presets = {};
-                _(data).each(function(datum){
-                    var operating_unit = (datum.operating_unit_id ? datum.operating_unit_id : datum.journal_id.operating_unit_id)
-                    var preset = {
-                        id: datum.id,
-                        name: datum.name,
-                        sequence: datum.sequence,
-                        lines: [{
-                            account_id: datum.account_id,
-                            journal_id: datum.journal_id,
-                            label: datum.label,
-                            amount_type: datum.amount_type,
-                            amount: datum.amount,
-                            tax_id: datum.tax_id,
-                            analytic_account_id: datum.analytic_account_id,
-                            operating_unit_id:operating_unit
-                        }]
-                    };
-                    if (datum.has_second_line) {
-                        preset.lines.push({
-                            account_id: datum.second_account_id,
-                            journal_id: datum.second_journal_id,
-                            label: datum.second_label,
-                            amount_type: datum.second_amount_type,
-                            amount: datum.second_amount,
-                            tax_id: datum.second_tax_id,
-                            analytic_account_id: datum.second_analytic_account_id,
-                            operating_unit_id:operating_unit
-                        });
-                    }
-                    self.presets[datum.id] = preset;
-                });
-            });
-            return $.when(deferred_last_update, deferred_presets);
         }
     })
 
