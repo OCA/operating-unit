ccf7e74e4c7dc61645af914b34b50098db1ea33a
Willem Hulshof <w.hulshof@magnus.nl>
2018-08-09 21:59:19 +0200

    account_cutoff_base and prepaid adapted for operating unit

---
+++ account_cutoff_base_operating_unit/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2018 Magnus (Willem Hulshof <w.hulshof@magnus.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Cut-off Base Operating Unit',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'OU adaptation for Base module Account Cut-offs',
+    'author': 'Magnus,Odoo Community Association (OCA)',
+    'website': 'http://www.magnus.nl',
+    'depends': ['account_operating_unit',
+                'account_cutoff_base'],
+    'data': ['views/account_cutoff.xml',
+    ],
+    'installable': True,
+}
