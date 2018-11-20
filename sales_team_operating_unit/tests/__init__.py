217582ed97605e584dbcf4813513450c48d83c92
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-20 11:29:47 +0100

    [MIG] sales_team_operating_unit to v12

--- sales_team_operating_unit/tests/__init__.py
+++ sales_team_operating_unit/tests/__init__.py
@@ -1,7 +1,3 @@
-# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
-# Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
-#   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import test_crm_team_operating_unit
