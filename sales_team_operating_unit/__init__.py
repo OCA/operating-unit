69f7511c59dd6394b8b50fccb290999258441574
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-22 09:04:19 +0100

    sales_team_operating_unit to version 12

--- sales_team_operating_unit/__init__.py
+++ sales_team_operating_unit/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
-# Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
-#   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 from . import models
--- sales_team_operating_unit/tests/test_crm_team_operating_unit.py
+++ sales_team_operating_unit/tests/test_crm_team_operating_unit.py
@@ -15,7 +15,8 @@ class TestSaleTeamOperatingUnit(common.TransactionCase):
         self.crm_team_model = self.env['crm.team']
         # Groups
         self.grp_sale_mngr = self.env.ref('sales_team.group_sale_manager')
-        self.grp_user = self.env.ref('base.group_user')
+        self.grp_user = self.env.ref(
+            'operating_unit.group_multi_operating_unit')
         # Company
         self.company = self.env.ref('base.main_company')
         # Main Operating Unit
@@ -23,6 +24,7 @@ class TestSaleTeamOperatingUnit(common.TransactionCase):
         # B2C Operating Unit
         self.b2c = self.env.ref('operating_unit.b2c_operating_unit')
         # Create User 1 with Main OU
+
         self.user1 = self._create_user('user_1', [self.grp_sale_mngr,
                                                   self.grp_user], self.company,
                                        [self.ou1])
@@ -52,9 +54,10 @@ class TestSaleTeamOperatingUnit(common.TransactionCase):
 
     def _create_crm_team(self, uid, operating_unit):
         """Create a Sales Team."""
-        crm = self.crm_team_model.sudo(uid).create({'name': 'CRM team',
-                                                    'operating_unit_id':
-                                                    operating_unit.id})
+        crm = self.crm_team_model.sudo(uid).create(
+            {'name': 'CRM team',
+             'operating_unit_id': operating_unit.id,
+             'company_id': self.company.id})
         return crm
 
     def test_crm_team(self):
