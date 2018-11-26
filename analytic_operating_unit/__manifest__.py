b36fdf5529888908ce7b9781cd063e0cb4799b4b
Sudhir Arya <sudhir@erpharbor.com>
2018-11-26 11:40:20 +0100

    [MIG]analytic_operating_unit to v12

--- analytic_operating_unit/__manifest__.py
+++ analytic_operating_unit/__manifest__.py
@@ -1,18 +1,20 @@
 # Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
 {
     "name": "Analytic Operating Unit",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Sales",
-    "depends": ["analytic", "operating_unit"],
+    "depends": [
+        "analytic",
+        "operating_unit",
+    ],
     "data": [
-        "views/analytic_account_view.xml",
         "security/analytic_account_security.xml",
+        "views/analytic_account_view.xml",
     ],
     'installable': True,
 }
