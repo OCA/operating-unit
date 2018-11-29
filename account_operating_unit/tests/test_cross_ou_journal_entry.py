a6c2ee3f0e643ef21790b567cf30d9fe8ca37fe3
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-29 12:53:36 +0100

    [MIG] account_operating_unit: Migration to v12

--- account_operating_unit/tests/test_cross_ou_journal_entry.py
+++ account_operating_unit/tests/test_cross_ou_journal_entry.py
@@ -1,6 +1,7 @@
-# © 2016-17 Eficent Business and IT Consulting Services S.L.
-# © 2016 Serpent Consulting Services Pvt. Ltd.
+# © 2019 Eficent Business and IT Consulting Services S.L.
+# © 2019 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 from . import test_account_operating_unit as test_ou
 
 
