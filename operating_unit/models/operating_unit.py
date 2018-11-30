fd9bff275a52cff9203bf4484cb739274cb99c87
aheficent <ahenriquez@eficent.com>
2018-11-30 11:14:16 +0100

    [IMP]add user_ids field in operating unit. Useful for accesing in other models

--- operating_unit/models/operating_unit.py
+++ operating_unit/models/operating_unit.py
@@ -18,6 +18,10 @@ class OperatingUnit(models.Model):
         'res.company', 'Company', required=True, default=lambda self:
         self.env['res.company']._company_default_get('account.account'))
     partner_id = fields.Many2one('res.partner', 'Partner', required=True)
+    user_ids = fields.Many2many(
+        'res.users', 'operating_unit_users_rel', 'poid', 'user_id',
+        'Users Allowed',
+    )
 
     _sql_constraints = [
         ('code_company_uniq', 'unique (code,company_id)',
