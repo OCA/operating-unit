1ad1ccda6009fbf5abb063c214f8e5d6999d6d7e
Beñat Jimenez <benat.jimenez@eficent.com>
2019-01-15 10:24:02 +0100

    [MIG] mrp_operating_unit: Migration to version 12

--- mrp_operating_unit/__manifest__.py
+++ mrp_operating_unit/__manifest__.py
@@ -1,20 +1,20 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
-# Copyright 2015-17 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in MRP",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
-    "website": "http://www.eficent.com",
+    "website": "https://github.com/operating-unit",
     "license": "LGPL-3",
     "category": "Manufacturing",
     "depends": [
         "mrp",
-        "procurement_operating_unit"
+        "stock_operating_unit",
+        "sales_team"
     ],
     "data": [
         "security/mrp_security.xml",
