6b917b5d93c62fb2ed13a4eaf3aa22efcdc87783
Beñat Jimenez <benat.jimenez@eficent.com>
2019-01-15 10:24:02 +0100

    [MIG] mrp_operating_unit: Migration to version 12

--- mrp_operating_unit/models/__init__.py
+++ mrp_operating_unit/models/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
-# Copyright 2015-17 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import mrp
-from . import procurement
+from . import stock_rule
