0dbce4cf9613fc6978764e4cd51379e826c56178
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-01-22 09:15:52 +0100

    [MIG] Migrate crm_operating_unit to v12

--- crm_operating_unit/models/crm_lead.py
+++ crm_operating_unit/models/crm_lead.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L. -
+# © 2015-19 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
 # © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
@@ -15,8 +14,7 @@ class CRMLead(models.Model):
         team = self.env['crm.team']._get_default_team_id()
         if team.operating_unit_id:
             return team.operating_unit_id
-        else:
-            return self.env['res.users'].operating_unit_default_get(self._uid)
+        return self.env['res.users'].operating_unit_default_get(self._uid)
 
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
                                         related='team_id.operating_unit_id',
