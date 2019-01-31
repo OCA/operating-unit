8cab16a3f58b01d5cc677c852947898884280959
Beñat Jimenez <benat.jimenez@eficent.com>
2019-01-31 13:38:36 +0100

    [MIG] sale_operating_unit: Migration to v11

---
+++ sale_operating_unit/readme/CONTRIBUTORS.rst
@@ -0,0 +1,6 @@
+* Jordi Ballester Alomar <jordi.ballester@eficent.com>
+* Aaron Henriquez <aheficent@eficent.com>
+* Lois Rilo <lois.rilo@eficent.com>
+* Miquel Raich <miquel.raich@eficent.com>
+* Sudhir Arya <sudhir.arya@serpentcs.com>
+* Darshan Patel <darshan.patel.serpencs@gmail.com>
--- sale_operating_unit/tests/test_sale_operating_unit.py
+++ sale_operating_unit/tests/test_sale_operating_unit.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L. -
+# © 2019 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
-# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2019 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo.tests import common
 
