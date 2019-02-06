eb7c4ca4a5ba59eb57c19a625f4431bfab480753
Beñat Jimenez <benat.jimenez@eficent.com>
2019-02-06 10:02:21 +0100

    [MIG] mis_builder_operating_unit: Migration to v11

--- mis_builder_operating_unit/__manifest__.py
+++ mis_builder_operating_unit/__manifest__.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
+# © 2015-19 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2015-19 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'MIS Builder with Operating Unit',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Reporting',
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "ACSONE SA/NV,"
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    'website': 'http://www.eficent.com',
+    'website': 'https://github.com/operating-unit',
     'depends': ['mis_builder', 'account_operating_unit'],
     'data': [
-        'view/mis_builder.xml',
-        'security/mis_builder_security.xml'
+        'security/mis_builder_security.xml',
+        'view/mis_builder.xml'
     ],
     'installable': True,
 }
