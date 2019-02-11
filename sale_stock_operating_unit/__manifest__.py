a264fed172d61d448079189bded82ce65212b167
Beñat Jimenez <benat.jimenez@eficent.com>
2019-02-11 14:01:26 +0100

    [MIG] sale_stock_operating_unit: Migration to v12

--- sale_stock_operating_unit/__manifest__.py
+++ sale_stock_operating_unit/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L. -
+# Copyright 2015-19 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
-# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2015-19 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in Sales Stock",
     "summary": "An operating unit (OU) is an organizational entity part of a "
                "company",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, Serpent Consulting Services Pvt. Ltd., "
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
