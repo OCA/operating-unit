8b8636c61d50c1ff1fceb2acfe89c8f0973cb6bd
Beñat Jimenez <benat.jimenez@eficent.com>
2019-02-11 14:01:26 +0100

    [MIG] sale_stock_operating_unit: Migration to v12

--- sale_stock_operating_unit/models/stock_move.py
+++ sale_stock_operating_unit/models/stock_move.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L. -
+# Copyright 2015-19 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
-# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2015-19 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import models
 
@@ -13,11 +12,10 @@ class StockMove(models.Model):
         """
         Override to add Operating Units to Picking.
         """
-        values = super(StockMove, self)._prepare_picking_assign()
-        sale_line = self.procurement_id and self.procurement_id.sale_line_id
-        if sale_line:
-            values.update({
-                'operating_unit_id': sale_line.order_id and
-                sale_line.order_id.operating_unit_id.id
-            })
+        values = super(StockMove, self)._get_new_picking_values()
+
+        values.update({
+            'operating_unit_id': self.sale_line_id.operating_unit_id.id
+        })
+
         return values
