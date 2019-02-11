86c16295dd36d19a12e09b6d52352144ed3a2422
Beñat Jimenez <benat.jimenez@eficent.com>
2019-02-11 14:01:26 +0100

    [MIG] sale_stock_operating_unit: Migration to v12

--- sale_stock_operating_unit/__init__.py
+++ sale_stock_operating_unit/__init__.py
@@ -1,6 +1,2 @@
-# -*- coding: utf-8 -*-
-# © 2015-17 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import models
