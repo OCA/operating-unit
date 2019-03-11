060255dcefdb19d53f2b9a487f70e25400972ff7
Beñat Jimenez <benat.jimenez@eficent.com>
2019-03-11 08:54:53 +0100

    [MIG] hr_expense_operating_unit: Migration to version 12.0

--- hr_expense_operating_unit/models/hr_expense.py
+++ hr_expense_operating_unit/models/hr_expense.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
-# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# Copyright 2016-19 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-19 Serpent Consulting Services Pvt. Ltd.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).).
 
-from odoo.tools.translate import _
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
@@ -39,22 +37,28 @@ class HrExpenseExpense(models.Model):
                                         'Expense must be the same.'))
 
     @api.multi
-    def submit_expenses(self):
-        res = super(HrExpenseExpense, self).submit_expenses()
+    def action_submit_expenses(self):
+        res = super(HrExpenseExpense, self).action_submit_expenses()
         if len(self.mapped('operating_unit_id')) != 1 or\
                 any(not expense.operating_unit_id for expense in self):
-            raise ValidationError(_('You cannot submit the Expenses having'
-                                    ' different Operating Units or with'
-                                    ' no Operating Unit!'))
+            raise ValidationError(_('You cannot submit the Expenses having '
+                                    'different Operating Units or with '
+                                    'no Operating Unit'))
         if res.get('context'):
             res.get('context').\
                 update({'default_operating_unit_id':
                         self[0].operating_unit_id.id})
         return res
 
-    def _prepare_move_line(self, line):
-        res = super(HrExpenseExpense, self)._prepare_move_line(line)
-        res.update({'operating_unit_id': self.operating_unit_id.id})
+    def _get_account_move_line_values(self):
+        res = super(HrExpenseExpense, self)._get_account_move_line_values()
+        for expense in self:
+            res[expense.id][0].update({
+                'operating_unit_id': self.operating_unit_id.id
+            })
+            res[expense.id][1].update({
+                'operating_unit_id': self.operating_unit_id.id
+            })
         return res
 
 
@@ -73,6 +77,5 @@ class HrExpenseSheet(models.Model):
         for rec in self:
             if (rec.company_id and rec.operating_unit_id and rec.company_id !=
                     rec.operating_unit_id.company_id):
-                raise ValidationError(_('Configuration error. The Company in '
-                                        'the Expense sheet and in the'
-                                        'Operating Unit must be the same.'))
+                raise ValidationError(_('''Configuration error. The company in
+                the Expense and in the Operating Unit must be the same'''))
