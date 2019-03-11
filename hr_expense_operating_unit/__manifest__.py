dbf55b6547252f7d6a18c7b40f50ac03d39b19c8
Beñat Jimenez <benat.jimenez@eficent.com>
2019-03-11 08:54:53 +0100

    [MIG] hr_expense_operating_unit: Migration to version 12.0

--- hr_expense_operating_unit/__manifest__.py
+++ hr_expense_operating_unit/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
-# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016-19 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-19 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "HR Expense Operating Unit",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": 'LGPL-3',
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
-    "website": "http://www.eficent.com",
+    "website": "https://github.com/OCA/operating-unit",
     "category": "Generic Modules/Human Resources",
     "depends": ["hr_expense", "account_operating_unit"],
     "data": [
