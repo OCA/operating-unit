1f2e016da2469ee99e0fea78674af64d8f7d5427
mara1 <raul.martin@braintec-group.com>
2019-03-28 16:46:39 +0100

    New module product_operating_unit to introduce the operating unit to the product template.

--- product_operating_unit/tests/__init__.py
+++ product_operating_unit/tests/__init__.py
@@ -1,2 +1,12 @@
+<<<<<<< HEAD
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+=======
+##############################################################################
+#
+#    Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
+#    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+#
+##############################################################################
+
+>>>>>>> New module product_operating_unit to introduce the operating unit to the product template.
 from . import test_product_operating_unit
