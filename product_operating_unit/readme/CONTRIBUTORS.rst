f4cfed9dcbea7e79f3d53f9a9031d95fcedded68
mara1 <raul.martin@braintec-group.com>
2019-03-28 16:46:39 +0100

    New module product_operating_unit to introduce the operating unit to the product template.

---
+++ product_operating_unit/readme/CONTRIBUTORS.rst
@@ -0,0 +1,1 @@
+* Raul Martin Felez <raul.martin@braintec-group.com>
---
+++ product_operating_unit/tests/__init__.py
@@ -0,0 +1,8 @@
+##############################################################################
+#
+#    Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
+#    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+#
+##############################################################################
+
+from . import test_product_operating_unit
