f0dea1532e8838943b24f60c4479a0564b49ed75
mara1 <raul.martin@braintec-group.com>
2019-03-29 09:37:50 +0100

    Unit Tests working now

--- product_operating_unit/tests/test_product_operating_unit.py
+++ product_operating_unit/tests/test_product_operating_unit.py
@@ -24,9 +24,12 @@ class TestProductOperatingUnit(common.TransactionCase):
         # B2B Operating Unit
         self.b2b = self.env.ref('operating_unit.b2b_operating_unit')
         # Products
-        self.product1 = self.env.ref('product.product_product_7')
-        self.product2 = self.env.ref('product.product_product_9')
-        self.product3 = self.env.ref('product.product_product_11')
+        self.product1 = self.env.ref(
+            'product.product_product_1_product_template')
+        self.product2 = self.env.ref(
+            'product.product_product_9_product_template')
+        self.product3 = self.env.ref(
+            'product.product_product_11_product_template')
         # Create users
         self.user1_id = self._create_user('user_1',
                                           [self.group_stock_user],
@@ -36,9 +39,9 @@ class TestProductOperatingUnit(common.TransactionCase):
                                           [self.group_stock_user],
                                           self.company2,
                                           [self.b2b])
-        self.product1.operating_unit_ids = [(6, 0, [self.ou1])]
-        self.product2.operating_unit_ids = [(6, 0, [self.b2b])]
-        self.product3.operating_unit_ids = [(6, 0, [self.ou1, self.b2b])]
+        self.product1.operating_unit_ids = [(6, 0, [self.ou1.id])]
+        self.product2.operating_unit_ids = [(6, 0, [self.b2b.id])]
+        self.product3.operating_unit_ids = [(6, 0, [self.ou1.id, self.b2b.id])]
 
     def _create_user(self, login, groups, company, operating_units):
         """ Create a user."""
@@ -59,15 +62,32 @@ class TestProductOperatingUnit(common.TransactionCase):
 
     def test_po_ou_security(self):
         """Test Security of Product Operating Unit"""
-        # User 2 is only assigned to Operating Unit 2, and cannot list
-        # products from Operating Unit 1.
+
+        # User 1 is only assigned to Operating Unit 1, and can see all
+        # products having Operating Unit 1.
+        product_ids = \
+            self.ProductTemplate.sudo(self.user1_id). \
+                search([('operating_unit_ids', 'in', self.ou1.id)]).ids
+        self.assertEqual(set(product_ids), set([self.product1.id, self.product3.id]))
+
+        # User 2 is only assigned to Operating Unit 2, so cannot see products
+        # having Operating Unit 1, expect those also having Operating Unit b2b
         product_ids = \
             self.ProductTemplate.sudo(self.user2_id). \
                 search([('operating_unit_ids', 'in', self.ou1.id)]).ids
-        self.assertEqual(product_ids, [])
-        # User 1 is assigned to Operating Unit 1, and can list
-        # the purchase order 1 from Operating Unit 1.
+        self.assertEqual(product_ids, [self.product3.id])
+
+        # User 2 is only assigned to Operating Unit 2, and can see all
+        # products having Operating Unit b2b.
+        product_ids = \
+            self.ProductTemplate.sudo(self.user2_id). \
+                search([('operating_unit_ids', 'in', self.b2b.id)]).ids
+        self.assertEqual(set(product_ids),
+                         set([self.product2.id, self.product3.id]))
+
+        # User 1 is only assigned to Operating Unit 1, so cannot see products
+        # having Operating Unit b2b, expect those also having Operating Unit 1
         product_ids = \
             self.ProductTemplate.sudo(self.user1_id). \
                 search([('operating_unit_ids', 'in', self.b2b.id)]).ids
-        self.assertNotEqual(product_ids, [])
+        self.assertEqual(product_ids, [self.product3.id])
