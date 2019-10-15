4dfb89ac6aeb997c1406501026e85c2f40dc218f
Olivier Jossen <olivier.jossen@braintec-group.com>
2019-10-15 06:47:17 +0200

    [12.0][ADD] New module mail_operating_unit to introduce the operating unit to mail

--- mail_operating_unit/models/mail_mail_ext.py
+++ mail_operating_unit/models/mail_mail_ext.py
@@ -17,25 +17,25 @@ class MailMailExt(models.Model):
         mails = super(MailMailExt, self).create(vals_list)
 
         for mail in mails:
+            operating_unit = False
             if mail.mail_message_id.model and mail.mail_message_id.res_id:
                 model = mail.mail_message_id.model
                 res_id = mail.mail_message_id.res_id
                 record = self.env[model].browse(res_id)
-                operating_unit = False
                 if getattr(record, 'operating_unit_id', False):
                     operating_unit = record.operating_unit_id
-                elif self.env.context.get('uid', False):
-                    user = self.env['res.users'].browse(self.env.context.get('uid'))
-                    operating_unit = user.operating_unit_for_mails_id
+            if not operating_unit and self.env.context.get('uid', False):
+                user = self.env['res.users'].browse(self.env.context.get('uid'))
+                operating_unit = user.operating_unit_for_mails_id
 
-                if operating_unit and operating_unit.catchall_alias and operating_unit.catchall_domain:
-                    sender_email_address = (operating_unit.catchall_alias + '@' +
-                                            operating_unit.catchall_domain)
-                    email_name = operating_unit.partner_id.name
-                    mail.email_from = formataddr((email_name, sender_email_address))
-                    mail.reply_to = mail.email_from
+            if operating_unit and operating_unit.catchall_alias and operating_unit.catchall_domain:
+                sender_email_address = (operating_unit.catchall_alias + '@' +
+                                        operating_unit.catchall_domain)
+                email_name = operating_unit.partner_id.name
+                mail.email_from = formataddr((email_name, sender_email_address))
+                mail.reply_to = mail.email_from
 
-                    if operating_unit.outgoing_mail_server_id:
-                        mail.mail_server_id = operating_unit.outgoing_mail_server_id.id
+                if operating_unit.outgoing_mail_server_id:
+                    mail.mail_server_id = operating_unit.outgoing_mail_server_id.id
 
         return mails
