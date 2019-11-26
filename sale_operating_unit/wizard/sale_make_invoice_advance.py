d7ec1674ad050a0a33d0e7610a6c43df5339789a
ahenriquez <ahenriquez@eficent.com>
2019-11-26 11:59:56 +0100

    [IMP]sale advance payment to include the OU

---
+++ sale_operating_unit/wizard/sale_make_invoice_advance.py
@@ -0,0 +1,12 @@
+from odoo import api, models
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    @api.multi
+    def _create_invoice(self, order, so_line, amount):
+        invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
+            order, so_line, amount)
+        invoice.operating_unit_id = order.operating_unit_id.id
+        return invoice
