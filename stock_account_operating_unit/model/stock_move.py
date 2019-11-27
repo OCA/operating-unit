46b0f4c64480cadc485f3f99aff207f556e16d47
mreficent <miquel.raich@eficent.com>
2019-11-27 20:57:57 +0100

    [FIX] Consider price_diff_line dict in _prepare_account_move_line

--- stock_account_operating_unit/model/stock_move.py
+++ stock_account_operating_unit/model/stock_move.py
@@ -17,6 +17,10 @@ class StockMove(models.Model):
         if res:
             debit_line_vals = res[0][2]
             credit_line_vals = res[1][2]
+            if len(res) == 3:
+                price_diff_line = res[2][2]
+            else:
+                price_diff_line = {}
 
             if (
                 self.operating_unit_id and self.operating_unit_dest_id and
@@ -28,18 +32,23 @@ class StockMove(models.Model):
                       ' and destination accounts related to different '
                       'operating units.')
                 )
-
-            debit_line_vals['operating_unit_id'] = (
-                self.operating_unit_dest_id.id or self.operating_unit_id.id
-            )
-            credit_line_vals['operating_unit_id'] = (
-                self.operating_unit_id.id or self.operating_unit_dest_id.id
-            )
             if (not self.operating_unit_dest_id
                     and not self.operating_unit_id.id):
-                ou = self.picking_id.picking_type_id.warehouse_id. \
-                    operating_unit_id
-                debit_line_vals['operating_unit_id'] = ou.id
-                credit_line_vals['operating_unit_id'] = ou.id
-            return [(0, 0, debit_line_vals), (0, 0, credit_line_vals)]
+                ou_id = self.picking_id.picking_type_id.warehouse_id.\
+                    operating_unit_id.id
+            else:
+                ou_id = False
+
+            debit_line_vals['operating_unit_id'] = ou_id or \
+                self.operating_unit_dest_id.id or self.operating_unit_id.id
+            credit_line_vals['operating_unit_id'] = ou_id or \
+                self.operating_unit_id.id or self.operating_unit_dest_id.id
+
+            rslt = [(0, 0, debit_line_vals), (0, 0, credit_line_vals)]
+            if price_diff_line:
+                price_diff_line['operating_unit_id'] = ou_id \
+                    or self.operating_unit_id.id or \
+                    self.operating_unit_dest_id.id
+                rslt.extend([(0, 0, price_diff_line)])
+            return rslt
         return res
