2a1d67a65615499eeaefde2ba6f0ef89be5b65a0
mreficent <miquel.raich@eficent.com>
2019-11-29 16:53:25 +0100

    [FIX] Delete uncalled file

--- sale_operating_unit/models/sale_order.py
+++ sale_operating_unit/models/sale_order.py
@@ -14,8 +14,7 @@ class SaleOrder(models.Model):
         team = self.env['crm.team']._get_default_team_id()
         if team.operating_unit_id:
             return team.operating_unit_id
-        else:
-            return self.env.user.default_operating_unit_id
+        return self.env.user.default_operating_unit_id
 
     operating_unit_id = fields.Many2one(
         comodel_name='operating.unit',
