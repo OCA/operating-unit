3d88f8c564fae1239da289f6dc45e4280a519a06
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2019-12-23 19:12:01 +0530

    [RFC] sale_multi_operating_unit v12

--- sale_multi_operating_unit/models/sale_order.py
+++ sale_multi_operating_unit/models/sale_order.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2019 Open Source Integrators
 # Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -9,3 +10,12 @@ class SaleOrder(models.Model):
 
     quote_ids = fields.One2many(
         'sale.order.quote', 'sale_id', string='Internal Quotes')
+
+    @api.constrains('quote_ids')
+    def _operating_unit_id(self):
+        operating_unit = [str(quote.operating_unit_id.id)
+                          for quote in self.quote_ids]
+        result = len(operating_unit) != len(set(operating_unit))
+        if result:
+            raise ValidationError(_(
+                """The Operating Unit must be unique per sale order!"""))
