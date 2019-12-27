637973e1a4104f201686cc6852cf552b33e9d0b9
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-27 14:22:01 -0600

    [IMP] sale_multi_operating_unit

--- sale_multi_operating_unit/models/__init__.py
+++ sale_multi_operating_unit/models/__init__.py
@@ -1,2 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import sale_order
+from . import (
+    sale_order_quote_line,
+    sale_order_quote,
+    sale_order,
+)
