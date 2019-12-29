04ac9032ac44044fff0f4f21ccd10942c7cfd2a5
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-12-29 09:49:32 -0600

    black, isort

--- sales_team_operating_unit/models/crm_team.py
+++ sales_team_operating_unit/models/crm_team.py
@@ -3,25 +3,33 @@
 # Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
 #   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class CrmTeam(models.Model):
 
-    _inherit = 'crm.team'
+    _inherit = "crm.team"
 
     operating_unit_id = fields.Many2one(
-        'operating.unit', 'Operating Unit',
-        default=lambda self: self.env['res.users'].operating_unit_default_get()
+        "operating.unit",
+        "Operating Unit",
+        default=lambda self: self.env["res.users"].operating_unit_default_get(),
     )
 
     @api.multi
-    @api.constrains('operating_unit_id', 'company_id')
+    @api.constrains("operating_unit_id", "company_id")
     def _check_company_operating_unit(self):
         for team in self:
-            if (team.company_id and team.operating_unit_id and
-                    team.company_id != team.operating_unit_id.company_id):
-                raise UserError(_('Configuration error, '
-                                  'The Company in the Sales Team and in the '
-                                  'Operating Unit must be the same.'))
+            if (
+                team.company_id
+                and team.operating_unit_id
+                and team.company_id != team.operating_unit_id.company_id
+            ):
+                raise UserError(
+                    _(
+                        "Configuration error, "
+                        "The Company in the Sales Team and in the "
+                        "Operating Unit must be the same."
+                    )
+                )
