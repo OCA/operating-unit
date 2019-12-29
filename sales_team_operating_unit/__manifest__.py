8e45e6a13ac5fb950cb8535cae026cf1d1adcc0e
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-12-29 09:49:32 -0600

    black, isort

--- sales_team_operating_unit/__manifest__.py
+++ sales_team_operating_unit/__manifest__.py
@@ -6,16 +6,11 @@
 {
     "name": "Sales Team Operating Unit",
     "version": "12.0.1.1.0",
-    "author": "Eficent, "
-              "SerpentCS,"
-              "Odoo Community Association (OCA)",
+    "author": "Eficent, " "SerpentCS," "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Sales",
     "depends": ["sales_team", "operating_unit"],
-    "data": [
-        "security/crm_security.xml",
-        "views/crm_team_view.xml",
-    ],
-    'installable': True,
+    "data": ["security/crm_security.xml", "views/crm_team_view.xml"],
+    "installable": True,
 }
