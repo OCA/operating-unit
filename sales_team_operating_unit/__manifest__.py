ad0bbc7df165a0879b9c0c5fd53761c66de76f80
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-12-29 09:56:33 -0600

    Migration to V13

--- sales_team_operating_unit/__manifest__.py
+++ sales_team_operating_unit/__manifest__.py
@@ -5,8 +5,8 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Sales Team Operating Unit",
-    "version": "12.0.1.1.0",
-    "author": "Eficent, " "SerpentCS," "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Eficent, SerpentCS, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Sales",
