6fd0e6b8980bc7be50abe6b99d133e1be9ab508d
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-12-30 07:40:23 -0600

    [IMP] operating_unit: black, isort

--- operating_unit/models/operating_unit.py
+++ operating_unit/models/operating_unit.py
@@ -7,40 +7,49 @@ from odoo import api, fields, models
 
 class OperatingUnit(models.Model):
 
-    _name = 'operating.unit'
-    _description = 'Operating Unit'
+    _name = "operating.unit"
+    _description = "Operating Unit"
 
-    name = fields.Char('Name', required=True)
-    code = fields.Char('Code', required=True)
-    active = fields.Boolean('Active', default=True)
+    name = fields.Char("Name", required=True)
+    code = fields.Char("Code", required=True)
+    active = fields.Boolean("Active", default=True)
     company_id = fields.Many2one(
-        'res.company', 'Company', required=True, readonly=True,
-        default=lambda self:
-        self.env['res.company']._company_default_get('account.account'))
-    partner_id = fields.Many2one('res.partner', 'Partner', required=True)
+        "res.company",
+        "Company",
+        required=True,
+        readonly=True,
+        default=lambda self: self.env["res.company"]._company_default_get(
+            "account.account"
+        ),
+    )
+    partner_id = fields.Many2one("res.partner", "Partner", required=True)
     user_ids = fields.Many2many(
-        'res.users', 'operating_unit_users_rel', 'poid', 'user_id',
-        'Users Allowed',
+        "res.users", "operating_unit_users_rel", "poid", "user_id", "Users Allowed"
     )
 
     _sql_constraints = [
-        ('code_company_uniq', 'unique (code,company_id)',
-         'The code of the operating unit must '
-         'be unique per company!'),
-        ('name_company_uniq', 'unique (name,company_id)',
-         'The name of the operating unit must '
-         'be unique per company!')
+        (
+            "code_company_uniq",
+            "unique (code,company_id)",
+            "The code of the operating unit must " "be unique per company!",
+        ),
+        (
+            "name_company_uniq",
+            "unique (name,company_id)",
+            "The name of the operating unit must " "be unique per company!",
+        ),
     ]
 
     @api.model
-    def name_search(self, name='', args=None, operator='ilike', limit=100):
+    def name_search(self, name="", args=None, operator="ilike", limit=100):
         # Make a search with default criteria
         names1 = super(models.Model, self).name_search(
-            name=name, args=args, operator=operator, limit=limit)
+            name=name, args=args, operator=operator, limit=limit
+        )
         # Make the other search
         names2 = []
         if name:
-            domain = [('code', '=ilike', name + '%')]
+            domain = [("code", "=ilike", name + "%")]
             names2 = self.search(domain, limit=limit).name_get()
         # Merge both results
         return list(set(names1) | set(names2))[:limit]
