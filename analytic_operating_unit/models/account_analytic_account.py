eab377989751572fe6d3614e7b19f93d170c08c6
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-12-30 16:25:26 -0600

    [IMP] analytic_operating_unit: black, isort

--- analytic_operating_unit/models/account_analytic_account.py
+++ analytic_operating_unit/models/account_analytic_account.py
@@ -5,11 +5,13 @@ from odoo import fields, models
 
 
 class AccountAnalyticAccount(models.Model):
-    _inherit = 'account.analytic.account'
+    _inherit = "account.analytic.account"
 
     operating_unit_ids = fields.Many2many(
-        comodel_name='operating.unit', string='Operating Units',
-        relation='analytic_account_operating_unit_rel',
+        comodel_name="operating.unit",
+        string="Operating Units",
+        relation="analytic_account_operating_unit_rel",
         domain="[('user_ids', '=', uid)]",
-        column1='analytic_account_id',
-        column2='operating_unit_id')
+        column1="analytic_account_id",
+        column2="operating_unit_id",
+    )
