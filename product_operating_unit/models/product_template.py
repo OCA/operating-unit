8f90c080f8e9f0a63de62f6660ca7b955f829388
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2019-12-30 11:53:39 +0530

    [ADD] Added a product_operating_unit v12

--- product_operating_unit/models/product_template.py
+++ product_operating_unit/models/product_template.py
@@ -1,10 +1,7 @@
-##############################################################################
-#
-#    Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
-#    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-#
-##############################################################################
-
+# Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
+# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2019 Serpent Consulting Services
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
@@ -12,24 +9,34 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    @api.model
     def _default_operating_unit_ids(self):
+        if self.categ_id and self.categ_id.operating_unit_ids:
+            return [(6, 0, self.categ_id.operating_unit_ids.ids)]
         return [(6, 0,
                  [self.env['res.users'].operating_unit_default_get(
                      self.env.uid).id])]
 
     operating_unit_ids = fields.Many2many(
-        'operating.unit', 'product_operating_unit_rel',
-        string='Operating Unit',
-        default=_default_operating_unit_ids
-    )
+        'operating.unit',
+        'product_operating_unit_rel',
+        string='Operating Units',
+        default=_default_operating_unit_ids)
 
-    @api.constrains('operating_unit_ids', 'company_id')
-    def _check_company_operating_unit(self):
+    @api.multi
+    @api.constrains('operating_unit_ids', 'categ_id')
+    def _check_operating_unit(self):
         for record in self:
-            if record.company_id and record.operating_unit_ids:
-                for operating_unit in record.operating_unit_ids:
-                    if record.company_id != operating_unit.company_id:
-                        raise ValidationError(
-                            _('Configuration error. The Company in the Product'
-                              ' and in the Operating Unit must be the same.')
-                        )
+            if record.categ_id.operating_unit_ids and \
+                    not all(ou in record.operating_unit_ids.ids for
+                            ou in record.categ_id.operating_unit_ids.ids):
+                raise ValidationError(_(
+                    "The operating units of the product must include the "
+                    "ones from the category."))
+
+    @api.multi
+    @api.onchange('categ_id')
+    def onchange_operating_unit_ids(self):
+        for record in self:
+            record.operating_unit_ids = \
+                [(6, 0, record.categ_id.operating_unit_ids.ids)]
--- product_operating_unit/tests/test_product_operating_unit.py
+++ product_operating_unit/tests/test_product_operating_unit.py
@@ -1,11 +1,9 @@
-##############################################################################
-#
-#    Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
-#    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-#
-##############################################################################
-
+# Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
+# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2019 Serpent Consulting Services
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo.tests import common
+from odoo.exceptions import ValidationError
 
 
 class TestProductOperatingUnit(common.TransactionCase):
@@ -14,11 +12,13 @@ class TestProductOperatingUnit(common.TransactionCase):
         super(TestProductOperatingUnit, self).setUp()
         self.ResUsers = self.env['res.users']
         self.ProductTemplate = self.env['product.template']
+        self.ProductCategory = self.env['product.category']
         # company
-        self.company1 = self.env.ref('base.main_company')
-        self.company2 = self.env.ref('stock.res_company_1')
+        self.company = self.env.ref('base.main_company')
         # groups
-        self.group_stock_user = self.env.ref('stock.group_stock_user')
+        self.group_stock_manager = self.env.ref('stock.group_stock_manager')
+        self.grp_acc_user = self.env.ref('account.group_account_invoice')
+        self.grp_stock_user = self.env.ref('stock.group_stock_user')
         # Main Operating Unit
         self.ou1 = self.env.ref('operating_unit.main_operating_unit')
         # B2B Operating Unit
@@ -31,17 +31,20 @@ class TestProductOperatingUnit(common.TransactionCase):
         self.product3 = self.env.ref(
             'product.product_product_11_product_template')
         # Create users
-        self.user1_id = self._create_user('user_1',
-                                          [self.group_stock_user],
-                                          self.company1,
-                                          [self.ou1])
-        self.user2_id = self._create_user('user_2',
-                                          [self.group_stock_user],
-                                          self.company2,
-                                          [self.b2b])
-        self.product1.operating_unit_ids = [(6, 0, [self.ou1.id])]
-        self.product2.operating_unit_ids = [(6, 0, [self.b2b.id])]
-        self.product3.operating_unit_ids = [(6, 0, [self.ou1.id, self.b2b.id])]
+        self.user1_id = self._create_user(
+            'user_1',
+            [self.grp_stock_user, self.grp_acc_user, self.group_stock_manager],
+            self.company, [self.ou1, self.b2b]
+        )
+        self.user2_id = self._create_user(
+            'user_2',
+            [self.grp_stock_user, self.grp_acc_user, self.group_stock_manager],
+            self.company, [self.b2b]
+        )
+        self.product1.categ_id.operating_unit_ids = [(6, 0, [self.ou1.id])]
+        self.product2.categ_id.operating_unit_ids = [(6, 0, [self.b2b.id])]
+        self.product3.categ_id.operating_unit_ids = \
+            [(6, 0, [self.ou1.id, self.b2b.id])]
 
     def _create_user(self, login, groups, company, operating_units):
         """ Create a user."""
@@ -60,35 +63,45 @@ class TestProductOperatingUnit(common.TransactionCase):
             })
         return user.id
 
+    def test_po_ou_onchange(self):
+        with self.assertRaises(ValidationError):
+            self.product1.operating_unit_ids = [(6, 0, [self.b2b.id])]
+
+        self.product1.onchange_operating_unit_ids()
+
     def test_po_ou_security(self):
         """Test Security of Product Operating Unit"""
 
         # User 1 is only assigned to Operating Unit 1, and can see all
         # products having Operating Unit 1.
+        ou_domain = [('operating_unit_ids', 'in', self.ou1.id)]
         product_ids = \
-            self.ProductTemplate.sudo(self.user1_id).search(
-                [('operating_unit_ids', 'in', self.ou1.id)]).ids
-        self.assertEqual(set(product_ids), set([self.product1.id,
-                                                self.product3.id]))
+            self.ProductTemplate.sudo(self.user1_id).search(ou_domain).ids
+        category_ids = \
+            self.ProductCategory.sudo(self.user1_id).search(ou_domain).ids
+        self.assertIn(category_ids[0], product_ids)
 
         # User 2 is only assigned to Operating Unit 2, so cannot see products
         # having Operating Unit 1, expect those also having Operating Unit b2b
         product_ids = \
-            self.ProductTemplate.sudo(self.user2_id).search(
-                [('operating_unit_ids', 'in', self.ou1.id)]).ids
-        self.assertEqual(product_ids, [self.product3.id])
+            self.ProductTemplate.sudo(self.user2_id).search(ou_domain).ids
+        category_ids = \
+            self.ProductCategory.sudo(self.user2_id).search(ou_domain).ids
+        self.assertIn(category_ids[0], product_ids)
 
         # User 2 is only assigned to Operating Unit 2, and can see all
         # products having Operating Unit b2b.
+        b2b_domain = [('operating_unit_ids', 'in', self.b2b.id)]
         product_ids = \
-            self.ProductTemplate.sudo(self.user2_id).search(
-                [('operating_unit_ids', 'in', self.b2b.id)]).ids
-        self.assertEqual(set(product_ids),
-                         set([self.product2.id, self.product3.id]))
+            self.ProductTemplate.sudo(self.user2_id).search(b2b_domain).ids
+        category_ids = \
+            self.ProductCategory.sudo(self.user2_id).search(b2b_domain).ids
+        self.assertIn(category_ids[0], product_ids)
 
         # User 1 is only assigned to Operating Unit 1, so cannot see products
         # having Operating Unit b2b, expect those also having Operating Unit 1
         product_ids = \
-            self.ProductTemplate.sudo(self.user1_id).search(
-                [('operating_unit_ids', 'in', self.b2b.id)]).ids
-        self.assertEqual(product_ids, [self.product3.id])
+            self.ProductTemplate.sudo(self.user1_id).search(b2b_domain).ids
+        category_ids = \
+            self.ProductCategory.sudo(self.user2_id).search(b2b_domain).ids
+        self.assertIn(category_ids[0], product_ids)
