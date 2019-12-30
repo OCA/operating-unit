e27fd466a04f53ad61c7777969cc32b73109ac7e
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2019-12-30 11:53:39 +0530

    [ADD] Added a product_operating_unit v12

--- product_operating_unit/__init__.py
+++ product_operating_unit/__init__.py
@@ -1,9 +1,2 @@
-##############################################################################
-#
-#    Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
-#    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-#
-##############################################################################
-
-
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import models
--- product_operating_unit/models/__init__.py
+++ product_operating_unit/models/__init__.py
@@ -1,10 +1,3 @@
-##############################################################################
-#
-#    Copyright (c) 2019 brain-tec AG (http://www.braintec-group.com)
-#    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-#
-##############################################################################
-
-
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import product_template
-
+from . import product_category
