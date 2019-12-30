012cf58a87ad70583e03b458b9e0ce834941b22d
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-12-30 16:25:26 -0600

    [IMP] analytic_operating_unit: black, isort

--- analytic_operating_unit/__manifest__.py
+++ analytic_operating_unit/__manifest__.py
@@ -3,18 +3,14 @@
 {
     "name": "Analytic Operating Unit",
     "version": "12.0.1.0.0",
-    "author": "Eficent, "
-              "Odoo Community Association (OCA)",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Sales",
-    "depends": [
-        "analytic",
-        "operating_unit",
-    ],
+    "depends": ["analytic", "operating_unit"],
     "data": [
         "security/analytic_account_security.xml",
         "views/analytic_account_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
