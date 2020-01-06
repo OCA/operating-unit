4bdd66c6fa9a7eba6a049f9572f3a1bd556f48d7
BT-nstuder <nicola.studer@braintec-group.com>
2020-01-06 14:58:20 +0100

    [IMP] stock_operating_unit: black, isort, prettier

--- stock_operating_unit/hooks.py
+++ stock_operating_unit/hooks.py
@@ -7,16 +7,17 @@ from odoo.api import Environment
 
 def update_operating_unit_location(cr, registry):
     env = Environment(cr, SUPERUSER_ID, {})
-    warehouses = env['stock.warehouse'].search([])
+    warehouses = env["stock.warehouse"].search([])
     for warehouse in warehouses:
         operating_unit = warehouse.operating_unit_id
         parent_location = warehouse.view_location_id
-        locations = env['stock.location'].search(
-            [('id', 'child_of', [parent_location.id]),
-             ('usage', '=', 'internal')])
+        locations = env["stock.location"].search(
+            [("id", "child_of", [parent_location.id]), ("usage", "=", "internal")]
+        )
         if operating_unit:
-            query = '''update stock_location set operating_unit_id = %s where
-            location_id in %s or id in %s'''
-            cr.execute(query, (operating_unit.id, tuple(locations.ids),
-                               tuple(locations.ids)))
+            query = """update stock_location set operating_unit_id = %s where
+            location_id in %s or id in %s"""
+            cr.execute(
+                query, (operating_unit.id, tuple(locations.ids), tuple(locations.ids))
+            )
     return True
