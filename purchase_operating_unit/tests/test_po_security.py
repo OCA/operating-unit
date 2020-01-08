4820eef50df33ec041a5da6512b67dc615ee3cf8
BT-nstuder <nicola.studer@braintec-group.com>
2020-01-08 08:01:51 +0100

    [MIG] purchase_operating_unit: Migration to 13.0

--- purchase_operating_unit/tests/test_po_security.py
+++ purchase_operating_unit/tests/test_po_security.py
@@ -11,44 +11,44 @@ class TestPoSecurity(test_po_ou.TestPurchaseOperatingUnit):
         # User 2 is only assigned to Operating Unit 2, and cannot list
         # purchase orders from Operating Unit 1.
         po_ids = (
-            self.PurchaseOrder.sudo(self.user2_id)
+            self.PurchaseOrder.with_user(self.user2_id)
             .search([("operating_unit_id", "=", self.ou1.id)])
             .ids
         )
         self.assertEqual(po_ids, [])
         # User 2 cannot list the picking that was created from PO 1
         picking_ids = (
-            self.StockPicking.sudo(self.user2_id)
+            self.StockPicking.with_user(self.user2_id)
             .search([("id", "in", self.purchase1.picking_ids.ids)])
             .ids
         )
         self.assertEqual(picking_ids, [])
         # User 2 cannot list the invoice that was created from PO 1
         invoice_ids = (
-            self.AccountInvoice.sudo(self.user2_id)
-            .search([("purchase_id", "=", self.purchase1.id)])
+            self.AccountInvoice.with_user(self.user2_id)
+            .search([("id", "=", self.invoice.id)])
             .ids
         )
         self.assertEqual(invoice_ids, [])
         # User 1 is assigned to Operating Unit 1, and can list
         # the purchase order 1 from Operating Unit 1.
         po_ids = (
-            self.PurchaseOrder.sudo(self.user1_id)
+            self.PurchaseOrder.with_user(self.user1_id)
             .search([("operating_unit_id", "=", self.ou1.id)])
             .ids
         )
         self.assertNotEqual(po_ids, [])
         # User 1 can list the picking that was created from PO 1
         picking_ids = (
-            self.StockPicking.sudo(self.user1_id)
+            self.StockPicking.with_user(self.user1_id)
             .search([("id", "in", self.purchase1.picking_ids.ids)])
             .ids
         )
         self.assertNotEqual(picking_ids, [])
-        #        # User 1 can list the invoice that was created from PO 2
+        # User 1 can list the invoice that was created from PO 2
         invoice_ids = (
-            self.AccountInvoice.sudo(self.user1_id)
-            .search([("purchase_id", "=", self.purchase1.id)])
+            self.AccountInvoice.with_user(self.user1_id)
+            .search([("id", "=", self.invoice.id)])
             .ids
         )
         self.assertNotEqual(invoice_ids, [])
