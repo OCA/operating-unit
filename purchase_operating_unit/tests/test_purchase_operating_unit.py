22b10f38d3f40f62b1a8c2a73cd760a4b6943248
BT-nstuder <nicola.studer@braintec-group.com>
2020-01-08 08:01:51 +0100

    [MIG] purchase_operating_unit: Migration to 13.0

--- purchase_operating_unit/tests/test_purchase_operating_unit.py
+++ purchase_operating_unit/tests/test_purchase_operating_unit.py
@@ -14,7 +14,7 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
         self.ResUsers = self.env["res.users"]
         self.StockPicking = self.env["stock.picking"]
         self.PurchaseOrder = self.env["purchase.order"]
-        self.AccountInvoice = self.env["account.invoice"]
+        self.AccountInvoice = self.env["account.move"]
         # company
         self.company1 = self.env.ref("base.main_company")
         self.company2 = self.env.ref("stock.res_company_1")
@@ -32,7 +32,7 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
         self.product2 = self.env.ref("product.product_product_9")
         self.product3 = self.env.ref("product.product_product_11")
         # Account
-        self.account = self.env.ref("l10n_generic_coa.conf_a_pay")
+        self.account = self.env.ref("l10n_generic_coa.payable")
         # Create users
         self.user1_id = self._create_user(
             "user_1",
@@ -50,8 +50,8 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
             self.user1_id,
             [(self.product1, 1000), (self.product2, 500), (self.product3, 800)],
         )
-        self.purchase1.sudo(self.user1_id).button_confirm()
-        self._create_invoice(self.purchase1, self.partner1, self.account)
+        self.purchase1.with_user(self.user1_id).button_confirm()
+        self.invoice = self._create_invoice(self.purchase1, self.partner1, self.account)
 
     def _create_user(self, login, groups, company, operating_units):
         """ Create a user."""
@@ -85,7 +85,7 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
                 "date_planned": time.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
             }
             lines.append((0, 0, line_values))
-        purchase = self.PurchaseOrder.sudo(user_id).create(
+        purchase = self.PurchaseOrder.with_user(user_id).create(
             {
                 "operating_unit_id": self.ou1.id,
                 "requesting_operating_unit_id": self.ou1.id,
@@ -101,7 +101,7 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
         invoice_vals = {
             "purchase_id": purchase.id,
             "partner_id": partner.id,
-            "account_id": account.id,
+            # "account_id": account.id,
             "type": "in_invoice",
         }
         purchase_context = {
@@ -109,5 +109,7 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
             "active_ids": purchase.ids,
             "active_model": "purchase.order",
         }
-        self.env["account.invoice"].with_context(purchase_context).create(invoice_vals)
-        return True
+        res = (
+            self.env["account.move"].with_context(purchase_context).create(invoice_vals)
+        )
+        return res
