0d6adf23a7f9d2df9e51c7c32fccedafd3a165e0
BT-nstuder <nicola.studer@braintec-group.com>
2020-01-08 08:01:51 +0100

    [MIG] purchase_operating_unit: Migration to 13.0

--- purchase_operating_unit/models/account_invoice.py
+++ purchase_operating_unit/models/account_invoice.py
@@ -5,35 +5,37 @@
 from odoo import _, api, exceptions, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     # Load all unsold PO lines
-    @api.onchange("purchase_id")
-    def purchase_order_change(self):
+    @api.onchange("purchase_vendor_bill_id", "purchase_id")
+    def _onchange_purchase_auto_complete(self):
         """
         Override to add Operating Unit from Purchase Order to Invoice.
         """
-        if self.purchase_id and self.purchase_id.operating_unit_id:
+        purchase_id = self.purchase_id
+        if self.purchase_vendor_bill_id.purchase_order_id:
+            purchase_id = self.purchase_vendor_bill_id.purchase_order_id
+        if purchase_id and purchase_id.operating_unit_id:
             # Assign OU from PO to Invoice
-            self.operating_unit_id = self.purchase_id.operating_unit_id.id
-        return super(AccountInvoice, self).purchase_order_change()
+            self.operating_unit_id = purchase_id.operating_unit_id.id
+        return super()._onchange_purchase_auto_complete()
 
     @api.onchange("operating_unit_id")
-    def _onchange_allowed_purchase_ids(self):
+    def _onchange_operating_unit_id(self):
         """
         Show only the purchase orders that have the same operating unit
         """
-        result = super(AccountInvoice, self)._onchange_allowed_purchase_ids()
+        return {
+            "domain": {
+                "purchase_id": [("operating_unit_id", "=", self.operating_unit_id.id)]
+            }
+        }
 
-        result["domain"]["purchase_id"] += [
-            ("operating_unit_id", "=", self.operating_unit_id.id)
-        ]
-        return result
 
-
-class AccountInvoiceLines(models.Model):
-    _inherit = "account.invoice.line"
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
     @api.constrains("operating_unit_id", "purchase_line_id")
     def _check_invoice_ou(self):
