527a294715de74996cdcf0ea9fc1c56cca770522
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-12 16:07:37 -0600

    [IMP] sale_operating_unit: black, isort

--- sale_operating_unit/models/crm_team.py
+++ sale_operating_unit/models/crm_team.py
@@ -7,18 +7,29 @@ from odoo.exceptions import ValidationError
 
 
 class CrmTeam(models.Model):
-    _inherit = 'crm.team'
+    _inherit = "crm.team"
 
     @api.multi
-    @api.constrains('operating_unit_id')
+    @api.constrains("operating_unit_id")
     def _check_sales_order_operating_unit(self):
         for rec in self:
-            orders = self.sudo().env['sale.order'].search(
-                [('team_id', '=', rec.id), ('operating_unit_id', '!=',
-                                            rec.operating_unit_id.id)])
+            orders = (
+                self.sudo()
+                .env["sale.order"]
+                .search(
+                    [
+                        ("team_id", "=", rec.id),
+                        ("operating_unit_id", "!=", rec.operating_unit_id.id),
+                    ]
+                )
+            )
             if orders:
-                raise ValidationError(_('Configuration error. It is not '
-                                        'possible to change this '
-                                        'team. There are sale orders '
-                                        'referencing it in other operating '
-                                        'units'))
+                raise ValidationError(
+                    _(
+                        "Configuration error. It is not "
+                        "possible to change this "
+                        "team. There are sale orders "
+                        "referencing it in other operating "
+                        "units"
+                    )
+                )
