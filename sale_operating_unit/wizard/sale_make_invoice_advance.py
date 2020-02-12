c4ed976c04d74e3f459755de8b69313de5cee058
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-12 16:07:37 -0600

    [IMP] sale_operating_unit: black, isort

--- sale_operating_unit/wizard/sale_make_invoice_advance.py
+++ sale_operating_unit/wizard/sale_make_invoice_advance.py
@@ -7,6 +7,7 @@ class SaleAdvancePaymentInv(models.TransientModel):
     @api.multi
     def _create_invoice(self, order, so_line, amount):
         invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
-            order, so_line, amount)
+            order, so_line, amount
+        )
         invoice.operating_unit_id = order.operating_unit_id.id
         return invoice
