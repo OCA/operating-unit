89458bb433b0fb8d4a14f3de63c677c98de69156
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-12 16:07:37 -0600

    [IMP] sale_operating_unit: black, isort

--- sale_operating_unit/report/sale_report.py
+++ sale_operating_unit/report/sale_report.py
@@ -7,14 +7,11 @@ class SaleReport(models.Model):
 
     _inherit = "sale.report"
 
-    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit')
+    operating_unit_id = fields.Many2one("operating.unit", "Operating Unit")
 
-    def _query(self, with_clause='', fields=False,  # noqa
-               groupby='', from_clause=''):
+    def _query(self, with_clause="", fields=False, groupby="", from_clause=""):  # noqa
         if not fields:
             fields = {}
-        fields['operating_unit_id'] = \
-            ", s.operating_unit_id as operating_unit_id"
-        groupby += ', s.operating_unit_id'
-        return super(SaleReport, self)._query(with_clause, fields,
-                                              groupby, from_clause)
+        fields["operating_unit_id"] = ", s.operating_unit_id as operating_unit_id"
+        groupby += ", s.operating_unit_id"
+        return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
