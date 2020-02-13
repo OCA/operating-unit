d3256374afe5a2023b8c6d43894ac9169c843d2d
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-12 18:06:35 -0600

    [IMP] sale_stock_operating_unit: black, isort

--- sale_stock_operating_unit/__manifest__.py
+++ sale_stock_operating_unit/__manifest__.py
@@ -6,17 +6,13 @@
 {
     "name": "Operating Unit in Sales Stock",
     "summary": "An operating unit (OU) is an organizational entity part of a "
-               "company",
+    "company",
     "version": "12.0.1.0.1",
     "author": "Eficent, Serpent Consulting Services Pvt. Ltd., "
-              "Odoo Community Association (OCA)",
+    "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Sales Management",
-    "depends": [
-        "sale_stock",
-        "sale_operating_unit",
-        "stock_operating_unit",
-    ],
-    'installable': True,
+    "depends": ["sale_stock", "sale_operating_unit", "stock_operating_unit"],
+    "installable": True,
 }
--- sale_stock_operating_unit/models/stock_warehouse.py
+++ sale_stock_operating_unit/models/stock_warehouse.py
@@ -2,21 +2,27 @@
 # Jordi Ballester Alomar
 # Copyright 2015-19 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
 class StockWarehouse(models.Model):
-    _inherit = 'stock.warehouse'
+    _inherit = "stock.warehouse"
 
     @api.multi
-    @api.constrains('operating_unit_id')
+    @api.constrains("operating_unit_id")
     def _check_existing_so_in_wh(self):
         for rec in self:
-            sales = self.env['sale.order'].search([
-                ('warehouse_id', '=', rec.id),
-                ('operating_unit_id', '!=', rec.operating_unit_id.id)])
+            sales = self.env["sale.order"].search(
+                [
+                    ("warehouse_id", "=", rec.id),
+                    ("operating_unit_id", "!=", rec.operating_unit_id.id),
+                ]
+            )
             if sales:
-                raise ValidationError(_(
-                    'Sales Order records already exist(s) for this warehouse'
-                    ' and operating unit.'))
+                raise ValidationError(
+                    _(
+                        "Sales Order records already exist(s) for this warehouse"
+                        " and operating unit."
+                    )
+                )
