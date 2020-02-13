44101bee42de647d7c136c6a9115386e5c16cac1
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-13 09:18:25 -0600

    [IMP] report_qweb_operating_unit: black, isort

--- report_qweb_operating_unit/__manifest__.py
+++ report_qweb_operating_unit/__manifest__.py
@@ -3,19 +3,15 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Qweb Report With Operating Unit',
-    'version': '12.0.1.0.0',
-    'category': 'Reports/Qweb',
-    'license': 'LGPL-3',
-    'author': 'Eficent Business and IT Consulting Services S.L., '
-              'Serpent Consulting Services Pvt. Ltd.,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://odoo-community.org/',
-    'depends': [
-        'operating_unit'
-    ],
-    'data': [
-        'views/report_qweb_operating_unit.xml',
-    ],
-    'installable': True,
+    "name": "Qweb Report With Operating Unit",
+    "version": "12.0.1.0.0",
+    "category": "Reports/Qweb",
+    "license": "LGPL-3",
+    "author": "Eficent Business and IT Consulting Services S.L., "
+    "Serpent Consulting Services Pvt. Ltd.,"
+    "Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org/",
+    "depends": ["operating_unit"],
+    "data": ["views/report_qweb_operating_unit.xml"],
+    "installable": True,
 }
