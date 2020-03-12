b1726df8dfb9f38c9456294541aeb3a48c88182c
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-03-12 09:16:09 -0600

    Update __init__.py

--- stock_operating_unit/model/__init__.py
+++ stock_operating_unit/model/__init__.py
@@ -1,8 +1,9 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
-from . import stock_location
-from . import stock_move
-from . import stock_picking
-from . import stock_rule
-from . import stock_warehouse
-from . import product_product
+from . import (
+    stock_location,
+    stock_move,
+    stock_picking,
+    stock_rule,
+    stock_warehouse,
+    product_product,
+)
