862b514afd3f6baf31531b58a4c138b549e729b5
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-06 11:49:00 +0530

    [IMP]Improved code as par flake8 standard.

--- sale_multi_operating_unit/models/crm_lead.py
+++ sale_multi_operating_unit/models/crm_lead.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class CRMLead(models.Model):
