469a6cbd598dabbb1e83f7475787cbd4251cfb44
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-04-15 11:15:28 -0500

    [MIG] account_operating_unit: Migration to V13

--- account_operating_unit/__manifest__.py
+++ account_operating_unit/__manifest__.py
@@ -5,21 +5,20 @@
     "name": "Accounting with Operating Units",
     "summary": "Introduces Operating Unit (OU) in invoices and "
     "Accounting Entries with clearing account",
-    "version": "12.0.1.0.2",
+    "version": "13.0.1.0.0",
     "author": "Eficent, "
     "Serpent Consulting Services Pvt. Ltd.,"
     "WilldooIT Pty Ltd,"
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Accounting & Finance",
-    "depends": ["account", "operating_unit", "analytic_operating_unit"],
+    "depends": ["account", "analytic_operating_unit"],
     "license": "LGPL-3",
     "data": [
         "security/account_security.xml",
         "views/account_move_view.xml",
         "views/account_journal_view.xml",
         "views/company_view.xml",
-        "views/invoice_view.xml",
         "views/account_payment_view.xml",
         "views/account_invoice_report_view.xml",
     ],
--- account_operating_unit/report/account_invoice_report.py
+++ account_operating_unit/report/account_invoice_report.py
@@ -9,25 +9,20 @@ class AccountInvoiceReport(models.Model):
 
     _inherit = "account.invoice.report"
 
-    operating_unit_id = fields.Many2one("operating.unit", "Operating Unit")
+    operating_unit_id = fields.Many2one(
+        comodel_name="operating.unit", string="Operating Unit",
+    )
 
     def _select(self):
-        select_str = super(AccountInvoiceReport, self)._select()
+        select_str = super()._select()
         select_str += """
-            ,sub.operating_unit_id
-        """
-        return select_str
-
-    def _sub_select(self):
-        select_str = super(AccountInvoiceReport, self)._sub_select()
-        select_str += """
-            ,ai.operating_unit_id
+            ,line.operating_unit_id
         """
         return select_str
 
     def _group_by(self):
-        group_by_str = super(AccountInvoiceReport, self)._group_by()
+        group_by_str = super()._group_by()
         group_by_str += """
-            ,ai.operating_unit_id
+            ,line.operating_unit_id
         """
         return group_by_str
