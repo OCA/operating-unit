bb8b94865b63696741528f53c5ca35bbac845058
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-08-06 17:32:54 +0530

    [MIG]res_partner_operating_unit: Migration to 13.0

--- res_partner_operating_unit/__manifest__.py
+++ res_partner_operating_unit/__manifest__.py
@@ -1,18 +1,18 @@
 # © 2017 Niaga Solution - Edi Santoso <repodevs@gmail.com>
+# Copyright (C) 2020 Serpent Consulting Services
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Partner with Operating Unit",
     "summary": "Introduces Operating Unit fields in Partner",
-    "version": "12.0.1.1.1",
+    "version": "14.0.1.0.0",
     "author": "Edi Santoso, "
-              "Niaga Solution, "
-              "Odoo Community Association (OCA)",
+    "Niaga Solution, "
+    "Serpent Consulting Services Pvt. Ltd., "
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Generic",
     "depends": ["operating_unit"],
     "license": "LGPL-3",
-    "data": [
-        "views/res_partner_view.xml",
-    ],
-    'installable': True,
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
 }
