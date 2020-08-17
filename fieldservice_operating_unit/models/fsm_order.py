14d4a5fbb20712ed3a5d038f308a1b1aa79c6a0a
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2020-08-17 09:21:47 +0530

    [MIG] Migrated fieldservice_operating_unit in v13

--- fieldservice_operating_unit/models/fsm_order.py
+++ fieldservice_operating_unit/models/fsm_order.py
@@ -5,11 +5,12 @@ from odoo import fields, models
 
 
 class FSMOrder(models.Model):
-    _inherit = 'fsm.order'
+    _inherit = "fsm.order"
 
     operating_unit_id = fields.Many2one(
-        comodel_name='operating.unit',
-        string='Operating Unit',
-        default=lambda self: self.env['res.users'].operating_unit_default_get(
-            self._uid)
+        comodel_name="operating.unit",
+        string="Operating Unit",
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
+            self._uid
+        ),
     )
