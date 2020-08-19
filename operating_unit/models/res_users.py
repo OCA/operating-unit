0743b27eca63e23766b0a53b4ec36b39236fd7e2
Luc De Meyer <luc.demeyer@noviat.com>
2020-08-19 17:12:40 +0200

    [13.0]fix operating_unit record rules

--- operating_unit/models/res_users.py
+++ operating_unit/models/res_users.py
@@ -24,16 +24,43 @@ class ResUsers(models.Model):
     def _default_operating_units(self):
         return self._default_operating_unit()
 
-    operating_unit_ids = fields.Many2many(
-        "operating.unit",
-        "operating_unit_users_rel",
-        "user_id",
-        "operating_unit_id",
-        "Operating Units",
+    operating_unit_ids = fields.One2many(
+        comodel_name="operating.unit",
+        compute="_compute_operating_unit_ids",
+        inverse="_inverse_operating_unit_ids",
+    )
+    assigned_operating_unit_ids = fields.Many2many(
+        comodel_name="operating.unit",
+        relation="operating_unit_users_rel",
+        column1="user_id",
+        column2="operating_unit_id",
+        string="Operating Units",
         default=lambda self: self._default_operating_units(),
     )
+
     default_operating_unit_id = fields.Many2one(
-        "operating.unit",
-        "Default Operating Unit",
+        comodel_name="operating.unit",
+        string="Default Operating Unit",
         default=lambda self: self._default_operating_unit(),
     )
+
+    @api.depends("groups_id", "assigned_operating_unit_ids")
+    def _compute_operating_unit_ids(self):
+        for user in self:
+            if user.has_group("operating_unit.group_manager_operating_unit"):
+                dom = []
+                if self.env.context.get("allowed_company_ids"):
+                    dom = [
+                        "|",
+                        ("company_id", "=", False),
+                        ("company_id", "in", self.env.context["allowed_company_ids"]),
+                    ]
+                else:
+                    dom = []
+                user.operating_unit_ids = self.env["operating.unit"].sudo().search(dom)
+            else:
+                user.operating_unit_ids = user.assigned_operating_unit_ids
+
+    def _inverse_operating_unit_ids(self):
+        for user in self:
+            user.assigned_operating_unit_ids = user.operating_unit_ids
