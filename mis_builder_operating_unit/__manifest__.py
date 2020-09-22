c8e3761b3528e42305d21cef768954685a3596e5
Holger Brunn <mail@hunki-enterprises.com>
2020-09-22 19:18:12 +0200

    [IMP] mis_builder_operating_unit: black, isort, prettier

--- mis_builder_operating_unit/__manifest__.py
+++ mis_builder_operating_unit/__manifest__.py
@@ -4,19 +4,16 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'MIS Builder with Operating Unit',
-    'version': '12.0.1.0.0',
-    'category': 'Reporting',
+    "name": "MIS Builder with Operating Unit",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
     "author": "Eficent Business and IT Consulting Services S.L., "
-              "Serpent Consulting Services Pvt. Ltd.,"
-              "ACSONE SA/NV,"
-              "Odoo Community Association (OCA)",
+    "Serpent Consulting Services Pvt. Ltd.,"
+    "ACSONE SA/NV,"
+    "Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    'website': 'https://github.com/operating-unit',
-    'depends': ['mis_builder', 'account_operating_unit'],
-    'data': [
-        'security/mis_builder_security.xml',
-        'view/mis_builder.xml'
-    ],
-    'installable': True,
+    "website": "https://github.com/operating-unit",
+    "depends": ["mis_builder", "account_operating_unit"],
+    "data": ["security/mis_builder_security.xml", "view/mis_builder.xml"],
+    "installable": True,
 }
