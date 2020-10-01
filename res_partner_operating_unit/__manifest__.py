4e01608d22438350ca969e3b324678f0446c9338
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2020-10-01 09:04:24 +0530

    [backport] res_partner_operating_unit v12 to v11

--- res_partner_operating_unit/__manifest__.py
+++ res_partner_operating_unit/__manifest__.py
@@ -3,16 +3,12 @@
 {
     "name": "Partner with Operating Unit",
     "summary": "Introduces Operating Unit fields in Partner",
-    "version": "12.0.1.1.0",
-    "author": "Edi Santoso, "
-              "Niaga Solution, "
-              "Odoo Community Association (OCA)",
+    "version": "11.0.1.0.0",
+    "author": "Edi Santoso, " "Niaga Solution, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Generic",
     "depends": ["operating_unit"],
     "license": "LGPL-3",
-    "data": [
-        "views/res_partner_view.xml",
-    ],
-    'installable': True,
+    "data": ["views/res_partner_view.xml",],
+    "installable": True,
 }
--- res_partner_operating_unit/readme/CONTRIBUTORS.rst
+++ res_partner_operating_unit/readme/CONTRIBUTORS.rst
@@ -1,3 +1,4 @@
 * Edi Santoso <repodevs@gmail.com>
 * Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
 * Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
+* Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
