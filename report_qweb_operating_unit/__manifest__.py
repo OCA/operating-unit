b30e1fac2b449bcc34104dfede01f996156bb8cf
mreficent <miquel.raich@eficent.com>
2020-10-05 10:42:21 +0200

    [UPD] Eficent -> ForgeFlow

--- report_qweb_operating_unit/__manifest__.py
+++ report_qweb_operating_unit/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -7,7 +7,7 @@
     "version": "13.0.1.0.0",
     "category": "Reports/Qweb",
     "license": "LGPL-3",
-    "author": "Eficent Business and IT Consulting Services S.L., "
+    "author": "ForgeFlow S.L., "
     "Serpent Consulting Services Pvt. Ltd.,"
     "Odoo Community Association (OCA)",
     "website": "https://odoo-community.org/",
