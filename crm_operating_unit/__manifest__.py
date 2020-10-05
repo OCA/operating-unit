d406a100c987c1e9df26cce263ad99c553fd936f
mreficent <miquel.raich@eficent.com>
2020-10-05 10:42:21 +0200

    [UPD] Eficent -> ForgeFlow

--- crm_operating_unit/__manifest__.py
+++ crm_operating_unit/__manifest__.py
@@ -1,12 +1,11 @@
-# © 2015-19 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
+# © 2015-19 ForgeFlow S.L. - Jordi Ballester Alomar
 # © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in CRM",
     "version": "13.0.1.0.0",
-    "author": "Eficent, "
+    "author": "ForgeFlow, "
     "Serpent Consulting Services Pvt. Ltd.,"
     "Odoo Community Association (OCA)",
     "license": "LGPL-3",
