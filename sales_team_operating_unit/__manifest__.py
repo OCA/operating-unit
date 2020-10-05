2e5086d7da1315f427e4f143e1253e98c6c66d17
mreficent <miquel.raich@eficent.com>
2020-10-05 10:42:21 +0200

    Eficent -> ForgeFlow

--- sales_team_operating_unit/__manifest__.py
+++ sales_team_operating_unit/__manifest__.py
@@ -1,12 +1,11 @@
-# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# Copyright 2016-17 ForgeFlow S.L. (http://www.forgeflow.com)
 # Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
 #   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Sales Team Operating Unit",
     "version": "13.0.1.0.0",
-    "author": "Eficent, SerpentCS, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, SerpentCS, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Sales",
