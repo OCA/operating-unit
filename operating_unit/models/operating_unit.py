40f5f7cf82ad64cda7f8c9938c87e0388115dcd0
mreficent <miquel.raich@eficent.com>
2020-10-05 10:42:21 +0200

    [UPD] Eficent -> ForgeFlow

--- operating_unit/models/operating_unit.py
+++ operating_unit/models/operating_unit.py
@@ -1,4 +1,4 @@
-# Copyright 2015-TODAY Eficent
+# Copyright 2015-TODAY ForgeFlow
 # - Jordi Ballester Alomar
 # Copyright 2015-TODAY Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
