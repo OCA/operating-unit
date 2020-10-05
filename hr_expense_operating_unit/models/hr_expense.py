2cbcd8f08a796eb670dedc2aa6397c8f7dee0b08
mreficent <miquel.raich@eficent.com>
2020-10-05 10:42:21 +0200

    [UPD] Eficent -> ForgeFlow

--- hr_expense_operating_unit/models/hr_expense.py
+++ hr_expense_operating_unit/models/hr_expense.py
@@ -1,4 +1,4 @@
-# Copyright 2016-19 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-19 ForgeFlow S.L.
 # Copyright 2016-19 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).).
 
