d8701427a5dd3a4407b6aaef39cc95f4c51c6830
mreficent <miquel.raich@eficent.com>
2020-10-05 10:42:21 +0200

    [UPD] Eficent -> ForgeFlow

--- stock_operating_unit/tests/test_stock_operating_unit.py
+++ stock_operating_unit/tests/test_stock_operating_unit.py
@@ -1,4 +1,4 @@
-# © 2019 Eficent Business and IT Consulting Services S.L.
+# © 2019 ForgeFlow S.L.
 # © 2019 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo.addons.stock.tests import common
