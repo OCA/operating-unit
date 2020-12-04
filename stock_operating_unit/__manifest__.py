898e3fcf6287f06589907ac34ba3528dfe66b751
Kitti U <kittiu@gmail.com>
2020-12-04 13:55:44 +0700

    [14.0][MIG] stock_operating_unit

--- stock_operating_unit/__manifest__.py
+++ stock_operating_unit/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock with Operating Units",
     "summary": "Adds the concept of operating unit (OU) in stock management",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules/Sales & Purchases",
     "author": "ForgeFlow, "
     "Serpent Consulting Services Pvt. Ltd., "
