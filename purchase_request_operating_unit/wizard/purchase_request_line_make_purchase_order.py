ae0f79084d63fc84b1a5ccd7cc6f16e3b603d0c8
Saran440 <saranl@ecosoft.co.th>
2020-12-10 16:21:09 +0700

    [IMP] purchase_request_operating_unit: black, isort, prettier

--- purchase_request_operating_unit/wizard/purchase_request_line_make_purchase_order.py
+++ purchase_request_operating_unit/wizard/purchase_request_line_make_purchase_order.py
@@ -4,7 +4,7 @@
 #   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -12,39 +12,40 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
     _inherit = "purchase.request.line.make.purchase.order"
 
     operating_unit_id = fields.Many2one(
-        'operating.unit',
-        string='Operating Unit',
-        readonly=True,
+        "operating.unit", string="Operating Unit", readonly=True,
     )
 
     @api.model
     def default_get(self, fields):
-        res = super(PurchaseRequestLineMakePurchaseOrder, self).\
-            default_get(fields)
-        request_line_obj = self.env['purchase.request.line']
-        request_line_ids = self._context.get('active_ids', [])
+        res = super(PurchaseRequestLineMakePurchaseOrder, self).default_get(fields)
+        request_line_obj = self.env["purchase.request.line"]
+        request_line_ids = self._context.get("active_ids", [])
         operating_unit_id = False
         for line in request_line_obj.browse(request_line_ids):
-            line_operating_unit_id = line.request_id.operating_unit_id \
-                and line.request_id.operating_unit_id.id or False
-            if operating_unit_id\
-                    and line_operating_unit_id != operating_unit_id:
-                raise ValidationError(_('Could not process !'
-                                        'You have to select lines'
-                                        'from the same operating unit.'))
+            line_operating_unit_id = (
+                line.request_id.operating_unit_id
+                and line.request_id.operating_unit_id.id
+                or False
+            )
+            if operating_unit_id and line_operating_unit_id != operating_unit_id:
+                raise ValidationError(
+                    _(
+                        "Could not process !"
+                        "You have to select lines"
+                        "from the same operating unit."
+                    )
+                )
             else:
                 operating_unit_id = line_operating_unit_id
-        res['operating_unit_id'] = operating_unit_id
+        res["operating_unit_id"] = operating_unit_id
         return res
 
     @api.model
-    def _prepare_purchase_order(self, picking_type, location,
-                                company_id, origin):
-        data = super(PurchaseRequestLineMakePurchaseOrder, self).\
-            _prepare_purchase_order(picking_type, location, company_id, origin)
+    def _prepare_purchase_order(self, picking_type, location, company_id, origin):
+        data = super(
+            PurchaseRequestLineMakePurchaseOrder, self
+        )._prepare_purchase_order(picking_type, location, company_id, origin)
         if self.operating_unit_id:
-            data['requesting_operating_unit_id'] = \
-                self.operating_unit_id.id
-            data['operating_unit_id'] = \
-                self.operating_unit_id.id
+            data["requesting_operating_unit_id"] = self.operating_unit_id.id
+            data["operating_unit_id"] = self.operating_unit_id.id
         return data
