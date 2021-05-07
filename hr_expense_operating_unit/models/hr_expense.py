9264852bede94f879596abc4b82362ef8e7e7127
Kitti U <kittiu@gmail.com>
2021-05-07 17:54:05 +0700

    [13.0][FIX] hr_expense_operating_unit, misc fix typo

--- hr_expense_operating_unit/models/hr_expense.py
+++ hr_expense_operating_unit/models/hr_expense.py
@@ -69,8 +69,12 @@ class HrExpenseExpense(models.Model):
     def _get_account_move_line_values(self):
         res = super(HrExpenseExpense, self)._get_account_move_line_values()
         for expense in self:
-            res[expense.id][0].update({"operating_unit_id": self.operating_unit_id.id})
-            res[expense.id][1].update({"operating_unit_id": self.operating_unit_id.id})
+            res[expense.id][0].update(
+                {"operating_unit_id": expense.operating_unit_id.id}
+            )
+            res[expense.id][1].update(
+                {"operating_unit_id": expense.operating_unit_id.id}
+            )
         return res
 
 
