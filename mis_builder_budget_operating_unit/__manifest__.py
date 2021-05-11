b8dfc8531bdeaa5d0020143184fcf7931c4c47b9
Kitti U <kittiu@gmail.com>
2021-05-11 16:46:50 +0700

    [13.0][MIG] mis_builder_budget_operating_unit

--- mis_builder_budget_operating_unit/__manifest__.py
+++ mis_builder_budget_operating_unit/__manifest__.py
@@ -2,12 +2,12 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "MIS Builder Budget with Operating Unit",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Reporting",
-    "author": "Camptocamp SA, " "Odoo Community Association (OCA)",
+    "author": "Camptocamp SA,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
-    "depends": ["mis_builder_budget", "operating_unit",],
-    "data": ["views/mis_builder_budget.xml",],
+    "depends": ["mis_builder_budget", "operating_unit"],
+    "data": ["views/mis_builder_budget.xml"],
     "auto_install": True,
 }
