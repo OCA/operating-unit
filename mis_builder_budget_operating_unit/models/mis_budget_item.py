271c263abe18c564f7d16adacb8ee709866eda5a
Kitti U <kittiu@gmail.com>
2021-05-11 16:44:36 +0700

    [IMP] mis_builder_budget_operating_unit: black, isort, prettier

--- mis_builder_budget_operating_unit/models/mis_budget_item.py
+++ mis_builder_budget_operating_unit/models/mis_budget_item.py
@@ -4,9 +4,8 @@ from odoo import fields, models
 
 
 class MisBudgetItem(models.Model):
-    _inherit = 'mis.budget.item'
+    _inherit = "mis.budget.item"
 
     operating_unit_id = fields.Many2one(
-        comodel_name='operating.unit',
-        string='Operating Unit',
+        comodel_name="operating.unit", string="Operating Unit",
     )
