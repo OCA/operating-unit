15df4e86f696ae8b290bba43a7c3603971bd192e
Rujia Liu <rujial@roof.co.nz>
2021-05-20 11:26:45 +1200

    [MIG] sale_stock_operating_unit: Migration to 14.0

--- sale_stock_operating_unit/tests/test_sale_stock_operating_unit.py
+++ sale_stock_operating_unit/tests/test_sale_stock_operating_unit.py
@@ -96,7 +96,7 @@ class TestSaleStockOperatingUnit(common.TransactionCase):
     def _create_sale_team(self, uid, operating_unit):
         """Create a sale team."""
         team = (
-            self.sale_team_model.sudo(uid)
+            self.sale_team_model.with_user(uid)
             .with_context(mail_create_nosubscribe=True)
             .create(
                 {"name": operating_unit.name, "operating_unit_id": operating_unit.id}
@@ -106,7 +106,7 @@ class TestSaleStockOperatingUnit(common.TransactionCase):
 
     def _create_sale_order(self, uid, customer, product, pricelist, team, wh):
         """Create a sale order."""
-        sale = self.sale_model.sudo(uid).create(
+        sale = self.sale_model.with_user(uid).create(
             {
                 "partner_id": customer.id,
                 "partner_invoice_id": customer.id,
@@ -117,7 +117,7 @@ class TestSaleStockOperatingUnit(common.TransactionCase):
                 "warehouse_id": wh.id,
             }
         )
-        self.sale_line_model.sudo(uid).create(
+        self.sale_line_model.with_user(uid).create(
             {"order_id": sale.id, "product_id": product.id, "name": "Sale Order Line"}
         )
         return sale
