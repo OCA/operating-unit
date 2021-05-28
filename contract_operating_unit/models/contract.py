7f54691b83ab426666f85c52c23662aa542b7db6
Kitti U <kittiu@gmail.com>
2021-05-28 10:38:51 +0700

    [IMP] contract_operating_unit: black, isort, prettier

--- contract_operating_unit/models/contract.py
+++ contract_operating_unit/models/contract.py
@@ -20,5 +20,5 @@ class ContractContract(models.Model):
     def _prepare_invoice(self, date_invoice, journal=None):
         invoice_vals = super()._prepare_invoice(date_invoice, journal)
         if self.operating_unit_id:
-            invoice_vals['operating_unit_id'] = self.operating_unit_id.id
+            invoice_vals["operating_unit_id"] = self.operating_unit_id.id
         return invoice_vals
