7fc8ec648131b397678b661a1eea6a975ff07cff
Kitti U <kittiu@gmail.com>
2021-06-07 20:56:57 +0700

    [14.0][ADD] analytic_operating_unit_access_all

---
+++ analytic_operating_unit_access_all/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Access all OUs' Analytics",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/operating-unit",
+    "depends": ["analytic_operating_unit"],
+    "data": [
+        "security/analytic_security.xml",
+    ],
+    "installable": True,
+    "maintainers": ["kittiu"],
+}
---
+++ analytic_operating_unit_access_all/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allow a user to have Access all OUs' analytic,
+without having to add OUs in user setting.
