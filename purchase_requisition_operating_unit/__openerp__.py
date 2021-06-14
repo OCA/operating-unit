a23ebf10e85dc2e395d7a1b9baace96319a67340
Kitti U <kittiu@gmail.com>
2021-06-14 15:01:07 +0700

    [IMP] purchase_requisition_operating_unit: black, isort, prettier

--- purchase_requisition_operating_unit/__openerp__.py
+++ purchase_requisition_operating_unit/__openerp__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
@@ -7,16 +6,15 @@
     "name": "Operating Unit in Purchase Requisitions",
     "version": "9.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L., "
-              "Serpent Consulting Services Pvt. Ltd.,"
-              "Odoo Community Association (OCA)",
+    "Serpent Consulting Services Pvt. Ltd.,"
+    "Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    "website": "http://www.eficent.com",
+    "website": "https://github.com/OCA/operating-unit",
     "category": "Purchase Management",
-    "depends": ["purchase_requisition",
-                "purchase_operating_unit"],
+    "depends": ["purchase_requisition", "purchase_operating_unit"],
     "data": [
         "view/purchase_requisition.xml",
         "security/purchase_security.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
