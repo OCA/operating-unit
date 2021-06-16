7f1b625bd59512bb31ec44147161729e10627c39
Kitti U <kittiu@gmail.com>
2021-06-16 14:58:11 +0700

    [14.0][ADD] operating_unit_access_all

---
+++ operating_unit_access_all/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Access all Operating Units",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "category": "Generic",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/operating-unit",
+    "depends": ["operating_unit"],
+    "data": [
+        "security/operating_unit_security.xml",
+    ],
+    "installable": True,
+    "maintainers": ["kittiu"],
+}
