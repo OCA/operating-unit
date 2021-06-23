0539dd63a717e49ea7ab13e60969b6b687fa8996
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-06-23 12:54:07 +0530

    [FIX] Fixed Travis.

--- hr_operating_unit/models/hr_employee.py
+++ hr_operating_unit/models/hr_employee.py
@@ -4,17 +4,19 @@ from odoo import fields, models
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
     operating_unit_ids = fields.Many2many(
-        'operating.unit', 'operating_unit_employees_rel', 'employee_id',
-        'poid', 'Operating Units',
-        default=lambda self: (
-            self.env['res.users'].operating_unit_default_get())
+        "operating.unit",
+        "operating_unit_employees_rel",
+        "employee_id",
+        "poid",
+        "Operating Units",
+        default=lambda self: (self.env["res.users"].operating_unit_default_get()),
     )
 
     default_operating_unit_id = fields.Many2one(
-        'operating.unit', 'Default Operating Unit',
-        default=lambda self: (
-            self.env['res.users'].operating_unit_default_get())
+        "operating.unit",
+        "Default Operating Unit",
+        default=lambda self: (self.env["res.users"].operating_unit_default_get()),
     )
