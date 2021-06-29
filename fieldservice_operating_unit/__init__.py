6906e4f5923b3a2cae755f413710bbec7e8d5551
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-06-29 16:41:25 +0530

    [14.0][MIG]fieldservice_operating_unit: migrated to v14
    [MIG] pre-commit

--- fieldservice_operating_unit/__init__.py
+++ fieldservice_operating_unit/__init__.py
@@ -1,2 +1,4 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright (C) 2021 Open Source Integrators
+# Copyright (C) 2021 Serpent Consulting Services
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import models
--- fieldservice_operating_unit/models/fsm_order.py
+++ fieldservice_operating_unit/models/fsm_order.py
@@ -1,15 +1,14 @@
-# Copyright (C) 2019 Open Source Integrators
-# Copyright (C) 2019 Serpent Consulting Services
+# Copyright (C) 2021 Open Source Integrators
+# Copyright (C) 2021 Serpent Consulting Services
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
 
 class FSMOrder(models.Model):
-    _inherit = 'fsm.order'
+    _inherit = "fsm.order"
 
     operating_unit_id = fields.Many2one(
-        comodel_name='operating.unit',
-        string='Operating Unit',
-        default=lambda self: self.env['res.users'].operating_unit_default_get(
-            self._uid)
+        comodel_name="operating.unit",
+        string="Operating Unit",
+        default=lambda self: self.env.user.default_operating_unit_id,
     )
