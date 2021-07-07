a5e78752620674ea5fc23d0b8cc46e7f923b1ad6
Murtaza Mithaiwala <77261450+Murtaza-OSI@users.noreply.github.com>
2021-07-07 13:23:08 +0530

    Update hr_operating_unit/models/hr_operating_unit.py
    Co-authored-by: Kitti U. @ Ecosoft <kittiu@ecosoft.co.th>
    
    [MIG] Pre-Commit
    
    [FIX] pre-commit

--- hr_operating_unit/migrations/14.0.1.0.0/pre-migration.py
+++ hr_operating_unit/migrations/14.0.1.0.0/pre-migration.py
@@ -3,15 +3,14 @@
 
 from openupgradelib import openupgrade
 
-
 _column_renames = {
-    'operating_unit_employees_rel': [
-        ('poid', 'operating_unit_id'),
-    ], }
+    "operating_unit_employees_rel": [
+        ("poid", "operating_unit_id"),
+    ],
+}
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    if openupgrade.column_exists(
-            env.cr, "operating_unit_employees_rel", "poid"):
+    if openupgrade.column_exists(env.cr, "operating_unit_employees_rel", "poid"):
         openupgrade.rename_columns(env.cr, _column_renames)
