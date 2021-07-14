8c3baff6ce3011b079756ba201a64a093c3fbd2b
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-14 12:42:31 +0530

    [14.0][MIG]agreement_operating_unit: Travis

--- agreement_operating_unit/models/agreement.py
+++ agreement_operating_unit/models/agreement.py
@@ -5,11 +5,12 @@ from odoo import fields, models
 
 
 class Agreement(models.Model):
-    _inherit = 'agreement'
+    _inherit = "agreement"
 
     operating_unit_id = fields.Many2one(
-        comodel_name='operating.unit',
-        string='Operating Unit',
-        default=lambda self: self.env['res.users'].operating_unit_default_get(
-            self._uid)
+        comodel_name="operating.unit",
+        string="Operating Unit",
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
+            self._uid
+        ),
     )
