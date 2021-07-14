0f51114247ceffe63950769639047af09d4c8387
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-14 12:42:31 +0530

    [14.0][MIG]agreement_operating_unit: Travis

--- agreement_operating_unit/__manifest__.py
+++ agreement_operating_unit/__manifest__.py
@@ -2,29 +2,26 @@
 # Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": 'Agreement with Operating Units',
+    "name": "Agreement with Operating Units",
     "summary": """
         This module adds operating unit information to agreements and service
         profiles.""",
-    "version": "12.0.1.1.0",
+    "version": "14.0.1.0.0",
     "author": "Open Source Integrators, "
-              "Serpent Consulting Services Pvt. Ltd.,"
-              "Odoo Community Association (OCA)",
+    "Serpent Consulting Services Pvt. Ltd.,"
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Agreement",
-    "depends": [
-        'operating_unit',
-        'agreement_serviceprofile'
-    ],
+    "depends": ["operating_unit", "agreement_serviceprofile"],
     "license": "AGPL-3",
     "data": [
-        'security/aggreement_security.xml',
-        'views/agreement.xml',
-        'views/agreement_serviceprofile.xml',
+        "security/aggreement_security.xml",
+        "views/agreement.xml",
+        "views/agreement_serviceprofile.xml",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': [
-        'max3903',
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": [
+        "max3903",
     ],
 }
--- agreement_operating_unit/models/agreement_serviceprofile.py
+++ agreement_operating_unit/models/agreement_serviceprofile.py
@@ -5,12 +5,13 @@ from odoo import fields, models
 
 
 class AgreementServiceprofile(models.Model):
-    _inherit = 'agreement.serviceprofile'
+    _inherit = "agreement.serviceprofile"
 
     operating_unit_id = fields.Many2one(
-        comodel_name='operating.unit',
-        related='agreement_id.operating_unit_id',
-        string='Operating Unit',
-        default=lambda self: self.env['res.users'].operating_unit_default_get(
-            self._uid)
+        comodel_name="operating.unit",
+        related="agreement_id.operating_unit_id",
+        string="Operating Unit",
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
+            self._uid
+        ),
     )
