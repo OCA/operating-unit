ae76bc1509e3fb1a311c0ac523fbab307ae898aa
Kitti U <kittiu@gmail.com>
2021-07-25 21:09:22 +0700

    [14.0][ADD] hr_expense_operating_unit_access_all

---
+++ hr_expense_operating_unit_access_all/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Access all OUs' Expenses",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "category": "Generic Modules/Human Resources",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/operating-unit",
+    "depends": ["hr_expense_operating_unit"],
+    "data": [
+        "security/hr_expense_security.xml",
+    ],
+    "installable": True,
+    "maintainers": ["kittiu"],
+}
---
+++ hr_expense_operating_unit_access_all/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module allow a user to have Access all OUs' expenses,
+without having to add OUs in user setting.
+
+Example use case, a shared HR team under an OU
+but need to work on Expenses of all OUs.
