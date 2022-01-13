bdcc5367379a903aa4c1dbb923fb7f3c46965c0f
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2022-01-13 13:50:54 -0600

    [MIG] purchase_operating_unit: Migration to 15.0

--- purchase_operating_unit/models/account_move.py
+++ purchase_operating_unit/models/account_move.py
@@ -1,6 +1,6 @@
-# © 2015-17 ForgeFlow S.L.
+# Copyright 2015-17 ForgeFlow S.L.
 # - Jordi Ballester Alomar
-# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import _, api, exceptions, models
 
--- purchase_operating_unit/tests/test_purchase_operating_unit.py
+++ purchase_operating_unit/tests/test_purchase_operating_unit.py
@@ -1,6 +1,6 @@
-# © 2015-17 ForgeFlow S.L.
+# Copyright 2015-17 ForgeFlow S.L.
 # - Jordi Ballester Alomar
-# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 import time
 
@@ -12,16 +12,13 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
     def setUp(self):
         super(TestPurchaseOperatingUnit, self).setUp()
         self.ResUsers = self.env["res.users"]
-        self.StockPicking = self.env["stock.picking"]
         self.PurchaseOrder = self.env["purchase.order"]
         self.AccountInvoice = self.env["account.move"]
         self.AccountAccount = self.env["account.account"]
         # company
-        self.company1 = self.env.ref("base.main_company")
-        self.company2 = self.env.ref("stock.res_company_1")
+        self.company = self.env.ref("base.main_company")
         # groups
         self.group_purchase_user = self.env.ref("purchase.group_purchase_user")
-        self.group_stock_user = self.env.ref("stock.group_stock_user")
         # Main Operating Unit
         self.ou1 = self.env.ref("operating_unit.main_operating_unit")
         # B2B Operating Unit
@@ -40,14 +37,14 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
         # Create users
         self.user1_id = self._create_user(
             "user_1",
-            [self.group_purchase_user, self.group_stock_user],
-            self.company1,
+            [self.group_purchase_user],
+            self.company,
             [self.ou1],
         )
         self.user2_id = self._create_user(
             "user_2",
-            [self.group_purchase_user, self.group_stock_user],
-            self.company2,
+            [self.group_purchase_user],
+            self.company,
             [self.b2b],
         )
         self.purchase1 = self._create_purchase(
@@ -60,7 +57,7 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
     def _create_user(self, login, groups, company, operating_units):
         """Create a user."""
         group_ids = [group.id for group in groups]
-        user = self.ResUsers.with_context({"no_reset_password": True}).create(
+        user = self.ResUsers.with_context(**{"no_reset_password": True}).create(
             {
                 "name": "Chicago Purchase User",
                 "login": login,
@@ -95,7 +92,7 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
                 "requesting_operating_unit_id": self.ou1.id,
                 "partner_id": self.partner1.id,
                 "order_line": lines,
-                "company_id": self.company1.id,
+                "company_id": self.company.id,
             }
         )
         return purchase
@@ -105,7 +102,6 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
         invoice_vals = {
             "purchase_id": purchase.id,
             "partner_id": partner.id,
-            # "account_id": account.id,
             "move_type": "in_invoice",
         }
         purchase_context = {
@@ -114,6 +110,8 @@ class TestPurchaseOperatingUnit(common.TransactionCase):
             "active_model": "purchase.order",
         }
         res = (
-            self.env["account.move"].with_context(purchase_context).create(invoice_vals)
+            self.env["account.move"]
+            .with_context(**purchase_context)
+            .create(invoice_vals)
         )
         return res
