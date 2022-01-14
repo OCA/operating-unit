17c142121195689a28e034cd7c1d9186cc4a7dfd
Bastian Guenther <Bastian.Guenther@ametras.com>
2022-01-14 12:02:42 +0100

    [IMP] sales_team_operating_unit: Added indexes on operating_unit_id

--- sales_team_operating_unit/models/crm_team.py
+++ sales_team_operating_unit/models/crm_team.py
@@ -13,6 +13,7 @@ class CrmTeam(models.Model):
     operating_unit_id = fields.Many2one(
         "operating.unit",
         default=lambda self: self.env["res.users"].operating_unit_default_get(),
+        index=True,
     )
 
     @api.constrains("operating_unit_id", "company_id")
