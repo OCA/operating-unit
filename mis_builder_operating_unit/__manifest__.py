5f306f54bee431b977efdacd3faca6ea611582bf
Rajan Soni <rajan.s.serpentcs@gmail.com>
2022-01-31 17:53:58 +0530

    [MIG] Migrated mis_builder_operating_unit in v15

--- mis_builder_operating_unit/__manifest__.py
+++ mis_builder_operating_unit/__manifest__.py
@@ -1,17 +1,17 @@
-# © 2015-19 ForgeFlow S.L. -
+# Copyright 2015-19 ForgeFlow S.L. -
 # Jordi Ballester Alomar
 # © 2015-19 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "MIS Builder with Operating Unit",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Reporting",
     "author": "ForgeFlow, "
     "Serpent Consulting Services Pvt. Ltd.,"
     "ACSONE SA/NV,"
     "Odoo Community Association (OCA)",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "depends": ["mis_builder", "account_operating_unit"],
     "data": ["security/mis_builder_security.xml", "view/mis_builder.xml"],
