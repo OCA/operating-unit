53520cceada91ae159948243e07d28015c68eac5
AaronHForgeFlow <ahenriquez@eficent.com>
2022-04-20 16:46:47 +0200

    [FIX] contract_operating_unit: license should be AGPL because of dependency

--- contract_operating_unit/__manifest__.py
+++ contract_operating_unit/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2020 ForgeFlow S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Contract Operating Unit",
     "version": "14.0.1.0.0",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Invoicing",
