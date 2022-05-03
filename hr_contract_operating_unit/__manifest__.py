8712dfbb550f5cef279d328312ab48942913f3ff
Juany Davila <juanyadavilap@gmail.com>
2022-05-03 18:09:00 -0500

    [IMP] hr_contract_operating_unit: black, isort, prettier

--- hr_contract_operating_unit/__manifest__.py
+++ hr_contract_operating_unit/__manifest__.py
@@ -9,14 +9,11 @@
     "version": "12.0.1.0.0",
     "license": "LGPL-3",
     "author": "Eficent Business and IT Consulting Services S.L., "
-              "Serpent Consulting Services Pvt. Ltd.,"
-              "Odoo Community Association (OCA)",
+    "Serpent Consulting Services Pvt. Ltd.,"
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Generic Modules/Human Resources",
     "depends": ["hr_contract", "operating_unit"],
-    "data": [
-        "views/hr_contract_view.xml",
-        "security/hr_contract_security.xml"
-    ],
-    'installable': True,
+    "data": ["views/hr_contract_view.xml", "security/hr_contract_security.xml"],
+    "installable": True,
 }
--- hr_contract_operating_unit/models/hr_contract.py
+++ hr_contract_operating_unit/models/hr_contract.py
@@ -9,9 +9,12 @@ from odoo import fields, models
 
 class HrContract(models.Model):
 
-    _inherit = 'hr.contract'
+    _inherit = "hr.contract"
 
-    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
-                                        default=lambda self:
-                                        self.env['res.users'].
-                                        operating_unit_default_get(self._uid))
+    operating_unit_id = fields.Many2one(
+        "operating.unit",
+        "Operating Unit",
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
+            self._uid
+        ),
+    )
