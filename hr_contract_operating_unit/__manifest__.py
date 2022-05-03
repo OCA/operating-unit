882a052442b43dbdcf48bfd1009e24692fe0fbc1
Juany Davila <juanyadavilap@gmail.com>
2022-05-03 18:28:35 -0500

    [MIG] hr_contract_operating_unit: Migration to 14.0

--- hr_contract_operating_unit/__manifest__.py
+++ hr_contract_operating_unit/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2016-19 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# Copyright 2016-19 ForgeFlow S.L.
+#   (http://www.forgeflow.com)
 # Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
 #   (<http://www.serpentcs.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "HR Contract Operating Unit",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
-    "author": "Eficent Business and IT Consulting Services S.L., "
+    "author": "Forgeflow S.L., "
     "Serpent Consulting Services Pvt. Ltd.,"
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
