a7c3db4fab3f02b026dcfccd392e8c1df0b8cbe2
Juany Davila <juanyadavilap@gmail.com>
2022-06-20 19:17:19 -0500

    [MIG] hr_contract_operating_unit: Migration to 15.0

--- hr_contract_operating_unit/models/hr_contract.py
+++ hr_contract_operating_unit/models/hr_contract.py
@@ -17,4 +17,14 @@ class HrContract(models.Model):
         default=lambda self: self.env["res.users"].operating_unit_default_get(
             self._uid
         ),
+        domain=lambda self: [
+            (
+                "id",
+                "in",
+                [
+                    operating_unit.id
+                    for operating_unit in self.env.user.operating_unit_ids
+                ],
+            )
+        ],
     )
