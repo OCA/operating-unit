178d7a6beb5899380262ac84b03b15cc0ec3bcba
jorge alberto olvera cuenca <jorge.olvera@jarsa.com>
2022-07-15 11:59:20 -0500

    [MIG] sale_operating_unit_sequence: Migration to 15.0

--- sale_operating_unit_sequence/__manifest__.py
+++ sale_operating_unit_sequence/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Sequence by Operating Unit",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "category": "Sale",
     "author": "Ecosoft, Odoo Community Association (OCA)",
