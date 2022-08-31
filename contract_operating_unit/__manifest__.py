819aa4c9e41cde35a2550f718ebfdd221c3546e8
rajasree.r@beo.in <rajasree.r@beo.in>
2022-08-31 13:29:27 +0530

    APX-1549: [ADD] contract_operating_unit

---
+++ contract_operating_unit/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Contract Operating Unit",
+    "version": "13.0.1.0.0",
+    "category": "Invoicing",
+    "summary": """Added operating unit in Contract.""",
+    "author": "Ametras intelligence GmbH",
+    "website": "http://www.ametras.com/",
+    "depends": ["contract", "account_operating_unit"],
+    "data": ["security/contract_security.xml", "views/contract_view.xml"],
+    "license": "OPL-1",
+}
