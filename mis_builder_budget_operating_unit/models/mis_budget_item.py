c3b6b3e3294ed02b30b36fd78b9d1576de2e635a
Téo Goddet <teo.goddet@gmail.com>
2022-11-22 12:54:19 +0000

    [IMP] Add OU to mis budget by account items
    this commit add the OU field to MIS budget by account item view
    and model

--- mis_builder_budget_operating_unit/models/mis_budget_item.py
+++ mis_builder_budget_operating_unit/models/mis_budget_item.py
@@ -3,8 +3,8 @@
 from odoo import fields, models
 
 
-class MisBudgetItem(models.Model):
-    _inherit = "mis.budget.item"
+class MisBudgetItem(models.AbstractModel):
+    _inherit = "mis.budget.item.abstract"
 
     operating_unit_id = fields.Many2one(
         comodel_name="operating.unit",
