81447360d06513a63b33207fa222c252df95d3b1
ps-tubtim <pimolnats@ecosoft.co.th>
2022-12-28 13:35:18 +0700

    [FIX] account_operating_unit: add OU to cash basis journal entry

--- account_operating_unit/models/__init__.py
+++ account_operating_unit/models/__init__.py
@@ -4,4 +4,5 @@ from . import res_company
 from . import account_bank_statement
 from . import account_journal
 from . import account_move
+from . import account_partial_reconcile
 from . import account_payment
