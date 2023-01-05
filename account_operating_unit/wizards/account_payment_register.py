b562568cf76875a3a7a1ae714ce558ec44922436
ps-tubtim <pimolnats@ecosoft.co.th>
2023-01-05 14:02:50 +0700

    [FIX] account_operating_unit: raise error when group payment from moves with difference OUs

--- account_operating_unit/wizards/account_payment_register.py
+++ account_operating_unit/wizards/account_payment_register.py
@@ -1,7 +1,8 @@
 # © 2020 Jarsa Sistemas, SA de CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import UserError
 
 
 class AccountPaymentRegister(models.TransientModel):
@@ -16,6 +17,12 @@ class AccountPaymentRegister(models.TransientModel):
             reconciled_moves = (
                 payment.reconciled_bill_ids + payment.reconciled_invoice_ids
             )
+            if len(reconciled_moves.operating_unit_id) > 1:
+                raise UserError(
+                    _(
+                        "The OU in the Bills/Invoices to register payment must be the same."
+                    )
+                )
             if reconciled_moves.operating_unit_id != payment.operating_unit_id:
                 destination_account = payments.destination_account_id
                 to_reconcile |= payment.move_id.line_ids.filtered(
