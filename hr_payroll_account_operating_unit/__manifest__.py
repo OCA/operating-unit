117318faa1c8f946fdb4fc2adcb94c3e092b6fc5
olveracuenca <jorge.olvera@jarsa.com.mx>
2023-01-19 15:11:59 -0600

    [MIG] hr_payroll_account_operating_unit: Migration to 15.0

--- hr_payroll_account_operating_unit/__manifest__.py
+++ hr_payroll_account_operating_unit/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "HR Payroll Account Operating Unit",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "LGPL-3",
     "author": "ForgeFlow S.L., "
     "Serpent Consulting Services Pvt. Ltd.,"
@@ -14,7 +14,7 @@
     "category": "Generic Modules/Human Resources",
     "website": "https://github.com/OCA/operating-unit",
     "depends": [
-        "payroll_account",
+        "hr_payroll_account",
         "hr_contract_operating_unit",
         "account_operating_unit",
     ],
