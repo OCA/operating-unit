dea6d21592dcd3d9b92f8666beaa835cd3c56670
Carlos Ramos <cramosh84@gmail.com>
2023-01-27 10:11:22 +0100

    [FIX] Error sale report to add fiel operating unit

--- sale_operating_unit/report/sale_report.py
+++ sale_operating_unit/report/sale_report.py
@@ -1,17 +1,19 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# Copyright 2022 Dm Integrations - Carlos Ramos
 
 from odoo import fields, models
 
 
 class SaleReport(models.Model):
-
     _inherit = "sale.report"
 
     operating_unit_id = fields.Many2one("operating.unit", "Operating Unit")
 
-    def _query(self, with_clause="", fields=False, groupby="", from_clause=""):  # noqa
-        if not fields:
-            fields = {}
+    def _group_by_sale(self, groupby=""):
+        res = super()._group_by_sale(groupby)
+        res += """, s.operating_unit_id"""
+        return res
+
+    def _select_additional_fields(self, fields):
         fields["operating_unit_id"] = ", s.operating_unit_id as operating_unit_id"
-        groupby += ", s.operating_unit_id"
-        return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
+        return super()._select_additional_fields(fields)
