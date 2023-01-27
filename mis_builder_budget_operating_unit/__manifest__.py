e8b0edc91b75504c9ee172d22d211cbba13d435a
Miquel Raïch <miquel.raich@forgeflow.com>
2023-01-27 12:44:27 +0100

    [UPD] Licence: be coherent

--- mis_builder_budget_operating_unit/__manifest__.py
+++ mis_builder_budget_operating_unit/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright (C) 2018 by Camptocamp
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "MIS Builder Budget with Operating Unit",
     "version": "13.0.1.0.0",
     "category": "Reporting",
     "author": "Camptocamp SA,Odoo Community Association (OCA)",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "depends": ["mis_builder_budget", "operating_unit"],
     "data": ["views/mis_builder_budget.xml"],
