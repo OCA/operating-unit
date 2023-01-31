1ff7ae252376165f77af0be592cf069303d14f0a
Saran440 <saranl@ecosoft.co.th>
2023-01-31 12:51:10 +0700

    [15.0][MIG] hr_operating_unit

--- hr_operating_unit/__manifest__.py
+++ hr_operating_unit/__manifest__.py
@@ -1,8 +1,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
     "name": "HR Operating Unit",
-    "version": "14.0.1.1.0",
-    "author": "Gonzalo González Domínguez," "Odoo Community Association (OCA)",
+    "version": "15.0.1.0.0",
+    "author": "Gonzalo González Domínguez, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Human Resources",
