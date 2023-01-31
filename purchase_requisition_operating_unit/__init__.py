f84a632bbc22cde0f3884d5eaca9229456dbab1a
Saran440 <saranl@ecosoft.co.th>
2023-01-31 15:16:35 +0700

    [15.0][MIG] purchase_requisition_operating_unit

--- purchase_requisition_operating_unit/__init__.py
+++ purchase_requisition_operating_unit/__init__.py
@@ -1,5 +1,5 @@
-# © 2016 ForgeFlow S.L. (https://www.forgeflow.com)
-# © 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 from . import model
-from . import tests
