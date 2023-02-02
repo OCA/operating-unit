9e4ad76ad2d759388f534e9cbd5dcdb32b8add3b
Saran440 <saranl@ecosoft.co.th>
2023-02-02 09:37:57 +0700

    [15.0][ADD] purchase_deposit_operating_unit

---
+++ purchase_deposit_operating_unit/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Operating Unit in Purchase Deposit",
+    "summary": "Adds the concecpt of operating unit (OU) when create purchase deposit",
+    "version": "15.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/operating-unit",
+    "category": "Purchase Management",
+    "depends": ["purchase_operating_unit", "purchase_deposit"],
+    "license": "AGPL-3",
+    "maintainers": ["kittiu"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ purchase_deposit_operating_unit/readme/USAGE.rst
@@ -0,0 +1,2 @@
+#. Form the confirmed PO, click Request Depost button, and create a deposit.
+#. The operating unit of the invoice created will use the same operating unit with PO.
