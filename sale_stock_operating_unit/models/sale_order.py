e49bb3aece3e05657788894188057cf59e2b4248
Danny W. Adair <danny.adair@unfold.co.nz>
2023-04-14 13:28:02 +0200

    [MIG] sale_stock_operating_unit: Migration to 14.0

--- sale_stock_operating_unit/models/sale_order.py
+++ sale_stock_operating_unit/models/sale_order.py
@@ -2,7 +2,7 @@
 # Jordi Ballester Alomar
 # Copyright 2015-19 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import _, api, fields, models
+from odoo import SUPERUSER_ID, _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -14,7 +14,14 @@ class SaleOrder(models.Model):
         res = super(SaleOrder, self)._default_warehouse_id()
         team = self._get_default_team()
         warehouses = self.env["stock.warehouse"].search(
-            [("operating_unit_id", "=", team.sudo().operating_unit_id.id)], limit=1
+            [
+                (
+                    "operating_unit_id",
+                    "=",
+                    team.with_user(SUPERUSER_ID).operating_unit_id.id,
+                )
+            ],
+            limit=1,
         )
         if warehouses:
             return warehouses
@@ -26,7 +33,6 @@ class SaleOrder(models.Model):
 
     @api.onchange("team_id")
     def onchange_team_id(self):
-        super(SaleOrder, self).onchange_team_id()
         if self.team_id and self.team_id.operating_unit_id:
             warehouses = self.env["stock.warehouse"].search(
                 [("operating_unit_id", "=", self.team_id.operating_unit_id.id)], limit=1
@@ -38,13 +44,14 @@ class SaleOrder(models.Model):
     def onchange_operating_unit_id(self):
         if self.operating_unit_id:
             warehouses = self.env["stock.warehouse"].search(
-                [("operating_unit_id", "=", self.operating_unit_id.id)], limit=1
+                [("operating_unit_id", "=", self.operating_unit_id.id)]
             )
-            if warehouses:
+            if warehouses and self.warehouse_id not in warehouses:
                 self.warehouse_id = warehouses[0]
 
     @api.onchange("warehouse_id")
     def onchange_warehouse_id(self):
+        self.onchange_operating_unit_id()
         if self.warehouse_id:
             self.operating_unit_id = self.warehouse_id.operating_unit_id
             if (
@@ -53,7 +60,6 @@ class SaleOrder(models.Model):
             ):
                 self.team_id = False
 
-    @api.multi
     @api.constrains("operating_unit_id", "warehouse_id")
     def _check_wh_operating_unit(self):
         for rec in self:
