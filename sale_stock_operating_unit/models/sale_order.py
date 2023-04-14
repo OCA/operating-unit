700195bc92e15bbc55bcf4a084bd8ebd55449cdd
AaronHForgeFlow <ahenriquez@eficent.com>
2023-04-14 13:27:05 +0200

    [IMP] sale_stock_operating_unit: black, isort, prettier

--- sale_stock_operating_unit/models/sale_order.py
+++ sale_stock_operating_unit/models/sale_order.py
@@ -2,65 +2,70 @@
 # Jordi Ballester Alomar
 # Copyright 2015-19 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.model
     def _default_warehouse_id(self):
         res = super(SaleOrder, self)._default_warehouse_id()
         team = self._get_default_team()
-        warehouses = self.env['stock.warehouse'].search(
-            [('operating_unit_id', '=', team.sudo().operating_unit_id.id)],
-            limit=1)
+        warehouses = self.env["stock.warehouse"].search(
+            [("operating_unit_id", "=", team.sudo().operating_unit_id.id)], limit=1
+        )
         if warehouses:
             return warehouses
         return res
 
     warehouse_id = fields.Many2one(
-        comodel_name='stock.warehouse',
-        default=_default_warehouse_id)
+        comodel_name="stock.warehouse", default=_default_warehouse_id
+    )
 
-    @api.onchange('team_id')
+    @api.onchange("team_id")
     def onchange_team_id(self):
         super(SaleOrder, self).onchange_team_id()
         if self.team_id and self.team_id.operating_unit_id:
-            warehouses = self.env['stock.warehouse'].search(
-                [('operating_unit_id', '=',
-                  self.team_id.operating_unit_id.id)],
-                limit=1)
+            warehouses = self.env["stock.warehouse"].search(
+                [("operating_unit_id", "=", self.team_id.operating_unit_id.id)], limit=1
+            )
             if warehouses:
                 self.warehouse_id = warehouses[0]
 
-    @api.onchange('operating_unit_id')
+    @api.onchange("operating_unit_id")
     def onchange_operating_unit_id(self):
         if self.operating_unit_id:
-            warehouses = self.env['stock.warehouse'].search(
-                [('operating_unit_id', '=',
-                  self.operating_unit_id.id)],
-                limit=1)
+            warehouses = self.env["stock.warehouse"].search(
+                [("operating_unit_id", "=", self.operating_unit_id.id)], limit=1
+            )
             if warehouses:
                 self.warehouse_id = warehouses[0]
 
-    @api.onchange('warehouse_id')
+    @api.onchange("warehouse_id")
     def onchange_warehouse_id(self):
         if self.warehouse_id:
             self.operating_unit_id = self.warehouse_id.operating_unit_id
-            if self.team_id and self.team_id.operating_unit_id != \
-                    self.operating_unit_id:
+            if (
+                self.team_id
+                and self.team_id.operating_unit_id != self.operating_unit_id
+            ):
                 self.team_id = False
 
     @api.multi
-    @api.constrains('operating_unit_id', 'warehouse_id')
+    @api.constrains("operating_unit_id", "warehouse_id")
     def _check_wh_operating_unit(self):
         for rec in self:
-            if (rec.warehouse_id.operating_unit_id and
-                    rec.operating_unit_id and
-                    rec.operating_unit_id !=
-                    rec.warehouse_id.operating_unit_id):
-                raise ValidationError(_('Configuration error!\nThe Operating'
-                                        'Unit in the Sales Order and in the'
-                                        ' Warehouse must be the same.'))
+            if (
+                rec.warehouse_id.operating_unit_id
+                and rec.operating_unit_id
+                and rec.operating_unit_id != rec.warehouse_id.operating_unit_id
+            ):
+                raise ValidationError(
+                    _(
+                        "Configuration error!\nThe Operating"
+                        "Unit in the Sales Order and in the"
+                        " Warehouse must be the same."
+                    )
+                )
--- sale_stock_operating_unit/tests/test_sale_stock_operating_unit.py
+++ sale_stock_operating_unit/tests/test_sale_stock_operating_unit.py
@@ -7,40 +7,41 @@ from odoo.tests import common
 
 
 class TestSaleStockOperatingUnit(common.TransactionCase):
-
     def setUp(self):
         super(TestSaleStockOperatingUnit, self).setUp()
-        self.res_groups = self.env['res.groups']
-        self.res_users_model = self.env['res.users']
-        self.warehouse_model = self.env['stock.warehouse']
-        self.sale_model = self.env['sale.order']
-        self.sale_line_model = self.env['sale.order.line']
-        self.sale_team_model = self.env['crm.team']
+        self.res_groups = self.env["res.groups"]
+        self.res_users_model = self.env["res.users"]
+        self.warehouse_model = self.env["stock.warehouse"]
+        self.sale_model = self.env["sale.order"]
+        self.sale_line_model = self.env["sale.order.line"]
+        self.sale_team_model = self.env["crm.team"]
         # Company
-        self.company = self.env.ref('base.main_company')
+        self.company = self.env.ref("base.main_company")
         # Groups
-        self.grp_sale_user = self.env.ref('sales_team.group_sale_manager')
-        self.grp_acc_user = self.env.ref('account.group_account_invoice')
+        self.grp_sale_user = self.env.ref("sales_team.group_sale_manager")
+        self.grp_acc_user = self.env.ref("account.group_account_invoice")
         # Main Operating Unit
-        self.ou1 = self.env.ref('operating_unit.main_operating_unit')
+        self.ou1 = self.env.ref("operating_unit.main_operating_unit")
         # B2C Operating Unit
-        self.b2c = self.env.ref('operating_unit.b2c_operating_unit')
+        self.b2c = self.env.ref("operating_unit.b2c_operating_unit")
         # Customer
-        self.customer = self.env.ref('base.res_partner_2')
+        self.customer = self.env.ref("base.res_partner_2")
         # Price list
-        self.pricelist = self.env.ref('product.list0')
+        self.pricelist = self.env.ref("product.list0")
         # Products
-        self.product1 = self.env.ref(
-            'product.product_product_7')
-        self.product1.write({'invoice_policy': 'order'})
+        self.product1 = self.env.ref("product.product_product_7")
+        self.product1.write({"invoice_policy": "order"})
         # Create user1
-        self.user1 = self._create_user('user_1', [self.grp_sale_user,
-                                                  self.grp_acc_user],
-                                       self.company, [self.ou1, self.b2c])
+        self.user1 = self._create_user(
+            "user_1",
+            [self.grp_sale_user, self.grp_acc_user],
+            self.company,
+            [self.ou1, self.b2c],
+        )
         # Create user2
-        self.user2 = self._create_user('user_2', [self.grp_sale_user,
-                                                  self.grp_acc_user],
-                                       self.company, [self.b2c])
+        self.user2 = self._create_user(
+            "user_2", [self.grp_sale_user, self.grp_acc_user], self.company, [self.b2c]
+        )
 
         # Create sales team OU1
         self.sale_team_ou1 = self._create_sale_team(self.user1.id, self.ou1)
@@ -49,61 +50,76 @@ class TestSaleStockOperatingUnit(common.TransactionCase):
         self.sale_team_b2c = self._create_sale_team(self.user2.id, self.b2c)
 
         # Warehouses
-        self.ou1_wh = self.env.ref('stock.warehouse0')
-        self.b2c_wh = self.env.ref('stock_operating_unit.stock_warehouse_b2c')
+        self.ou1_wh = self.env.ref("stock.warehouse0")
+        self.b2c_wh = self.env.ref("stock_operating_unit.stock_warehouse_b2c")
         # Locations
-        self.b2c_wh.lot_stock_id.write({'company_id': self.company.id,
-                                        'operating_unit_id': self.b2c.id})
+        self.b2c_wh.lot_stock_id.write(
+            {"company_id": self.company.id, "operating_unit_id": self.b2c.id}
+        )
 
         # Create Sale Order1
-        self.sale1 = self._create_sale_order(self.user1.id, self.customer,
-                                             self.product1, self.pricelist,
-                                             self.sale_team_ou1, self.ou1_wh)
+        self.sale1 = self._create_sale_order(
+            self.user1.id,
+            self.customer,
+            self.product1,
+            self.pricelist,
+            self.sale_team_ou1,
+            self.ou1_wh,
+        )
         # Create Sale Order2
-        self.sale2 = self._create_sale_order(self.user2.id, self.customer,
-                                             self.product1, self.pricelist,
-                                             self.sale_team_b2c, self.b2c_wh)
+        self.sale2 = self._create_sale_order(
+            self.user2.id,
+            self.customer,
+            self.product1,
+            self.pricelist,
+            self.sale_team_b2c,
+            self.b2c_wh,
+        )
 
     def _create_user(self, login, groups, company, operating_units):
         """Create a user."""
         group_ids = [group.id for group in groups]
-        user = self.res_users_model.create({
-            'name': 'Test Sales User',
-            'login': login,
-            'password': 'demo',
-            'email': 'example@yourcompany.com',
-            'company_id': company.id,
-            'company_ids': [(4, company.id)],
-            'operating_unit_ids': [(4, ou.id) for ou in operating_units],
-            'groups_id': [(6, 0, group_ids)]
-        })
+        user = self.res_users_model.create(
+            {
+                "name": "Test Sales User",
+                "login": login,
+                "password": "demo",
+                "email": "example@yourcompany.com",
+                "company_id": company.id,
+                "company_ids": [(4, company.id)],
+                "operating_unit_ids": [(4, ou.id) for ou in operating_units],
+                "groups_id": [(6, 0, group_ids)],
+            }
+        )
         return user
 
     def _create_sale_team(self, uid, operating_unit):
         """Create a sale team."""
-        team = self.sale_team_model.sudo(uid).with_context(
-            mail_create_nosubscribe=True).create({
-                'name': operating_unit.name,
-                'operating_unit_id': operating_unit.id
-            })
+        team = (
+            self.sale_team_model.sudo(uid)
+            .with_context(mail_create_nosubscribe=True)
+            .create(
+                {"name": operating_unit.name, "operating_unit_id": operating_unit.id}
+            )
+        )
         return team
 
     def _create_sale_order(self, uid, customer, product, pricelist, team, wh):
         """Create a sale order."""
-        sale = self.sale_model.sudo(uid).create({
-            'partner_id': customer.id,
-            'partner_invoice_id': customer.id,
-            'partner_shipping_id': customer.id,
-            'pricelist_id': pricelist.id,
-            'team_id': team.id,
-            'operating_unit_id': team.operating_unit_id.id,
-            'warehouse_id': wh.id
-        })
-        self.sale_line_model.sudo(uid).create({
-            'order_id': sale.id,
-            'product_id': product.id,
-            'name': 'Sale Order Line'
-        })
+        sale = self.sale_model.sudo(uid).create(
+            {
+                "partner_id": customer.id,
+                "partner_invoice_id": customer.id,
+                "partner_shipping_id": customer.id,
+                "pricelist_id": pricelist.id,
+                "team_id": team.id,
+                "operating_unit_id": team.operating_unit_id.id,
+                "warehouse_id": wh.id,
+            }
+        )
+        self.sale_line_model.sudo(uid).create(
+            {"order_id": sale.id, "product_id": product.id, "name": "Sale Order Line"}
+        )
         return sale
 
     def _confirm_sale(self, sale):
@@ -115,12 +131,16 @@ class TestSaleStockOperatingUnit(common.TransactionCase):
         # Confirm Sale1
         self._confirm_sale(self.sale1)
         # Checks that OU in sale order and stock picking matches or not.
-        self.assertEqual(self.sale1.operating_unit_id,
-                         self.sale1.picking_ids.operating_unit_id,
-                         'OU in Sale Order and Picking should be same')
+        self.assertEqual(
+            self.sale1.operating_unit_id,
+            self.sale1.picking_ids.operating_unit_id,
+            "OU in Sale Order and Picking should be same",
+        )
         # Confirm Sale2
         self._confirm_sale(self.sale2)
         # Checks that OU in sale order and stock picking matches or not.
-        self.assertEqual(self.sale2.operating_unit_id,
-                         self.sale2.picking_ids.operating_unit_id,
-                         'OU in Sale Order and Picking should be same')
+        self.assertEqual(
+            self.sale2.operating_unit_id,
+            self.sale2.picking_ids.operating_unit_id,
+            "OU in Sale Order and Picking should be same",
+        )
