40f9ffcef1b657300c381bf913cd3d4b5f726828
tiago <tiagofiliperangel@gmail.com>
2023-05-30 17:00:59 +0100

    [MIG] purchase_operating_unit: Migration to 16.0

---
+++ sales_team_operating_unit/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2016-17 ForgeFlow S.L. (http://www.forgeflow.com)
+# Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Sales Team Operating Unit",
+    "version": "16.0.1.0.0",
+    "author": "ForgeFlow, SerpentCS, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/operating-unit",
+    "category": "Sales",
+    "depends": ["sales_team", "operating_unit"],
+    "data": ["security/crm_security.xml", "views/crm_team_view.xml"],
+    "installable": True,
+}
---
+++ sales_team_operating_unit/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
+* Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
+* Jarsa Sistemas <info@jarsa.com.mx>
