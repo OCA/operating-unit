284326b4abbc9e2904b98f111f58bbcda17fe1a5
Borruso <gborruso@dinamicheaziendali.it>
2023-06-22 10:13:07 +0200

    [MIG] sale_operating_unit: Migration to 16.0

--- sale_operating_unit/report/sale_report.py
+++ sale_operating_unit/report/sale_report.py
@@ -8,11 +8,12 @@ class SaleReport(models.Model):
 
     operating_unit_id = fields.Many2one("operating.unit", "Operating Unit")
 
-    def _group_by_sale(self, groupby=""):
-        res = super()._group_by_sale(groupby)
+    def _group_by_sale(self):
+        res = super()._group_by_sale()
         res += """, s.operating_unit_id"""
         return res
 
-    def _select_additional_fields(self, fields):
-        fields["operating_unit_id"] = ", s.operating_unit_id as operating_unit_id"
-        return super()._select_additional_fields(fields)
+    def _select_additional_fields(self):
+        res = super()._select_additional_fields()
+        res["operating_unit_id"] = "s.operating_unit_id"
+        return res
