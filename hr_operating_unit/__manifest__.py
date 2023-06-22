cc3c9dc35810d1a4a022e64907e6207e927575cc
Borruso <gborruso@dinamicheaziendali.it>
2023-06-22 09:11:18 +0200

    [MIG] hr_operating_unit: Migration to 16.0

--- hr_operating_unit/__manifest__.py
+++ hr_operating_unit/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "HR Operating Unit",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Gonzalo González Domínguez, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/operating-unit",
