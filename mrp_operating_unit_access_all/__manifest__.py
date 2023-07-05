39fed5745695c3943ab29d85d076634a991183ff
ps-tubtim <pimolnats@ecosoft.co.th>
2023-07-05 14:41:57 +0700

    [15.0][ADD] mrp_operating_unit_access_all

---
+++ mrp_operating_unit_access_all/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Access all OUs' MRP",
+    "version": "15.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/operating-unit",
+    "depends": ["mrp_operating_unit"],
+    "data": ["security/mrp_security.xml"],
+    "installable": True,
+    "maintainers": ["ps-tubtim"],
+}
---
+++ mrp_operating_unit_access_all/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allow a user to have Access all OUs' manufacturing order,
+without having to add OUs in user setting.
