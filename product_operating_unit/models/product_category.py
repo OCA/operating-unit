b0b8b2d7ee98d8057a5a2b32f542f0d4716fbcb3
Jérémy Didderen <jeremy.didderen@nsi-sa.be>
2024-05-27 14:50:01 +0200

    [MIG] product_operating_unit: Migration to 17.0

--- product_operating_unit/models/product_category.py
+++ product_operating_unit/models/product_category.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2019 Serpent Consulting Services
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import fields, models
+from odoo.models import Command
 
 
 class ProductCategory(models.Model):
@@ -14,15 +15,15 @@ class ProductCategory(models.Model):
     )
 
     def write(self, vals):
-        res = super(ProductCategory, self).write(vals)
+        res = super().write(vals)
         product_template_obj = self.env["product.template"]
         if vals.get("operating_unit_ids"):
             for rec in self:
                 products = product_template_obj.search(
                     [("categ_id", "child_of", rec.id)]
                 )
+                category_ou_ids = rec.operating_unit_ids
                 for product in products:
-                    ou_ids = product.operating_unit_ids.ids
-                    ou_ids.extend(vals.get("operating_unit_ids")[0][2])
-                    product.operating_unit_ids = [(6, 0, ou_ids)]
+                    ou_ids = product.operating_unit_ids | category_ou_ids
+                    product.operating_unit_ids = [Command.set(ou_ids.ids)]
         return res
