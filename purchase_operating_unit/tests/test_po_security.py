dee018698d085505e0d2e9315c47cb388a4a7e48
Jérémy Didderen <jeremy.didderen@nsi-sa.be>
2024-05-29 12:27:20 +0200

    [MIG] purchase_operating_unit: Migration to 17.0

--- purchase_operating_unit/tests/test_po_security.py
+++ purchase_operating_unit/tests/test_po_security.py
@@ -11,30 +11,31 @@ class TestPoSecurity(test_po_ou.TestPurchaseOperatingUnit):
         # User 2 is only assigned to Operating Unit 2, and cannot list
         # purchase orders from Operating Unit 1.
         po_ids = (
-            self.PurchaseOrder.with_user(self.user2_id)
+            self.PurchaseOrder.with_user(self.user2)
             .search([("operating_unit_id", "=", self.ou1.id)])
             .ids
         )
         self.assertEqual(po_ids, [])
+        invoice = self._create_invoice(self.purchase1.with_user(self.user1))
         # User 2 cannot list the invoice that was created from PO 1
         invoice_ids = (
-            self.AccountInvoice.with_user(self.user2_id)
-            .search([("id", "=", self.invoice.id)])
+            self.AccountInvoice.with_user(self.user2)
+            .search([("id", "=", invoice.id)])
             .ids
         )
         self.assertEqual(invoice_ids, [])
         # User 1 is assigned to Operating Unit 1, and can list
         # the purchase order 1 from Operating Unit 1.
         po_ids = (
-            self.PurchaseOrder.with_user(self.user1_id)
+            self.PurchaseOrder.with_user(self.user1)
             .search([("operating_unit_id", "=", self.ou1.id)])
             .ids
         )
         self.assertNotEqual(po_ids, [])
         # User 1 can list the invoice that was created from PO 2
         invoice_ids = (
-            self.AccountInvoice.with_user(self.user1_id)
-            .search([("id", "=", self.invoice.id)])
+            self.AccountInvoice.with_user(self.user1)
+            .search([("id", "=", invoice.id)])
             .ids
         )
         self.assertNotEqual(invoice_ids, [])
