164eb980e512d62809813424e953b3ac59b9147d
TraiTran <traitt@trobz.com>
2024-06-25 16:52:45 +0700

    [MIG] operating_unit_access_all: Migration to 17.0

--- operating_unit_access_all/__manifest__.py
+++ operating_unit_access_all/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Access all Operating Units",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Ecosoft,Odoo Community Association (OCA)",
     "category": "Generic",
     "license": "AGPL-3",
