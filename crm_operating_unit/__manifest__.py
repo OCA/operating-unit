8032201a91926074497065c185e35e8bcfc3192a
TraiTran <traitt@trobz.com>
2024-06-26 11:06:03 +0700

    [MIG] crm_operating_unit: Migration to 17.0

--- crm_operating_unit/__manifest__.py
+++ crm_operating_unit/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Operating Unit in CRM",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "ForgeFlow, "
     "Serpent Consulting Services Pvt. Ltd.,"
     "Odoo Community Association (OCA)",
