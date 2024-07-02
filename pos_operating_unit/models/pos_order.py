c62964c2bf3447414bb2b73864cba709d42fc87c
Jérémy Didderen <jeremy.didderen@nsi-sa.be>
2024-07-02 09:16:38 +0200

    [MIG] pos_operating_unit: Migration to 17.0

--- pos_operating_unit/models/pos_order.py
+++ pos_operating_unit/models/pos_order.py
@@ -11,14 +11,15 @@ class POSOrder(models.Model):
     )
     config_id = fields.Many2one(related="session_id.config_id", readonly=True)
 
-    @api.model
-    def create(self, vals):
-        session_id = self.env["pos.session"].sudo().browse(vals.get("session_id"))
-        if session_id.config_id:
-            vals["operating_unit_ids"] = [
-                (6, 0, session_id.config_id.operating_unit_ids.ids)
-            ]
-        return super(POSOrder, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            session_id = self.env["pos.session"].sudo().browse(vals.get("session_id"))
+            if session_id.config_id:
+                vals["operating_unit_ids"] = [
+                    (6, 0, session_id.config_id.operating_unit_ids.ids)
+                ]
+        return super().create(vals_list)
 
 
 class POSOrderLine(models.Model):
@@ -30,11 +31,12 @@ class POSOrderLine(models.Model):
         string="Operating Units",
     )
 
-    @api.model
-    def create(self, vals):
-        order_id = self.env["pos.order"].sudo().browse(vals.get("order_id"))
-        if order_id.config_id:
-            vals["operating_unit_ids"] = [
-                (6, 0, order_id.config_id.operating_unit_ids.ids)
-            ]
-        return super(POSOrderLine, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            order_id = self.env["pos.order"].sudo().browse(vals.get("order_id"))
+            if order_id.config_id:
+                vals["operating_unit_ids"] = [
+                    (6, 0, order_id.config_id.operating_unit_ids.ids)
+                ]
+        return super().create(vals_list)
