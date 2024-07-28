5a332b2359159efe99327576fd53d38e18debc08
TraiTran <traitt@trobz.com>
2024-07-28 21:25:16 +0700

    [MIG] contract_operating_unit: Migration to 17.0

--- contract_operating_unit/models/contract.py
+++ contract_operating_unit/models/contract.py
@@ -10,7 +10,9 @@ class ContractContract(models.Model):
     operating_unit_id = fields.Many2one(
         "operating.unit",
         "Operating Unit",
-        default=lambda self: self.env["res.users"].operating_unit_default_get(),
+        default=lambda self: self.env["res.users"]._get_default_operating_unit(
+            self._uid
+        ),
     )
 
     def _prepare_invoice(self, date_invoice, journal=None):
