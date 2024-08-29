279a1f09268cf791abfa0aff12d1a231d8912a7b
Pani-k-folk <panithank@ecosoft.co.th>
2024-08-29 13:29:05 +0700

    [FIX] ou_hr_department_check_company

--- hr_operating_unit/models/hr_department.py
+++ hr_operating_unit/models/hr_department.py
@@ -5,9 +5,11 @@ from odoo import fields, models
 
 class Department(models.Model):
     _inherit = "hr.department"
+    _check_company_auto = True
 
     operating_unit_id = fields.Many2one(
         comodel_name="operating.unit",
         string="Operating Unit",
         default=lambda self: (self.env["res.users"].operating_unit_default_get()),
+        check_company=True,
     )
