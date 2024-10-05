fdef1310ba21e4d9f14bf78a9435ef1855da0168
Jérémy Didderen <didderen.jeremy@jdi.me>
2024-10-05 23:17:59 +0200

    [FIX] account_financial_report_operating_unit - typo in module's name

--- account_financial_report_operating_unit/__manifest__.py
+++ account_financial_report_operating_unit/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Accounting Fincnaial Report Operating Unit",
+    "name": "Accounting Financial Report Operating Unit",
     "summary": "Introduces Operating Unit (OU) in financial reports",
     "version": "17.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
