ef3d3af2419555f0dad2f07215f7bd61ae6eccc3
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2025-03-17 17:38:56 +0100

    [FIX] account_operating_unit: Inter OU Balance entry several issues: * [FIX] account_operating_unit: Inter OU Balance entry should be move_type entry * [FIX] account_operating_unit: Inter OU Balance entry should consider the amount being reconciled

--- account_operating_unit/models/account_move.py
+++ account_operating_unit/models/account_move.py
@@ -104,6 +104,7 @@ class AccountMoveLine(models.Model):
             "date": max(self.mapped("date")),
             "ref": "Inter OU Balancing",
             "company_id": journal.company_id.id,
+            "move_type": "entry",
         }
         return move_vals
 
@@ -198,11 +199,16 @@ class AccountMove(models.Model):
             "account_id": move.company_id.inter_ou_clearing_account_id.id,
             "display_type": "ou_balance",
         }
-
+        # total_balance_to_match is the minimim of the abs of the values
+        total_balance_to_match = min(
+            abs(ou_balances[ou])
+            for ou in list(ou_balances.keys())
+            if ou_balances[ou] != 0.0
+        )
         if ou_balances[ou_id] < 0.0:
-            res["debit"] = abs(ou_balances[ou_id])
+            res["debit"] = total_balance_to_match
         else:
-            res["credit"] = ou_balances[ou_id]
+            res["credit"] = total_balance_to_match
         return res
 
     def _check_ou_balance(self, move):
