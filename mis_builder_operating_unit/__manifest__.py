7f521cb9c4dfc34afaf4a886ec52c75849536480
Daniel Palomar <danypr92@gmail.com>
2025-07-17 13:35:57 +0200

    [MIG] mis_builder_operating_unit: Migration to 16.0

--- mis_builder_operating_unit/__manifest__.py
+++ mis_builder_operating_unit/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "MIS Builder with Operating Unit",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "Reporting",
     "author": "ForgeFlow, "
     "Serpent Consulting Services Pvt. Ltd.,"
