7d969c6990b8b4d35403779bd249b75e8efcc69f
Ruchir Shukla <ruchir@bizzappdev.com>
2025-09-04 17:13:05 +0530

    [MIG] mail_template_operating_unit: Migration to 15.0

--- mail_template_operating_unit/__manifest__.py
+++ mail_template_operating_unit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Mail Template Operating Unit",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Pavlov Media, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/operating-unit",
